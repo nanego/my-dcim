0d2220f749a9804284040d721a065105dc2dea60
Vincent Robert <vincent.robert@nanego.com>
2016-03-08 09:33:11 +0100

    Fix headers middleware in production

---
+++ config/initializers/fix_headers_middleware.rb
@@ -0,0 +1,16 @@
+require 'rack/utils'
+
+class FixHeadersMiddleware
+  def initialize(app)
+    @app = app
+  end
+
+  def call(env)
+    if env["HTTP_X_FORWARDED_HOST"]
+      env["HTTP_X_FORWARDED_HOST"].gsub!(/,\s?[^,]+.ac.cs$/,"")
+    end
+    @app.call(env)
+  end
+end
+
+Rails.application.config.middleware.use FixHeadersMiddleware
