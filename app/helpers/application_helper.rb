1a5cb50126469ded9ee05f9f9e0d3b6cf3b8be7f
Vincent Robert <vincent.robert@nanego.com>
2016-03-15 13:13:18 +0100

    Auto-assign background color to server model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -20,4 +20,13 @@ module ApplicationHelper
     link_to(name, '#', class: "add_fields btn btn-default", data: {id: id, fields: fields.gsub("\n", "")})
   end
 
+  def lighten_color(hex_color, amount=0.6)
+    hex_color = hex_color.gsub('#','')
+    rgb = hex_color.scan(/../).map {|color| color.hex}
+    rgb[0] = [(rgb[0].to_i + 255 * amount).round, 255].min
+    rgb[1] = [(rgb[1].to_i + 255 * amount).round, 255].min
+    rgb[2] = [(rgb[2].to_i + 255 * amount).round, 255].min
+    "#%02x%02x%02x" % rgb
+  end
+
 end
