5583949de19d875ecf12a02663d5a61ffc7f8473
Vincent Robert <vincent.robert@nanego.com>
2016-03-17 16:38:23 +0100

    Improve baie selection in server form

--- app/models/baie.rb
+++ app/models/baie.rb
@@ -3,4 +3,8 @@ class Baie < ActiveRecord::Base
   has_many :serveurs
   belongs_to :salle
 
+  def name_with_salle_and_ilot
+    "#{salle.title.present? ? "Salle #{salle.title} " : ''} #{ilot.present? ? "Ilot #{ilot}" : ''} Baie #{title.present? ? title : 'non précisée' }"
+  end
+
 end
