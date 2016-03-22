181e7c661c0dd41eb93f3c858311ab3ca08cb93f
Vincent Robert <vincent.robert@nanego.com>
2016-03-22 10:10:27 +0100

    Order baies in forms

--- app/models/baie.rb
+++ app/models/baie.rb
@@ -3,6 +3,8 @@ class Baie < ActiveRecord::Base
   has_many :serveurs
   belongs_to :salle
 
+  scope :sorted, -> {joins(:salle).order("salles.title asc", "baies.ilot asc", "baies.title asc")}
+
   def name_with_salle_and_ilot
     "#{salle.title.present? ? "Salle #{salle.title} " : ''} #{ilot.present? ? "Ilot #{ilot}" : ''} Baie #{title.present? ? title : 'non précisée' }"
   end
