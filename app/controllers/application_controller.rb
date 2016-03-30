2d0a78fa55b4a62ec11dc61e4b2b3ec8311547d0
Vincent Robert <vincent.robert@nanego.com>
2016-03-30 16:06:24 +0200

    Force authentication on all pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  # before_filter :authenticate_user!
+  before_filter :authenticate_user!
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
