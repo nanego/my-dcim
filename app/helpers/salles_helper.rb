58bbf2ffa921e086318539758c832298d8bd487b
Vincent Robert <vincent.robert@nanego.com>
2016-07-13 17:51:27 +0200

    Display small badge aside server in baie in order to show current filters

--- app/helpers/salles_helper.rb
+++ app/helpers/salles_helper.rb
@@ -1,2 +1,17 @@
 module SallesHelper
+
+  def current_filters
+    html = "<h5>"
+    if params[:cluster_id].present?
+      @cluster = Cluster.find_by_id(params[:cluster_id])
+      html << "<span>Cluster : #{@cluster.title}</span>" if @cluster.present?
+    end
+    if params[:gestion_id].present?
+      @gestion = Gestion.find_by_id(params[:gestion_id])
+      html << "<span>Gestionnaire : #{@gestion.title}</span>" if @gestion.present?
+    end
+    html << "</h5>"
+    html.html_safe
+  end
+
 end
