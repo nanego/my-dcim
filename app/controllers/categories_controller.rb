a2fc1a250ad7104a2eb22abcce6a125b6063f761
Vincent Robert <vincent.robert@nanego.com>
2016-09-06 10:23:27 +0200

    Improve Category settings page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,7 +4,7 @@ class CategoriesController < ApplicationController
   # GET /categories
   # GET /categories.json
   def index
-    @categories = Category.all
+    @categories = Category.order(:title => :asc).all
   end
 
   # GET /categories/1
