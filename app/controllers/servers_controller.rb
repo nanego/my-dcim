8ca228effe980914e2b7361e28cf30177e432507
Vincent Robert <vincent.robert@nanego.com>
2016-10-03 16:36:10 +0200

    Lets get server show page through serial number

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -140,7 +140,8 @@ class ServersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_server
-      @server = Server.friendly.find(params[:id].to_s.downcase)
+      @server = Server.where('lower(numero) = ?', params[:id].to_s.downcase).first
+      @server = Server.friendly.find(params[:id].to_s.downcase) unless @server
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
