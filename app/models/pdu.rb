41a4eb8f04acf5af560122dca440837bd242b2cc
Vincent Robert <vincent.robert@nanego.com>
2017-01-10 10:10:19 +0100

    Minor code cleanup

--- app/models/pdu.rb
+++ app/models/pdu.rb
@@ -12,18 +12,20 @@ class Pdu < ActiveRecord::Base
     line = cablename[2]
     group = cablename[1]
     create_pdu_elements(line, group)
-    group = group.to_i
-    if group.odd? #Impair
-      create_pdu_elements(line, group+1)
+
+    # Always create a second group if
+    if group.to_i.odd? #Impair
+      second_group = (group.to_i+1).to_s
     else
-      create_pdu_elements(line, group-1)
+      second_group = (group.to_i-1).to_s
     end
+    create_pdu_elements(line, second_group)
   end
 
   def create_pdu_elements(line, group)
     self.pdu_lines << PduLine.create(name: line) if self.pdu_lines.select{|l|l.name == line}.blank?
     pdu_line = self.pdu_lines.select{|l|l.name == line}.first
-    pdu_line.pdu_outlet_groups << PduOutletGroup.create(name: group) if pdu_line.pdu_outlet_groups.select{|l|l.name == group}.blank?
+    pdu_line.pdu_outlet_groups << PduOutletGroup.create(name: group) if pdu_line.pdu_outlet_groups.select{|l|l.name == group.to_s}.blank?
   end
 
 end
