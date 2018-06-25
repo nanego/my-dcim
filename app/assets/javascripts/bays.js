386c3e90daf77935b09bf7938015424c92acb544
Vincent Robert <vincent.robert@nanego.com>
2018-06-25 11:26:22 +0200

    Avoid some bugs with turbolink

--- app/assets/javascripts/bays.js
+++ app/assets/javascripts/bays.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $('.frames').on('click', '.couple', function(event){
+    $(document).on('click', '.frames .couple', function(event){
         window.location.href = event.target.getAttribute('data-url')
     });
 });
