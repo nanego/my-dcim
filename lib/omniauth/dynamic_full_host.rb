40b527722516efe347ac615b036e5ca536bdb2b5
Vincent Robert <vincent.robert@nanego.com>
2018-07-04 11:13:50 +0200

    Add test to omniauth dynamic_full_host patch

--- lib/omniauth/dynamic_full_host.rb
+++ lib/omniauth/dynamic_full_host.rb
@@ -1,11 +1,10 @@
 # configures public url for our application
 OmniAuth.config.full_host = Proc.new do |env|
   url = env["rack.session"]["omniauth.origin"] || env["omniauth.origin"]
-  #if no url found, fall back to config/app_config.yml addresses
+  #if no url found, fall back to config secrets
   if url.blank?
-    url = "http://#{APP_CONFIG[:domain]}"
-    #else, parse it and remove both request_uri and query_string
-  else
+    url = "http://#{Rails.application.secrets.domain_name}"
+  else #else, parse it and remove both request_uri and query_string
     uri = URI.parse(url)
     url = "#{uri.scheme}://#{uri.host}"
     url << ":#{uri.port}" unless uri.default_port == uri.port
---
+++ test/services/dynamic_full_host_test.rb
@@ -0,0 +1,46 @@
+require File.expand_path("../../test_helper", __FILE__)
+
+class DynamicFullHostTest < ActiveSupport::TestCase
+
+  def make_env(path = '/auth/test', props = {})
+    {
+        'REQUEST_METHOD' => 'GET',
+        'PATH_INFO' => path,
+        'rack.session' => {},
+        'rack.input' => StringIO.new('test=true')
+    }.merge(props)
+  end
+
+
+  def setup
+    app = lambda {|_env| [404, {}, ['Awesome']]}
+    @strategy = ExampleStrategy.new(app, {})
+  end
+
+  def test_full_host_without_specific_origin
+    @strategy.call!(make_env('/whatever', 'rack.url_scheme' => 'http', 'SERVER_NAME' => 'facebook.lame', 'QUERY_STRING' => 'code=asofibasf|asoidnasd', 'SCRIPT_NAME' => '', 'SERVER_PORT' => 80))
+    assert @strategy.full_host, 'http://example.com'
+  end
+
+  def test_full_host_with_url_origin
+    env = {"omniauth.origin" => 'http://example.com/sub_uri/origin'}
+    @strategy.call(make_env('/auth/test/callback', env))
+    assert @strategy.full_host, 'http://example.com'
+  end
+
+end
+
+class ExampleStrategy
+  include OmniAuth::Strategy
+  attr_reader :last_env
+  option :name, 'test'
+
+  def call(env)
+    options[:dup] ? super : call!(env)
+  end
+
+  def initialize(*args, &block)
+    super
+    @fail = nil
+  end
+end
