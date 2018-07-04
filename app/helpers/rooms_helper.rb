ddd9e604b5b4614117b0bbba69e2aa20b2584df7
Vincent Robert <vincent.robert@nanego.com>
2018-07-04 16:21:06 +0200

    Remove legacy and unreachable view

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,19 +1,5 @@
 module RoomsHelper
 
-  def current_filters
-    html = "<h5>"
-    if params[:cluster_id].present?
-      @cluster = Cluster.find_by_id(params[:cluster_id])
-      html << "<span>Cluster : #{@cluster.name}</span>" if @cluster.present?
-    end
-    if params[:gestion_id].present?
-      @gestion = Gestion.find_by_id(params[:gestion_id])
-      html << "<span>Gestionnaire : #{@gestion.name}</span>" if @gestion.present?
-    end
-    html << "</h5>"
-    html.html_safe
-  end
-
   def frames_sort_order(type_of_view, lane_index = 2)
     lane_index = 2 if lane_index.nil?
     if type_of_view == 'back'
