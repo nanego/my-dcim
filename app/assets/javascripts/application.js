0192be38fb30b5fb94f6ae60bc04cadd8de6419e
Vincent Robert <vincent.robert@nanego.com>
2018-07-12 16:09:08 +0200

    Re-scale server slots after resizing the window

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -39,51 +39,53 @@ $(document).on("click", ".draw_connections_link", function (event) {
 });
 
 $(document).ready(function () {
-    (function scale_large_slots(){
-        $(".very-specific-design").each(function (index) {
-            var $el = $(this);
-            var elHeight = $el.outerHeight();
-            var elWidth = $el.outerWidth();
-
-            var $wrapper = $el.closest(".scaleable-wrapper");
-            // $el.css('width', $wrapper.width());
-
-            $wrapper.resizable({
-                resize: doResize
-            });
-
-            function doResize(event, ui) {
-
-                if (elWidth > ui.size.width && ui.size.width > 100) {
-                    var scale, origin;
-
-                    scale = Math.min(
-                        ui.size.width / elWidth,
-                        ui.size.height / elHeight
-                    );
-
-                    $el.css({
-                        transform: "translate(0,0) " + "scale(" + scale + ")"
-                    });
-                } else {
-                    $el.css({
-                        transform: "translate(0,0) " + "scale(1)"
-                    });
-                }
-
-            }
-
-            var starterData = {
-                size: {
-                    width: $wrapper.width() - 5,
-                    height: $wrapper.height()
-                }
-            }
-            doResize(null, starterData);
-
-        });
-    })();
+    scale_large_slots();
     window.addEventListener('resize', function (event) {
         scale_large_slots()
-    });
+    })
 });
+
+function scale_large_slots(){
+    $(".very-specific-design").each(function (index) {
+        var $el = $(this);
+        var elHeight = $el.outerHeight();
+        var elWidth = $el.outerWidth();
+
+        var $wrapper = $el.closest(".scaleable-wrapper");
+        // $el.css('width', $wrapper.width());
+
+        $wrapper.resizable({
+            resize: doResize
+        });
+
+        function doResize(event, ui) {
+
+            if (elWidth > ui.size.width && ui.size.width > 100) {
+                var scale, origin;
+
+                scale = Math.min(
+                    ui.size.width / elWidth,
+                    ui.size.height / elHeight
+                );
+
+                $el.css({
+                    transform: "translate(0,0) " + "scale(" + scale + ")"
+                });
+            } else {
+                $el.css({
+                    transform: "translate(0,0) " + "scale(1)"
+                });
+            }
+
+        }
+
+        var starterData = {
+            size: {
+                width: $wrapper.width() - 5,
+                height: $wrapper.height()
+            }
+        }
+        doResize(null, starterData);
+
+    });
+}
