74cf17df51cf0a40f9e1c896a303a47b1035424b
Vincent Robert <vincent.robert@nanego.com>
2018-07-12 15:50:07 +0200

    Fix scaleable server when there is no wrapper

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,72 +18,72 @@
 //= require_tree .
 //= require bootstrap
 
-function hideFilters(){
-  $('.hide-filters').hide();
-  $('.show-filters').show();
-  $('.panel-body').hide();
-}
-function showFilters(){
-  $('.hide-filters').show();
-  $('.show-filters').hide();
-  $('.panel-body').show();
+function hideFilters() {
+    $('.hide-filters').hide();
+    $('.show-filters').show();
+    $('.panel-body').hide();
 }
 
-$(document).ready(function(){
-  $('.dropdown-toggle').dropdown();
+function showFilters() {
+    $('.hide-filters').show();
+    $('.show-filters').hide();
+    $('.panel-body').show();
+}
+
+$(document).ready(function () {
+    $('.dropdown-toggle').dropdown();
 });
 
-$( document ).on( "click", ".draw_connections_link", function(event) {
+$(document).on("click", ".draw_connections_link", function (event) {
     $(event.target).html('<span class="glyphicon glyphicon-option-horizontal" aria-hidden="true"></span>')
 });
 
-$(document).ready(function(){
+$(document).ready(function () {
+    (function scale_large_slots(){
+        $(".very-specific-design").each(function (index) {
+            var $el = $(this);
+            var elHeight = $el.outerHeight();
+            var elWidth = $el.outerWidth();
 
-    $(".very-specific-design").each(function( index ) {
-        console.log( index + ": " + $( this ).text() );
+            var $wrapper = $el.closest(".scaleable-wrapper");
+            // $el.css('width', $wrapper.width());
 
-        var $el = $( this );
-        var elHeight = $el.outerHeight();
-        var elWidth = $el.outerWidth();
+            $wrapper.resizable({
+                resize: doResize
+            });
 
-        var $wrapper = $el.closest(".scaleable-wrapper");
-        // $el.css('width', $wrapper.width());
+            function doResize(event, ui) {
 
-        $wrapper.resizable({
-            resize: doResize
+                if (elWidth > ui.size.width && ui.size.width > 100) {
+                    var scale, origin;
+
+                    scale = Math.min(
+                        ui.size.width / elWidth,
+                        ui.size.height / elHeight
+                    );
+
+                    $el.css({
+                        transform: "translate(0,0) " + "scale(" + scale + ")"
+                    });
+                } else {
+                    $el.css({
+                        transform: "translate(0,0) " + "scale(1)"
+                    });
+                }
+
+            }
+
+            var starterData = {
+                size: {
+                    width: $wrapper.width() - 5,
+                    height: $wrapper.height()
+                }
+            }
+            doResize(null, starterData);
+
         });
-
-        function doResize(event, ui) {
-
-            if(elWidth > ui.size.width){
-                var scale, origin;
-
-                scale = Math.min(
-                    ui.size.width / elWidth,
-                    ui.size.height / elHeight
-                );
-
-                $el.css({
-                    transform: "translate(0,0) " + "scale(" + scale + ")"
-                });
-            }else{
-                $el.css({
-                    transform: "translate(0,0) " + "scale(1)"
-                });
-            }
-
-        }
-
-        var starterData = {
-            size: {
-                width: $wrapper.width() - 5,
-                height: $wrapper.height()
-            }
-        }
-        doResize(null, starterData);
-
+    })();
+    window.addEventListener('resize', function (event) {
+        scale_large_slots()
     });
-
-
-
 });
