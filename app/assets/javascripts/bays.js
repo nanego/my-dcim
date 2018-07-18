3807f55fe9265e70711d94d5aa9ca3a7e12d622c
Vincent Robert <vincent.robert@nanego.com>
2018-07-18 17:42:35 +0200

    Fix link to couple of frames

--- app/assets/javascripts/bays.js
+++ app/assets/javascripts/bays.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
     $(document).on('click', '.frames .couple', function (event) {
-        if (event.target == this) {
+        var body = $(this).find('.col-sm-6')[0]
+        var footer = $(this).children('.panel-footer').first()[0]
+        if (event.target == this || event.target == body || event.target == footer) {
             window.location.href = $(event.target).closest('.couple').data('url')
         }
     })
@@ -10,11 +12,10 @@ $(document).ready(function () {
     $(document).on('mouseleave', '.frames .couple', function (event) {
         $(event.target).removeClass('hover')
     })
-    $(document).on('mouseenter', '.frames .couple .panel-body, .frames .couple .panel-footer', function (event) {
+    $(document).on('mouseenter', '.frames .couple .frame', function (event) {
         $(event.target).closest('.couple').removeClass('hover')
     })
-    $(document).on('mouseleave', '.frames .couple .panel-body, .frames .couple .panel-footer', function (event) {
+    $(document).on('mouseleave', '.frames .couple .frame', function (event) {
         $(event.target).closest('.couple').addClass('hover')
     })
-
 });
