d219fb726b3bfd7898bfb64a137d498c61aaf28e
Vincent Robert <vincent.robert@nanego.com>
2018-07-18 16:04:24 +0200

    Add hover effect on couple bay link

--- app/assets/javascripts/bays.js
+++ app/assets/javascripts/bays.js
@@ -3,5 +3,18 @@ $(document).ready(function () {
         if (event.target == this) {
             window.location.href = $(event.target).closest('.couple').data('url')
         }
-    });
+    })
+    $(document).on('mouseenter', '.frames .couple', function (event) {
+        $(event.target).addClass('hover')
+    })
+    $(document).on('mouseleave', '.frames .couple', function (event) {
+        $(event.target).removeClass('hover')
+    })
+    $(document).on('mouseenter', '.frames .couple .panel-body, .frames .couple .panel-footer', function (event) {
+        $(event.target).closest('.couple').removeClass('hover')
+    })
+    $(document).on('mouseleave', '.frames .couple .panel-body, .frames .couple .panel-footer', function (event) {
+        $(event.target).closest('.couple').addClass('hover')
+    })
+
 });
