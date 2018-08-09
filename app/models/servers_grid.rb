066fc098c668eaa3879b6753c93bca34da72c5e9
Vincent Robert <vincent.robert@nanego.com>
2018-08-09 11:11:19 +0200

    Allow multiple values in servers searches

--- app/models/servers_grid.rb
+++ app/models/servers_grid.rb
@@ -18,30 +18,30 @@ class ServersGrid
   filter :name do |value|
     where("servers.name LIKE ?", value)
   end
-  filter 'Catégorie', :enum, :select => Category.sorted.where(published: true).map {|r| [r.to_s, r.id]}  do |value|
+  filter 'Catégorie', :enum, multiple: true, :select => Category.sorted.where(published: true).map {|r| [r.to_s, r.id]} do |value|
     joins(:modele).where("modeles.category_id = ?", value)
   end
   filter :nb_elts, :integer do |value|
     joins(:modele).where("modeles.nb_elts = ?", value)
   end
-  filter :architecture, :enum, :select => Architecture.sorted.where(published: true).map {|r| [r.to_s, r.id]} do |value|
+  filter :architecture, :enum, multiple: true, checkboxes: true, :select => Architecture.sorted.where(published: true).map {|r| [r.to_s, r.id]} do |value|
     joins(:modele).where("modeles.architecture_id = ?", value)
   end
   filter :u, :integer do |value|
     joins(:modele).where("modeles.u = ?", value)
   end
-  filter :manufacturer, :enum, :select => Manufacturer.sorted.where(published: true).map {|r| [r.to_s, r.id]} do |value|
+  filter :manufacturer, :enum, multiple: true, :select => Manufacturer.sorted.where(published: true).map {|r| [r.to_s, r.id]} do |value|
     joins(:modele).where("modeles.manufacturer_id = ?", value)
   end
-  filter(:modele, :enum, :select => Modele.all_sorted.map {|r| [r.name_with_brand, r.id]})
+  filter(:modele, :enum, multiple: true, :select => Modele.all_sorted.map {|r| [r.name_with_brand, r.id]})
   filter(:numero, :string)
   filter(:position, :integer)
   filter :conso, :integer, :range => true #, :default => proc { [Server.minimum(:conso), Server.maximum(:conso)] }
-  filter(:cluster, :enum, :select => Cluster.sorted.map {|r| [r.to_s, r.id]})
+  filter(:cluster, :enum, multiple: true, :select => Cluster.sorted.map {|r| [r.to_s, r.id]})
   filter(:critique, :xboolean)
-  filter(:domaine, :enum, :select => Domaine.sorted.where(published: true).map {|r| [r.to_s, r.id]})
-  filter(:gestion, :enum, :select => Gestion.sorted.where(published: true).map {|r| [r.to_s, r.id]})
-  filter(:frame, :enum, :select => Frame.all_sorted.map {|b| [b.name_with_room_and_islet, b.id]})
+  filter(:domaine, :enum, multiple: true, :select => Domaine.sorted.where(published: true).map {|r| [r.to_s, r.id]})
+  filter(:gestion, :enum, multiple: true, :select => Gestion.sorted.where(published: true).map {|r| [r.to_s, r.id]})
+  filter(:frame, :enum, multiple: true, :select => Frame.all_sorted.map {|b| [b.name_with_room_and_islet, b.id]})
 
   filter(:condition, :dynamic, :header => "Condition dynamique")
 
@@ -70,32 +70,32 @@ class ServersGrid
       end
     end
   end
-  column(:type, :order => proc { |scope|
+  column(:type, :order => proc {|scope|
     scope.joins(:modele => :category).order("categories.name")
   }) do |record|
     record.modele.try(:category)
   end
-  column(:nb_elts, :order => proc { |scope|
+  column(:nb_elts, :order => proc {|scope|
     scope.joins(:modele).order("nb_elts")
   }) do |record|
     record.modele.try(:nb_elts)
   end
-  column(:architecture, :order => proc { |scope|
+  column(:architecture, :order => proc {|scope|
     scope.joins(:modele => :architecture).order("architectures.name")
   }) do |record|
     record.modele.try(:architecture)
   end
-  column(:u, :order => proc { |scope|
+  column(:u, :order => proc {|scope|
     scope.joins(:modele).order("u")
   }) do |record|
     record.modele.try(:u)
   end
-  column(:manufacturer, :order => proc { |scope|
+  column(:manufacturer, :order => proc {|scope|
     scope.joins(:modele => :manufacturer).order("manufacturers.name")
   }) do |record|
     record.modele.try(:manufacturer)
   end
-  column(:modele, :order => proc { |scope|
+  column(:modele, :order => proc {|scope|
     scope.joins(:modele).order("modeles.name")
   }) do |record|
     record.modele
@@ -106,22 +106,22 @@ class ServersGrid
   column(:critique, :mandatory => false) do
     critique ? "Oui" : "Non"
   end
-  column(:cluster, :order => proc { |scope|
+  column(:cluster, :order => proc {|scope|
     scope.joins(:cluster).order("clusters.name")
   }) do |record|
     record.cluster
   end
-  column(:domaine, :order => proc { |scope|
+  column(:domaine, :order => proc {|scope|
     scope.joins(:domaine).order("domaines.name")
   }) do |record|
     record.domaine
   end
-  column(:gestionnaire, :order => proc { |scope|
+  column(:gestionnaire, :order => proc {|scope|
     scope.joins(:gestion).order("gestions.name")
   }) do |record|
     record.gestion
   end
-  column(:frame, :order => proc { |scope|
+  column(:frame, :order => proc {|scope|
     scope.joins(:frames).order("frames.name")
   }) do |record|
     record.frame.try(:name_with_room_and_islet)
@@ -134,7 +134,7 @@ class ServersGrid
   column(:action_conf_reseau)
 
   column("Boutons", :html => true, :mandatory => false) do |record|
-    link_to('Modifier', edit_server_path(record.id)).to_s + '<span style="margin-left:10px">'.html_safe + link_to('Supprimer', server_path(record.id), method: :delete, data: { confirm: 'Are you sure?' }, class: 'btn btn-warning').to_s + '</span>'.html_safe
+    link_to('Modifier', edit_server_path(record.id)).to_s + '<span style="margin-left:10px">'.html_safe + link_to('Supprimer', server_path(record.id), method: :delete, data: {confirm: 'Are you sure?'}, class: 'btn btn-warning').to_s + '</span>'.html_safe
   end
 
 end
