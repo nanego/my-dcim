8d345d5b6118cf0ad73b5950dc5e5319122ccae0
Vincent Robert <vincent.robert@nanego.com>
2019-02-21 10:03:03 +0100

    Show current display mode per model

--- app/controllers/modeles_controller.rb
+++ app/controllers/modeles_controller.rb
@@ -5,7 +5,7 @@ class ModelesController < ApplicationController
   before_action :set_modele, only: [:show, :edit, :update, :destroy]
 
   def index
-    @modeles = Modele.includes(:category).order(:name)
+    @modeles = Modele.includes(:category, :enclosures).order(:name)
     @types = @modeles.group_by { |m| m.category.name }.sort_by { |categorie, modeles| categorie.to_s }
   end
 
