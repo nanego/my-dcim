26d44dfef73d5a025b89211d8605630f75f4cbe8
Vincent Robert <vincent.robert@nanego.com>
2021-09-08 16:42:56 +0200

    Display server data from cmdb

--- test/services/glpi_client_test.rb
+++ test/services/glpi_client_test.rb
@@ -6,9 +6,9 @@ class GlpiClientTest < ActiveSupport::TestCase
   def test_load_computer_details_by_serial
     client = GlpiClient.new
     response = client.computer(serial: 'AZERTY')
-    assert response["id"], "4090"
-    assert response["serial"], "c27xmq1"
-    assert response["name"], "argoli"
+    assert response.id, 4090
+    assert response.serial, "c27xmq1"
+    assert response.name, "argoli"
   end
 
 end
