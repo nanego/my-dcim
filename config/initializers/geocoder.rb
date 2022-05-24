6783eb25a54faf8f304c395a55aa285511bf667b
Vincent Robert <vincent.robert@nanego.com>
2022-05-24 10:57:39 +0200

    Use mapbox account to geocode site location

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,13 +1,13 @@
 Geocoder.configure(
   # Geocoding options
   # timeout: 3,                 # geocoding service timeout (secs)
-  # lookup: :nominatim,         # name of geocoding service (symbol)
+  lookup: :mapbox,         # name of geocoding service (symbol)
   # ip_lookup: :ipinfo_io,      # name of IP address geocoding service (symbol)
   # language: :en,              # ISO-639 language code
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   http_proxy: Rails.env.production? ? Rails.application.credentials.http_proxy : "",            # HTTP proxy server (user:pass@host:port)
   https_proxy: Rails.env.production? ? Rails.application.credentials.http_proxy : "",           # HTTPS proxy server (user:pass@host:port)
-  # api_key: nil,               # API key for geocoding service
+  api_key: Rails.application.credentials.mapbox[:access_token],               # API key for geocoding service
   # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
 
   # Exceptions that should not be rescued by default
