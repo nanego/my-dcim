cf82082f68422f8b3e67879fdfc5c178233147c0
Nicolas Brousse <n.brousse@pantographe.studio>
2022-11-30 18:24:41 +0100

    wip: bump ruby and rails version

--- test/integration/homepage_test.rb
+++ test/integration/homepage_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class HomepageTest < Capybara::Rails::TestCase
+class HomepageTest < ActionDispatch::IntegrationTest
   test 'anonymous user visits homepage' do
     visit root_url
     assert page.has_content?("Vous devez vous connecter ou vous inscrire pour continuer.")
