df4575c8a6bb8a7fe53d9e7f3a679cc92b704fac
Francois Dumas <dumaslattaque.francois@gmail.com>
2023-01-19 12:17:37 +0100

    Fix - Tortuous optional: true

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class Connection < ApplicationRecord
+  # Fixme This optional: true is needed for time being to solve
+  # the execute_movement (model MovedConnection) which call a port change
+  # with the method connect_to_port (model Port)
+  # there were the following issue
+  # ActiveRecord::RecordNotSaved: Failed to remove the existing associated connection. The record failed to save after its foreign key was set to nil.
+  # at line 55 of the method connect_to_port
+  belongs_to :port, optional: true
   belongs_to :cable
-  belongs_to :port
 
   def paired_connection
     cable.connections.where.not(id: self.id).first
