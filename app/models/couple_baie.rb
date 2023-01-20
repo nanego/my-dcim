5ff6dcb454aafbeb1163e1b80ae07b17dfd80088
Francois Dumas <dumaslattaque.francois@gmail.com>
2023-01-20 17:46:37 +0100

    Feat - Cleaning

--- app/models/couple_baie.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class CoupleBaie < ApplicationRecord
-  include PublicActivity::Model
-  tracked owner: ->(controller, model) { controller && controller.current_user }
-
-  belongs_to :baie_one, :class_name => "Frame"
-  belongs_to :baie_two, :class_name => "Frame"
-end
