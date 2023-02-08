a25e0a5858327fcfd046f73eb9ed698d1f8d2d5d
Nicolas Brousse <n.brousse@pantographe.studio>
2023-02-08 16:00:05 +0100

    migrations

--- config/application.rb
+++ config/application.rb
@@ -10,8 +10,8 @@ require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
 require "sprockets/railtie"
@@ -33,5 +33,11 @@ module Demenagement
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.i18n.default_locale = :fr
+
+    # https://github.com/public-activity/public_activity/issues/374
+    # https://discuss.rubyonrails.org/t/cve-2022-32224-possible-rce-escalation-bug-with-serialized-columns-in-active-record/81017
+    config.active_record.yaml_column_permitted_classes = [ActiveSupport::TimeWithZone, ActiveSupport::TimeZone, Time]
   end
 end
