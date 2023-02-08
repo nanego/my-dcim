3797d39ddb8b7a7ad101f5553fb438cbf14457c5
Nicolas Brousse <n.brousse@pantographe.studio>
2023-02-08 15:53:36 +0100

    wip

--- config/application.rb
+++ config/application.rb
@@ -1,17 +1,17 @@
 # frozen_string_literal: true
 
-require_relative 'boot'
+require_relative "boot"
 
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
 require "active_job/railtie"
 require "active_record/railtie"
-# require "action_mailbox/engine"
-# require "action_text/engine"
 require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_mailbox/engine"
+require "action_text/engine"
 require "action_view/railtie"
 # require "action_cable/engine"
 require "sprockets/railtie"
@@ -30,15 +30,8 @@ module Demenagement
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
-
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-
-    config.i18n.default_locale = :fr
-
-    # https://github.com/public-activity/public_activity/issues/374
-    # https://discuss.rubyonrails.org/t/cve-2022-32224-possible-rce-escalation-bug-with-serialized-columns-in-active-record/81017
-    config.active_record.yaml_column_permitted_classes = [ActiveSupport::TimeWithZone, ActiveSupport::TimeZone, Time]
   end
 end
