2e523e48284b0a53a13b69918e878b58a211ab03
Nicolas Brousse <n.brousse@pantographe.studio>
2023-02-15 16:32:45 +0100

    fix: temporarily disable TomSelect

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,7 +17,7 @@
 //= require functions
 //= require_tree .
 //= require bootstrap
-//= require tom-select.complete.min
+// require tom-select.complete.min
 
 function hideFilters() {
     $('.hide-filters').hide()
