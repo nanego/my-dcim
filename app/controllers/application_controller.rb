f1aacca5814d2f4ae6174c8c4b04f68416812c6f
B-Rass <B-Rass@users.noreply.github.com>
2023-10-18 15:15:53 +0200

    Update ApplicationController to allow API requests (#97)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,8 @@ class ApplicationController < ActionController::Base
 
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :exception, unless: -> { request.format.json? }
+  protect_from_forgery with: :null_session, if: -> { request.format.json? }
 
   rescue_from ActiveRecord::RecordNotFound, with: :show_not_found_error
 
