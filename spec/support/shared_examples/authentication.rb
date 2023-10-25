afa87c7ace3f6ea9c6174b131fd84b04c1ea3c29
Nicolas Brousse <n.brousse@pantographe.studio>
2023-10-25 15:01:23 +0200

    Cleanup RSpec configuration

---
+++ spec/support/shared_examples/authentication.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+RSpec.shared_context "with authenticated user" do
+  let(:user) { User.create!(email: "user@example.com", password: "passwordpassword") }
+  let(:_keep_user_logged_out) { false }
+
+  context "with logged out user" do
+    let(:_keep_user_logged_out) { true }
+
+    it { expect(response).to have_http_status(:redirect) }
+    it { expect(response).to redirect_to(new_user_session_path) }
+  end
+
+  before do
+    sign_in(user) unless _keep_user_logged_out
+  end
+end
