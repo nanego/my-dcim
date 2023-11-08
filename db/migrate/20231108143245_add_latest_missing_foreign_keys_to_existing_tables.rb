03762aefb5a63a563c27eceeb6acd7b4ce09dd95
Nicolas Brousse <n.brousse@pantographe.studio>
2023-11-08 15:35:06 +0100

    wip

---
+++ db/migrate/20231108143245_add_latest_missing_foreign_keys_to_existing_tables.rb
@@ -0,0 +1,26 @@
+class AddLatestMissingForeignKeysToExistingTables < ActiveRecord::Migration[7.0]
+  def change
+    add_foreign_key :ports, :cards
+
+    add_foreign_key :moved_connections, :port_froms
+    add_foreign_key :moved_connections, :port_tos
+
+    add_foreign_key :memory_components, :servers
+
+    add_foreign_key :islets, :rooms
+
+    add_foreign_key :enclosures, :modeles
+
+    add_foreign_key :disks, :servers
+
+    add_foreign_key :connections, :ports
+
+    add_foreign_key :cards, :servers
+    add_foreign_key :cards, :composants
+
+    add_foreign_key :composants, :enclosures
+
+    add_foreign_key :servers, :frames
+    add_foreign_key :servers, :domaines
+  end
+end
