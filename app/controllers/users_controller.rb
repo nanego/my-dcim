3c57c7f671b82c2a0cf298ac1e46e37704daddd6
B_Rass <b.rasschaert@pantographe.studio>
2023-11-29 15:16:42 +0100

    Update users pages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,7 +19,7 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    if @user.update_attributes(secure_params)
+    if @user.update(secure_params)
       redirect_to users_path, :notice => "User updated."
     else
       redirect_to users_path, :alert => "Unable to update user."
