f3ca9b944a0608e9c5f7546b974285b27200bd7c
Maximilien Bausson <m.bausson@pantographe.studio>
2023-12-06 11:48:04 +0100

    Added translations for controller flashes

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,7 +28,7 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to @category, notice: 'Categorie was successfully created.' }
+        format.html { redirect_to @category, notice: t(".flashes.created") }
         format.json { render :show, status: :created, location: @category }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to @category, notice: 'Categorie was successfully updated.' }
+        format.html { redirect_to @category, notice: t(".flashes.updated") }
         format.json { render :show, status: :ok, location: @category }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class CategoriesController < ApplicationController
   def destroy
     if @category.destroy
       respond_to do |format|
-        format.html { redirect_to categories_url, notice: 'Category a bien été supprimée.' }
+        format.html { redirect_to categories_url, notice: t(".flashes.destroyed") }
         format.json { head :no_content }
       end
     else
