0b9958717a0faca7fa1199b4d36b25524a0ddf5f
B_Rass <b.rasschaert@pantographe.studio>
2024-01-10 18:11:49 +0100

    Move test/controllers/servers to spec/requests/servers + add dup. test

---
+++ spec/requests/servers_request_spec.rb
@@ -0,0 +1,264 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "/servers" do
+  fixtures :users, :servers
+
+  let(:server) { servers(:one) }
+  let(:server2) { servers(:two) }
+
+  before do
+    sign_in users(:one)
+
+    server.save
+    server2.save
+  end
+
+  describe "GET /index" do
+    before { get servers_path }
+
+    it { expect(response).to have_http_status(:success) }
+    it { expect(response).to render_template(:index) }
+    it { expect(response.body).to include(server.name) }
+    it { expect(response.body).to include(server2.name) }
+  end
+
+  describe "GET /show" do
+    before { get server_path(server) }
+
+    it { expect(response).to have_http_status(:success) }
+    it { expect(response).to render_template(:show) }
+    it { expect(response.body).to include(server.name) }
+
+    context "while using id" do
+      before { get server_path(server.id) }
+
+      it { expect(response).to have_http_status(:success) }
+    end
+
+    context "while using name" do
+      before { get server_path(server.name) }
+
+      it { expect(response).to render_template(:show) }
+    end
+
+    context "while using serial number" do
+      before { get server_path(server.numero) }
+
+      it { expect(response).to render_template(:show) }
+    end
+  end
+
+  describe "GET /new" do
+    before { get new_server_path }
+
+    it { expect(response).to have_http_status(:success) }
+    it { expect(response).to render_template(:new) }
+  end
+
+  describe "GET /duplicate" do
+    before { get duplicate_server_path(server) }
+
+    it { expect(response).to have_http_status(:success) }
+    it { expect(response).to render_template(:duplicate) }
+  end
+
+  describe "POST /create" do
+    context "with valid parameters" do
+      let(:valid_attributes) do
+        server.attributes.except(["id", "numero", "name"]).merge(numero: "new_numero", name: "NewServerName")
+      end
+
+      it "creates a new Server" do
+        expect do
+          post servers_path, params: { server: valid_attributes }
+        end.to change(Server, :count).by(1)
+      end
+
+      it "redirects to the created server" do
+        post servers_path, params: { server: valid_attributes }
+        expect(response).to redirect_to(assigns(:server))
+      end
+    end
+
+    context "with invalid parameters" do
+      let(:invalid_attributes) { { name: "" } }
+
+      it "does not create a new Server without attributes" do
+        expect { post servers_path, params: { server: {} } }
+          .to raise_error(ActionController::ParameterMissing)
+      end
+
+      it "does not create a new Server without parameters" do
+        expect { post servers_path, params: {} }
+          .to raise_error(ActionController::ParameterMissing)
+      end
+
+      it "does not create a new Server with an existing serial number" do
+        expect do
+          post servers_path, params: { server: server2.attributes.except(["id"]) }
+        end.not_to change(Server, :count)
+      end
+
+      it "does not create a new Server with invalid parameters", :aggregate_failures do
+        post servers_path, params: { server: invalid_attributes }
+        expect(response).to render_template(:new)
+      end
+    end
+  end
+
+  describe "GET /edit" do
+    before { get edit_server_path(server) }
+
+    it { expect(response).to have_http_status(:success) }
+    it { expect(response).to render_template(:edit) }
+  end
+
+  describe "PATCH /update" do
+    context "with valid parameters" do
+      let(:new_attributes) { server.attributes.except("name").merge(name: "New name") }
+
+      it :aggregate_failures do
+        patch server_path(server), params: { server: new_attributes }
+        server.reload
+        assigns(:server).reload
+
+        expect(response).to be_redirection
+        expect(response).to redirect_to(server_path(assigns(:server)))
+        expect(server.name).to eq(new_attributes[:name])
+
+        # old name should continue to work
+        get server_path("ServerName1")
+        expect(response).to have_http_status(:success)
+        expect(server).to eq(assigns(:server))
+      end
+
+      it "does update a Server with a numero which is its name", :aggregate_failures do
+        patch server_path(server), params: { server: { numero: server.name } }
+
+        expect(response).to redirect_to(server_path(server))
+        expect(assigns(:server).errors.details[:numero]).to be_empty
+      end
+
+      it "does update cards in a server", :aggregate_failures do
+        patch server_path(server), params: { server: {cards_attributes: { id:1,
+                                                                          composant_id: 1,
+                                                                          twin_card_id:2,
+                                                                          orientation:"lr-td" } } }
+
+        assigns(:server).reload
+        server.reload
+
+        expect(response).to be_redirection
+        expect(response).to redirect_to(server_path(assigns(:server)))
+
+        # Test new card
+        get server_path(server)
+        expect(response).to have_http_status(:success)
+        expect(server).to eq(assigns(:server))
+        expect(Card.find(1).twin_card_id).to eq 2
+
+        # Test twin card
+        expect(Card.find(2).twin_card_id).to eq 1
+      end
+    end
+
+    context "with invalid parameters" do
+      it "does not update a Server without attributes" do
+        expect { patch server_path(server), params: { server: {} } }
+          .to raise_error(ActionController::ParameterMissing)
+      end
+
+      it "does not update a Server without parameters" do
+        expect { patch server_path(server), params: {} }
+          .to raise_error(ActionController::ParameterMissing)
+      end
+
+      it "does not update a Server with invalid parameters", :aggregate_failures do
+        patch server_path(server), params: { server: { name: "" } }
+
+        expect(response).to render_template(:edit)
+      end
+
+      it "does not update a Server with a numero which is a server name", :aggregate_failures do
+        patch server_path(server), params: { server: { numero: server2.name } }
+        assigns(:server).reload
+
+        expect(response).to render_template(:edit)
+        expect(server.numero).to eq(assigns(:server).numero)
+        expect(assigns(:server).errors.details[:numero]).not_to be_empty
+      end
+    end
+  end
+
+  describe "DELETE /destroy" do
+    context "with a server without association" do
+      it "destroys the requested sever" do
+        expect do
+          delete server_path(server2)
+        end.to change(Server, :count).by(-1)
+      end
+
+      it "redirects to the servers list" do
+        delete server_path(server2)
+        expect(response).to redirect_to(servers_path)
+      end
+    end
+
+    context "with a server with association" do
+      it "does not destroy the requested sever" do
+        expect do
+          delete server_path(server)
+        end.not_to change(Server, :count)
+      end
+
+      it "redirects to the servers list" do
+        delete server_path(server)
+        expect(response).to redirect_to(servers_path)
+      end
+    end
+  end
+
+  describe "GET /import_csv" do
+    fixtures :server_states
+
+    before { get import_csv_servers_path }
+
+    it { expect(response).to have_http_status(:success) }
+    it { expect(response).to render_template(:import_csv) }
+  end
+
+  describe "POST /import" do
+    fixtures :server_states
+    fixtures :frames
+    fixtures :rooms
+    fixtures :servers
+
+    let(:csv) { Rack::Test::UploadedFile.new(Rails.root + "test/files/orders.csv") }
+    let(:destination_frame) { Frame.find_by_name('MyFrame2') }
+    let(:nb_of_servers_in_frame) { destination_frame.servers.count }
+    let(:frames_count) { Frame.where(name: 'orders').last.servers.count }
+
+    before { nb_of_servers_in_frame }
+
+    it :aggregate_failures do
+      expect do
+        expect do
+          expect do
+            post import_servers_path, params: { import: { file: csv,
+                                                          room_id: Room.first.id,
+                                                          server_state_id: ServerState.first.id } }
+          end.to change(Server, :count).by(26)
+        end.to change(Frame, :count)
+      end.to change(Bay, :count)
+
+      expect(response).to have_http_status(:found)
+      expect(response).to redirect_to(frame_path("orders"))
+      expect(Server.find_by_numero('1234567AS').comment).to eq "This is a comment"
+      expect(destination_frame.servers.count).to eq (nb_of_servers_in_frame + 4)
+      expect(destination_frame.servers.first.position).to eq 30
+      expect(frames_count).to eq 22
+    end
+  end
+end
