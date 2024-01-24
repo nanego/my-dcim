0b65e2a36bdb444d57e593c3030c0b2e48f638c1
Maximilien Bausson <m.bausson@pantographe.studio>
2024-01-24 17:45:53 +0100

    Added form errors view component

---
+++ app/components/form_errors_component.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class FormErrorsComponent < ApplicationComponent
+  erb_template <<~ERB
+    <div class="alert alert-danger alert-dismissable" role="alert">
+      <button type="button" class="close" data-dismiss="alert">
+        <span aria-hidden="true">&times;</span>
+        <span class="sr-only">Close</span>
+      </button>
+
+      <h4><%= t(".title", count: @object.errors.count) %></h4>
+
+      <ul>
+        <% @object.errors.full_messages.each do |msg| %>
+          <li><%= msg %></li>
+        <% end %>
+      </ul>
+    </div>
+  ERB
+
+  def initialize(object)
+    @object = object
+
+    super()
+  end
+
+  def render?
+    @object.errors.any?
+  end
+end
