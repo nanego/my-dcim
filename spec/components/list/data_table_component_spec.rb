4a0ea6e0ca655fa7651d93b807640544ff8cb5ca
B_Rass <b.rasschaert@pantographe.studio>
2024-03-27 16:17:55 +0100

    Clean Rubocop

--- spec/components/list/data_table_component_spec.rb
+++ spec/components/list/data_table_component_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe List::DataTableComponent, type: :component do
     [
       { product: "Emerald Silk Gown", price: "$875.00", sku: 124689, qty: 140, sales: "$122,500.00" },
       { product: "Mauve Cashmere Scarf", price: "$230.00", sku: 124533, qty: 83, sales: "$19,090.00" },
-      { product: "Navy Merino Wool Blazer with khaki chinos and yellow belt", price: "$445.00", sku: 124518, qty: 32, sales: "$14,240.00" }
+      { product: "Navy Merino Wool Blazer with khaki chinos and yellow belt", price: "$445.00", sku: 124518, qty: 32, sales: "$14,240.00" },
     ]
   end
 
