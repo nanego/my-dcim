47600ff1725fea7143a79d260797e636b78420e0
B_Rass <b.rasschaert@pantographe.studio>
2024-03-27 19:21:04 +0100

    wip

--- config/importmap.rb
+++ config/importmap.rb
@@ -6,10 +6,9 @@ pin "new.application", preload: true
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "stimulus-reveal-controller" # @4.1.0
-pin "bootstrap" # @4.6.2
-pin "jquery" # @3.7.1
-pin "popper.js" # @1.16.1
 pin "tom-select" # @2.3.1
+pin "bootstrap" # @5.3.3
+pin "@popperjs/core", to: "@popperjs--core.js" # @2.11.8
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin "tom-select" # @1.7.8
