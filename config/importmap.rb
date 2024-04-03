d6d2b9f7efbc91701dbb931c08c2cda81d2cb3b6
Nicolas Brousse <n.brousse@pantographe.studio>
2024-04-03 13:51:20 +0200

    wip

--- config/importmap.rb
+++ config/importmap.rb
@@ -7,8 +7,13 @@ pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
 pin "stimulus-reveal-controller" # @4.1.0
 pin "tom-select" # @2.3.1
+pin "leader-line" # @1.0.5
+pin "sortablejs" # @1.14.0
 pin "popper", to: "popper.js", preload: true
 pin "bootstrap", to: "bootstrap.js", preload: true
+pin "jquery", preload: true # @3.7.0
+pin "jquery-ujs", preload: true # @1.2.3
+# pin "jquery-ui", preload: true # @1.13.0
 
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "tom-select" # @1.7.8
+pin_all_from "app/javascript/src", under: "src", to: "src"
