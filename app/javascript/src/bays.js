ccd9895e798abf8d4fbb2e0a5822a4f285ef4168
Nicolas Brousse <n.brousse@pantographe.studio>
2024-04-03 14:47:13 +0200

    wip js

--- app/javascript/src/bays.js
+++ app/javascript/src/bays.js
@@ -1,21 +1,23 @@
-$(document).ready(function () {
-    $(document).on('click', '.frames .couple', function (event) {
-        var body = $(this).find('.col-sm-6')[0]
-        var footer = $(this).children('.panel-footer').first()[0]
-        if (event.target == this || event.target == body || event.target == footer) {
-            window.location.href = $(event.target).closest('.couple').data('url')
-        }
-    })
-    $(document).on('mouseenter', '.frames .couple', function (event) {
-        $(event.target).addClass('hover')
-    })
-    $(document).on('mouseleave', '.frames .couple', function (event) {
-        $(event.target).removeClass('hover')
-    })
-    $(document).on('mouseenter', '.frames .couple .frame', function (event) {
-        $(event.target).closest('.couple').removeClass('hover')
-    })
-    $(document).on('mouseleave', '.frames .couple .frame', function (event) {
-        $(event.target).closest('.couple').addClass('hover')
-    })
+import jQuery from "jquery"
+
+jQuery(document).ready(function () {
+  jQuery(document).on('click', '.frames .couple', function (event) {
+    var body = jQuery(this).find('.col-sm-6')[0]
+    var footer = jQuery(this).children('.panel-footer').first()[0]
+    if (event.target == this || event.target == body || event.target == footer) {
+      window.location.href = jQuery(event.target).closest('.couple').data('url')
+    }
+  })
+  jQuery(document).on('mouseenter', '.frames .couple', function (event) {
+    jQuery(event.target).addClass('hover')
+  })
+  jQuery(document).on('mouseleave', '.frames .couple', function (event) {
+    jQuery(event.target).removeClass('hover')
+  })
+  jQuery(document).on('mouseenter', '.frames .couple .frame', function (event) {
+    jQuery(event.target).closest('.couple').removeClass('hover')
+  })
+  jQuery(document).on('mouseleave', '.frames .couple .frame', function (event) {
+    jQuery(event.target).closest('.couple').addClass('hover')
+  })
 });
