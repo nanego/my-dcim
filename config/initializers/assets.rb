9c8a16d8aba32f8c564971047c02cc877b44b8a3
Nicolas Brousse <n.brousse@pantographe.studio>
2024-04-03 12:33:16 +0200

    fix js

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,4 +13,4 @@ Rails.application.config.assets.version = "1.0"
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
-Rails.application.config.assets.precompile += %w( bootstrap.js @popperjs--core.js )
+Rails.application.config.assets.precompile += %w[bootstrap.js popper.js]
