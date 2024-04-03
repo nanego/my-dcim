0dc682875145d0a48147abf76f544aeccc295494
Nicolas Brousse <n.brousse@pantographe.studio>
2024-04-03 13:51:20 +0200

    wip

--- app/javascript/new.application.js
+++ app/javascript/new.application.js
@@ -1,7 +1,16 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+
+document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+
 // import "@hotwired/turbo-rails"
 import "controllers"
 import "popper"
 import "bootstrap"
 
-document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
+import * as $ from "jquery"
+window.$ = window.jQuery = $
+
+import "jquery-ujs"
+// import "jquery-ui"
+
+import "src"
