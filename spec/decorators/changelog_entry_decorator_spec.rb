943142e2bb663c091a81ee2c3c9bee3c8b778b81
Maximilien Bausson <m.bausson@pantographe.studio>
2024-04-10 15:34:10 +0200

    Added tests for ChangelogEntryDecorator

--- spec/decorators/changelog_entry_decorator_spec.rb
+++ spec/decorators/changelog_entry_decorator_spec.rb
@@ -2,16 +2,52 @@
 
 require 'rails_helper'
 
-# Example:
-#
-# describe ChangelogEntryDecorator, type: :decorator do
-#   let(:object) { User.new(first_name: "John", last_name: "Doe") }
-#   let(:decorated_user) { described_class.new(object) }
-#
-#   describe "#full_name" do
-#     it { expect(decorated_user.full_name).to eq("John Doe") }
-#   end
-# end
 RSpec.describe ChangelogEntryDecorator, type: :decorator do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:object) { ChangelogEntry.new(object_type: "colors", object_changed_attributes: { id: [nil, 42] }) }
+  let(:decorated_changelog_entry) { described_class.decorate(object) }
+
+  describe '#object_pre_change_attributes' do
+    it { expect(decorated_changelog_entry.object_pre_change_attributes).to eq({ "id" => nil }) }
+  end
+
+  describe '#object_pre_change_attributes_to_json' do
+    it { expect(decorated_changelog_entry.object_pre_change_attributes_to_json).to include("id", "null") }
+  end
+
+  describe '#object_post_change_attributes' do
+    it { expect(decorated_changelog_entry.object_post_change_attributes).to eq({ "id" => 42 }) }
+  end
+
+  describe "#object_post_change_attributes_to_json" do
+    it { expect(decorated_changelog_entry.object_post_change_attributes_to_json).to include("id", "42") }
+  end
+
+  describe "#split_diff" do
+    it { expect(decorated_changelog_entry.split_diff.left).to include("null") }
+    it { expect(decorated_changelog_entry.split_diff.right).to include("42") }
+  end
+
+  describe "#object_changed_attributes_to_sentence" do
+    it { expect(decorated_changelog_entry.object_changed_attributes_to_sentence).to include("Id") }
+  end
+
+  describe "#action_label_to_component" do
+    expected_types = {
+      create: :success,
+      update: :warning,
+      destroy: :danger,
+      unknown: :default
+    }
+
+    let(:object) { ChangelogEntry.new(params) }
+    let(:params) { {} }
+
+    expected_types.each do |action, expected_type|
+      context "with #{action}" do
+        let(:params) { { action: action } }
+
+        it { expect(decorated_changelog_entry.action_label_to_component.instance_variable_get(:@type)).to eq(expected_type) }
+      end
+    end
+  end
 end
