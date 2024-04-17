e1ccb3bdf7655c91d3bcf67e72c0782c3a3dcfce
Nicolas Brousse <n.brousse@pantographe.studio>
2024-04-17 14:12:28 +0200

    wip

--- app/processors/servers_processor.rb
+++ app/processors/servers_processor.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 class ServersProcessor < Rubanok::Processor
-  map :name do |name:|
-    raw.where(Server.arel_table[:name].matches("%#{name}%"))
+  map :q do |q:|
+    raw.where(Server.arel_table[:name].matches("%#{q}%"))
+       .or(raw.where(Server.arel_table[:id].matches("%#{q}%")))
   end
 
   map :cluster_id do |cluster_id:|
     raw.where(cluster_id: cluster_id)
+  end
+
+  map :bay_id do |bay_id:|
+    raw.where(bay_id: bay_id) # TODO
   end
 end
