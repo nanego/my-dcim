8c30c3535b22ea989363e7a2080f7db7e3381d50
B_Rass <b.rasschaert@pantographe.studio>
2024-04-24 12:45:20 +0200

    Update connection index

--- app/models/connection.rb
+++ app/models/connection.rb
@@ -6,6 +6,11 @@ class Connection < ApplicationRecord
   belongs_to :port, touch: true
   belongs_to :cable, touch: true, counter_cache: true
 
+  has_one :card, through: :port
+  has_one :server, through: :port
+  has_one :card_type, through: :card
+  has_one :port_type, through: :card_type
+
   def paired_connection
     cable.connections.where.not(id: self.id).first
   end
