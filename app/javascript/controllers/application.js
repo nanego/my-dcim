6dd00755b8c5eae0c89b68d4874c8bce5c17cae2
Nicolas Brousse <n.brousse@pantographe.studio>
2024-05-23 17:20:36 +0200

    cleanup

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,8 +1,6 @@
 import { Application } from "@hotwired/stimulus"
-import Reveal from "stimulus-reveal-controller"
 
 const application = Application.start()
-application.register('reveal', Reveal)
 
 // Configure Stimulus development experience
 application.debug = false
