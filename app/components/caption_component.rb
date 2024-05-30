81589b68a6dde65076e797e4ee0cae44abe25742
B_Rass <b.rasschaert@pantographe.studio>
2024-05-30 15:47:56 +0200

    Debug CaptionComponent

--- app/components/caption_component.rb
+++ app/components/caption_component.rb
@@ -3,8 +3,8 @@
 class CaptionComponent < ApplicationComponent
   erb_template <<~ERB
     <span class="caption-component">
-      <span class="caption-component-button bi bi-info-circle" aria-hidden="true"></span>
-      <span class="caption-component-content"><%= content %></span>
+      <span class="caption-component-button bi bi-info-circle-fill text-body-tertiary" aria-hidden="true"></span>
+      <span class="caption-component-content bg-body-secondary border rounded"><%= content %></span>
     </span>
   ERB
 end
