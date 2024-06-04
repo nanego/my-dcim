d218a8b6629bcf515a945a410ef1bf18fefdeb31
B_Rass <b.rasschaert@pantographe.studio>
2024-06-04 17:49:14 +0200

    Raw fix of infrastructure to make LeaderLine works with scroll

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,10 +9,11 @@ pin "stimulus-reveal-controller" # @4.1.0
 pin "tom-select" # @2.3.1
 pin "leader-line" # @1.0.5
 pin "sortablejs" # @1.14.0
+pin "anim-event" # @1.0.17
 pin "popper", to: "popper.js", preload: true
 pin "bootstrap", to: "bootstrap.js", preload: true
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
+pin "@stimulus-components/reveal", to: "@stimulus-components--reveal.js" # @5.0.0
 
 pin_all_from "app/javascript/controllers", under: "controllers"
 pin_all_from "app/javascript/src", under: "src", to: "src"
-pin "@stimulus-components/reveal", to: "@stimulus-components--reveal.js" # @5.0.0
