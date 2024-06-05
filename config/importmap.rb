8bc2818235ddcc5dbe94d9b509460daa6bc0376d
Nicolas Brousse <n.brousse@pantographe.studio>
2024-06-05 17:34:27 +0200

    style: code cleanup

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,16 +2,16 @@
 
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
-pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
-pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true
+pin "application"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin "stimulus-reveal-controller" # @4.1.0
 pin "tom-select" # @2.3.1
 pin "leader-line" # @1.0.5
 pin "sortablejs" # @1.14.0
 pin "anim-event" # @1.0.17
-pin "popper", to: "popper.js", preload: true
-pin "bootstrap", to: "bootstrap.js", preload: true
+pin "popper", to: "popper.js"
+pin "bootstrap", to: "bootstrap.js"
 pin "@rails/ujs", to: "@rails--ujs.js" # @7.1.3
 pin "@stimulus-components/reveal", to: "@stimulus-components--reveal.js" # @5.0.0
 
