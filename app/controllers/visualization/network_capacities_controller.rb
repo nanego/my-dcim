ff49c116019c3e951b1588ea795261969cdca714
Nicolas Brousse <n.brousse@pantographe.studio>
2024-06-19 17:52:40 +0200

    wip

--- app/controllers/visualization/network_capacities_controller.rb
+++ app/controllers/visualization/network_capacities_controller.rb
@@ -1,6 +1,8 @@
 module Visualization
   class InfrastructuresController < ApplicationController
     def show
+      @sites = Site.joins(:rooms).includes(rooms: { islets: { bays: :frames } })
+                   .order(:position).distinct
     end
   end
 end
