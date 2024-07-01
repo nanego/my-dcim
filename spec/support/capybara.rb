3fdc50c29b6f74776ac67180e13cfa30add276dd
Maximilien Bausson <m.bausson@pantographe.studio>
2024-07-01 17:42:07 +0200

    wip

---
+++ spec/support/capybara.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "capybara/cuprite"
+
+Capybara.register_driver(:cuprite) do |app|
+  options = {
+    window_size: [1920, 1080],
+    browser_options: {},
+    inspector: false,
+    js_errors: true
+  }
+
+  options[:headless] = true
+  options[:browser_options][:'no-sandbox'] = nil if ENV["CI"]
+
+  Capybara::Cuprite::Driver.new(app, options)
+end
+
+# NOTE: Wait for AJAX response since turbo make SHR requests.
+Capybara.default_max_wait_time = 5
+Capybara.javascript_driver = :cuprite
+Capybara.server = :puma, { Silent: true }
