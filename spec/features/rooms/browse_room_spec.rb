3ef8745f04718c2e26930a54266b06d2e3d61757
Maximilien Bausson <m.bausson@pantographe.studio>
2024-07-02 14:40:46 +0200

    wip

--- spec/features/rooms/browse_room_spec.rb
+++ spec/features/rooms/browse_room_spec.rb
@@ -3,13 +3,12 @@
 require "rails_helper"
 
 RSpec.describe "Rooms::BrowseRoom", :js do
-  let(:user) { users(:one) }
   let(:room) { rooms(:one) }
 
   it("browse a room and its islets and bays") do
-    sign_in(user)
+    sign_in(users(:one))
 
-    visit room_path(room)
-    expect(page).to have_current_path(room_path(room))
+    visit room_path(room, 'islet-id': room.islets.first, 'bay-id': room.bays.first, 'frame-id': room.frames.first, view: "front")
+    expect(page).to have_current_path(room_path(room, 'islet-id': room.islets.first, 'bay-id': room.bays.first, 'frame-id': room.frames.first, view: "front"))
   end
 end
