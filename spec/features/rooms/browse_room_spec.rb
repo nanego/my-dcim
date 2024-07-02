6c60c8f818c59dc6958942f5b2cd90e64267cc82
Maximilien Bausson <m.bausson@pantographe.studio>
2024-07-02 11:43:19 +0200

    wip

--- spec/features/rooms/browse_room_spec.rb
+++ spec/features/rooms/browse_room_spec.rb
@@ -6,22 +6,10 @@ RSpec.describe "Rooms::BrowseRoom", :js do
   let(:user) { users(:one) }
   let(:room) { rooms(:one) }
 
-  it("browse a room and its islets and bays") do # rubocop:disable RSpec/MultipleExpectations
-    # First, sign in the user
-    visit new_user_session_path
-    expect(page).to have_current_path(new_user_session_path)
-
-    within("form") do
-      fill_form(:user, :new, {
-        email: user.email,
-        password: "passwordpassword"
-      })
-
-      click_on submit
-    end
+  it("browse a room and its islets and bays") do
+    sign_in(user)
 
     visit room_path(room)
-
     expect(page).to have_current_path(room_path(room))
   end
 end
