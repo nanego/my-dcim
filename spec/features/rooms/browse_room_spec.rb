2e85d65eb1781a1a5df82f5334bf167e78233ef6
Maximilien Bausson <m.bausson@pantographe.studio>
2024-07-03 11:09:33 +0200

    Added final steps to the test

--- spec/features/rooms/browse_room_spec.rb
+++ spec/features/rooms/browse_room_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe "Rooms::BrowseRoom", :js do
     room.islets.first.bays.last.destroy!
   end
 
-  it("browse a room and its islets and bays") do
+  it("browse a room, islets and bays, change view and background color") do # rubocop:disable RSpec/MultipleExpectations
     sign_in(users(:one))
 
     visit room_path(room)
@@ -18,6 +18,7 @@ RSpec.describe "Rooms::BrowseRoom", :js do
 
     frame = room.frames.first
 
+    # 1. Select a frame
     overview_frame = find("div[data-frame-id=#{frame.slug}]")
     overview_frame.click
 
@@ -25,45 +26,48 @@ RSpec.describe "Rooms::BrowseRoom", :js do
     overview_container = find("div##{room.name.downcase}")
 
     frame.servers.each do |server|
-      expect(overview_container.text).to include(server.name)
+      expect(page).to have_content(server.name)
     end
 
-    # Change view to 'back'
+    # 2. Change view to 'back'
     switch_view_btn = find("span.pull-right > a.btn.btn-success")
     switch_view_btn.click
 
     # Since the overview container is already loaded, we cannot use find() to wait for loading
-    sleep 1.5
+    sleep 1
 
     # On the back view, we should see some server cards (only the 2nd server has cards)
     frame.servers.second.cards.pluck(:name).each do |card_name|
-      expect(overview_container.text).to include(card_name)
+      expect(page).to have_content(card_name)
     end
 
-    # Background color
+    # Modele background color
     overview_frame_title = find("div.title > a[data-frame-id=#{frame.slug}]")
     overview_frame_title.click
 
-    sleep 1.5
+    sleep 1
 
     # For background color set to 'modele', we check for a cyan/white background-color
-    colored_lines = all("li.server.mystring")
+    colored_lines = all("ul.servers > li.server.mystring")
 
     colored_lines.each do |line|
       expect(line.style("background-color")["background-color"]).to eq("rgb(183, 247, 255)").or eq("rgb(255, 255, 255)")
     end
 
-    # Change background color to 'gestionnaire'
-    bg_dropdown_button = first("button.dropdown-toggle")
+    # 3. Change background color to 'gestionnaire'
+    bg_dropdown_button = first("span.pull-right > * > .btn-default.dropdown-toggle")
     bg_dropdown_button.click
 
-    # binding.b
+    bg_dropdown_gestionnaire = all(".dropdown-menu > li")[1]
+    bg_dropdown_gestionnaire.click
 
-    # TODO: does not find anything
-    # bg_dropdown_gestionnaire = all("button.dropdown-toggle li")[1]
-    # bg_dropdown_gestionnaire.click
+    sleep 1
 
-    # $stdin.gets
-    # binding.b
+    # For background color set to 'gestionnaire', we check for a green/white background-color
+    colored_lines = all("ul.servers > li.server.mystring")
+
+    colored_lines.each do |line|
+      expect(line.style("background-color")["background-color"]).to eq("rgb(118, 255, 160)").or eq("rgb(255, 255, 255)")
+    end
   end
 end
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -6,7 +6,8 @@ Capybara.register_driver(:cuprite) do |app|
   options = {
     window_size: [1920, 1080],
     inspector: false,
-    js_errors: true
+    js_errors: true,
+    timeout: 10
   }
 
   options[:headless] = true
@@ -16,6 +17,6 @@ Capybara.register_driver(:cuprite) do |app|
 end
 
 # NOTE: Wait for AJAX response since turbo make SHR requests.
-Capybara.default_max_wait_time = 10
+Capybara.default_max_wait_time = 5
 Capybara.javascript_driver = :cuprite
 Capybara.server = :puma, { Silent: true }
