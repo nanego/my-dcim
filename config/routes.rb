665946f54ff8e8f9f033da8efffb448451872e26
Vincent Robert <vincent.robert@nanego.com>
2024-07-10 09:43:35 +0200

    Remove unnecessary route and view files

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :air_conditioners
+  resources :air_conditioners, except: %i[show]
   resources :documents
   resources :moves do
     member do
