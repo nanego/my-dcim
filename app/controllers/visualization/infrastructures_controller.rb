84a07963d36d1b53e17a7832d726a2eeee3be018
Nicolas Brousse <n.brousse@pantographe.studio>
2024-07-17 16:42:55 +0200

    refactor: update visualization, apply default value on filters

--- app/controllers/visualization/infrastructures_controller.rb
+++ app/controllers/visualization/infrastructures_controller.rb
@@ -3,7 +3,12 @@
 module Visualization
   class InfrastructuresController < ApplicationController
     def show
-      @filter = Filter.new(params, %i[network_type islet_id stack_ids])
+      @filter = Filter.new(params, %i[network_type islet_id])
+
+      unless @filter.filled?
+        @filter.islet_id = Islet.sorted.not_empty.has_name.distinct.first.id
+        @filter.network_type = :gbe
+      end
 
       @islet = Islet.find(@filter.islet_id) if @filter.filled?
     end
--- app/controllers/visualization/network_capacities_controller.rb
+++ app/controllers/visualization/network_capacities_controller.rb
@@ -5,6 +5,11 @@ module Visualization
     def show
       @filter = Filter.new(params, %i[network_type islet_id])
 
+      unless @filter.filled?
+        @filter.islet_id = Islet.sorted.not_empty.has_name.distinct.first.id
+        @filter.network_type = :gbe
+      end
+
       @islet = Islet.find(@filter.islet_id) if @filter.filled?
     end
   end
