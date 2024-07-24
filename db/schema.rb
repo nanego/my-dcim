dbce81ee68d509f58ebac0f9ad00d7da998e921c
B_Rass <b.rasschaert@pantographe.studio>
2024-07-24 11:58:12 +0200

    Clean after rebase

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_07_09_153016) do
+ActiveRecord::Schema[7.1].define(version: 2024_07_09_134050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
