c33b97790ae7911cdda42c1ed0ba5913d181b48e
B_Rass <b.rasschaert@pantographe.studio>
2024-08-07 17:28:46 +0200

    Re-add air_conditionner#show route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  resources :air_conditioners, except: %i[show]
+  resources :air_conditioners
   resources :documents
   resources :moves do
     member do
