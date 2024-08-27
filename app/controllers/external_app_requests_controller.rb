70ccdcc84584aba073fa6672b704f0e42f9a1f9d
Vincent Robert <vincent.robert@nanego.com>
2024-08-27 14:08:01 +0200

    Show in-progress sync status to all users and enable cancellation

--- app/controllers/external_app_requests_controller.rb
+++ app/controllers/external_app_requests_controller.rb
@@ -6,4 +6,11 @@ class ExternalAppRequestsController < ApplicationController
                    progress: @external_app_request.progress }
   end
 
+  def destroy
+    @external_app_request = ExternalAppRequest.find(params[:id])
+    @external_app_request.destroy
+
+    redirect_to external_app_records_path
+  end
+
 end
