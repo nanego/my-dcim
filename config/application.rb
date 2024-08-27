1aa6dcd9e7fe5996ac8e85696690a1744192ff02
Vincent Robert <vincent.robert@nanego.com>
2024-08-27 11:34:46 +0200

    Set the timezone configuration

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,7 @@ module Demenagement
     # in config/environments, which are processed later.
     #
     # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Europe/Paris'
     # config.eager_load_paths << Rails.root.join("extras")
 
     config.i18n.default_locale = :fr
