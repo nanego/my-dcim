f18d53d0d4be7b194e44d6777d0ef65b49f563c8
Maximilien Bausson <m.bausson@pantographe.studio>
2024-09-12 12:29:11 +0200

    Improved helper

--- app/helpers/changelog_entries_helper.rb
+++ app/helpers/changelog_entries_helper.rb
@@ -2,12 +2,16 @@
 
 module ChangelogEntriesHelper
   def object_changelog_entries(object, **)
-    turbo_frame_tag "changelog-entries", loading: :lazy, src: object_changelog_entries_path(object), **
+    turbo_frame_tag "changelog-entries", loading: :lazy, src: object_changelog_entries_path(model_name(object), object.id), ** do
+      tag.div class: "d-flex justify-content-center w-100" do
+        tag.span class: "spinner-border spinner-border-sm p-4"
+      end
+    end
   end
 
   private
 
-  def object_changelog_entries_path(object)
-    "/#{object.class.name.underscore.pluralize}/#{object.id}/changelog_entries"
+  def model_name(object)
+    object.model_name.plural
   end
 end
