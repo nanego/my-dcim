113ac3314affed8c6c8a930616e474e0bf14d476
Maximilien Bausson <m.bausson@pantographe.studio>
2024-09-12 12:40:22 +0200

    wip

--- app/helpers/changelog_entries_helper.rb
+++ app/helpers/changelog_entries_helper.rb
@@ -2,16 +2,17 @@
 
 module ChangelogEntriesHelper
   def object_changelog_entries(object, **)
-    turbo_frame_tag "changelog-entries", loading: :lazy, src: object_changelog_entries_path(model_name(object), object.id), ** do
-      tag.div class: "d-flex justify-content-center w-100" do
-        tag.span class: "spinner-border spinner-border-sm p-4"
+    turbo_frame_tag "changelog-entries", loading: :lazy, src: object_changelog_path(object), ** do
+      tag.div class: "w-100 d-flex justify-content-center align-items-center gap-3" do
+        concat("Loading changelog...")
+        concat(tag.span(class: "spinner-border spinner-border-sm p-4"))
       end
     end
   end
 
   private
 
-  def model_name(object)
-    object.model_name.plural
+  def object_changelog_path(object)
+    object_changelog_entries_path(object.model_name.plural, object.id)
   end
 end
