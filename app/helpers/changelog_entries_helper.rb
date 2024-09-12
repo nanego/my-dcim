ee738efb67d50a90641bfe8710bec49b74b50210
Maximilien Bausson <m.bausson@pantographe.studio>
2024-09-12 10:25:54 +0200

    wip

--- app/helpers/changelog_entries_helper.rb
+++ app/helpers/changelog_entries_helper.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 module ChangelogEntriesHelper
+  def object_changelog_entries(object, **)
+    tag.turbo_frame(id: "changelog-entries", loading: :lazy, src: object_changelog_entries_path(object), **)
+  end
+
+  private
+
   def object_changelog_entries_path(object)
     "/#{object.class.name.underscore.pluralize}/#{object.id}/changelog_entries"
   end
