e20dd26067246e70ee19d6454a3d644480eeab80
B_Rass <b.rasschaert@pantographe.studio>
2024-09-19 18:39:34 +0200

    Updating Settings#edit page + Registrations#edit page

--- app/components/page/heading_component.rb
+++ app/components/page/heading_component.rb
@@ -2,7 +2,7 @@
 
 module Page
   class HeadingComponent < ApplicationComponent
-    STYLES = %i[show create edit].freeze
+    BUTTONS = %i[show create edit none].freeze
 
     renders_one :extra_buttons
 
@@ -10,16 +10,20 @@ module Page
       <%= render "layouts/breadcrumb", breadcrumb_steps: @breadcrumb_steps %>
       <div class="col-12 bg-body pb-5 pt-4 px-4">
         <div class="d-flex justify-content-between align-items-center">
-          <span class="<%= class_names("flex-grow-1": @style == :create) %>">
-            <%= link_to polymorphic_url(@resource.class),
-                        class: "btn d-inline-flex",
-                        title: t("action.back") do %>
-              <span class="bi bi-chevron-left"></span>
-              <span class="d-none d-md-inline-block ms-1"><%= t("action.back") %></span>
-            <% end %>
-          </span>
+          <% if @buttons == :none %>
+            <span class="flex-grow-1"></span>
+          <% else %>
+            <span class="<%= class_names("flex-grow-1": @buttons == :create) %>">
+              <%= link_to polymorphic_url(@resource.class),
+                          class: "btn d-inline-flex",
+                          title: t("action.back") do %>
+                <span class="bi bi-chevron-left"></span>
+                <span class="d-none d-md-inline-block ms-1"><%= t("action.back") %></span>
+              <% end %>
+            </span>
+          <% end %>
           <h1 class="text-center px-4"><%= @title %></h1>
-          <% if @style == :create %>
+          <% if %i[create none].include? @buttons %>
             <span class="flex-grow-1"></span>
           <% else %>
             <div class="align-self-center d-inline-flex">
@@ -27,7 +31,7 @@ module Page
                 <%= extra_buttons %>
               <% end %>
 
-              <% if @style == :show %>
+              <% if @buttons == :show %>
                 <%= link_to [:edit, @resource], class: "btn btn-info d-inline-flex", title: t("action.edit") do %>
                   <span class="bi bi-pencil"></span>
                   <span class="d-none d-xl-inline-block ms-1"><%= t("action.edit") %></span>
@@ -46,13 +50,13 @@ module Page
       </div>
     ERB
 
-    def initialize(resource:, title:, breadcrumb_steps:, style: :show)
+    def initialize(resource:, title:, breadcrumb_steps:, buttons: :show)
       @resource = resource
       @title = title
       @breadcrumb_steps = breadcrumb_steps
 
-      @style = style.to_sym
-      raise ArgumentError, "'#{@style.inspect}' is not a valid type" unless STYLES.include?(@style)
+      @buttons = buttons.to_sym
+      raise ArgumentError, "'#{@button.inspect}' is not a valid buttons type" unless BUTTONS.include?(@buttons)
 
       super
     end
