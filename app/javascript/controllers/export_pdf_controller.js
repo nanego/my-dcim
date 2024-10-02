36575e873900c2b35eaec70b33793b3e6fee0379
B_Rass <b.rasschaert@pantographe.studio>
2024-10-02 11:43:57 +0200

    Improve rendering for PDF exports

--- app/javascript/controllers/export_pdf_controller.js
+++ app/javascript/controllers/export_pdf_controller.js
@@ -11,7 +11,7 @@ const exportOptions = {
   },
   pagebreak: { avoid: ".server" },
   enableLinks: false,
-  html2canvas:  { windowWidth: 1200, width: 1200 },
+  html2canvas:  { windowWidth: 1200, width: 1200, scale: 1.25 },
   jsPDF: { format: "legal" }
 }
 
