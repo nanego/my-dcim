5f6c5b83bd983157704b666854dad3ac4c821f20
B_Rass <b.rasschaert@pantographe.studio>
2024-10-03 11:59:49 +0200

    Add size to button component

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -4,21 +4,25 @@ class ButtonComponent < ApplicationComponent
   VARIANTS = %i[default primary secondary success danger warning info light dark link].freeze
   SIZES = %i[sm default lg].freeze
 
-  def initialize(title:, url:, variant: :default, icon: nil, is_responsive: false, extra_classes: "")
+  def initialize(title:, url:, variant: :default, size: :default, icon: nil, is_responsive: false, extra_classes: "")
     @title = title
     @url = url
     @variant = variant&.to_sym
+    @size = size.to_sym
     @icon = icon
     @is_responsive = is_responsive
     @extra_classes = extra_classes
 
     raise ArgumentError, "'#{@variant.inspect}' is not a valid variant type" unless VARIANTS.include?(@variant)
+    raise ArgumentError, "'#{@size.inspect}' is not a valid size type" unless SIZES.include?(@size)
 
     super
   end
 
   def call
-    link_to @url, class: "btn btn-#{@variant} align-self-center d-inline-flex #{@extra_classes}", title: @title do
+    link_to @url,
+            class: "btn btn-#{@variant} btn-#{@size} align-self-center d-inline-flex #{@extra_classes}",
+            title: @title do
       concat(tag.span class: "bi bi-#{@icon}")
       concat(tag.span @title, class: class_names("ms-1", "d-none d-md-inline-block": @is_responsive))
     end
