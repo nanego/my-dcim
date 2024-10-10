bb72d481a44546e4e56c7ca3dbde396c23b04be9
B_Rass <b.rasschaert@pantographe.studio>
2024-10-10 13:28:34 +0200

    Update Connection and Cable pages

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -4,7 +4,7 @@ class ButtonComponent < ApplicationComponent
   VARIANTS = %i[default primary secondary success danger warning info light dark link].freeze
   SIZES = %i[sm default lg].freeze
 
-  def initialize(title:, url:, variant: :default, size: :default, icon: nil, is_responsive: false, extra_classes: "")
+  def initialize(title:, url:, variant: :default, size: :default, icon: nil, is_responsive: false, extra_classes: "", **html_options)
     @title = title
     @url = url
     @variant = variant&.to_sym
@@ -12,6 +12,8 @@ class ButtonComponent < ApplicationComponent
     @icon = icon
     @is_responsive = is_responsive
     @extra_classes = extra_classes
+    @html_options = html_options
+
 
     raise ArgumentError, "'#{@variant.inspect}' is not a valid variant type" unless VARIANTS.include?(@variant)
     raise ArgumentError, "'#{@size.inspect}' is not a valid size type" unless SIZES.include?(@size)
@@ -22,7 +24,8 @@ class ButtonComponent < ApplicationComponent
   def call
     link_to @url,
             class: "btn btn-#{@variant} btn-#{@size} align-self-center d-inline-flex #{@extra_classes}",
-            title: @title do
+            title: @title,
+            **@html_options do
       concat(tag.span(class: "bi bi-#{@icon}"))
       concat(tag.span(@title, class: class_names("ms-1", 'd-none d-md-inline-block': @is_responsive)))
     end
