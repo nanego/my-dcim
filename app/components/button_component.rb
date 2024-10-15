15182d8f2935a4d05dfea88ace338d72f17ed811
B_Rass <b.rasschaert@pantographe.studio>
2024-10-15 18:55:28 +0200

    Updating HeadingComponent spec test

--- app/components/button_component.rb
+++ app/components/button_component.rb
@@ -4,7 +4,7 @@ class ButtonComponent < ApplicationComponent
   VARIANTS = %i[default primary secondary success danger warning info light dark link outline_primary].freeze
   SIZES = %i[sm default lg].freeze
 
-  def initialize(title:, url:, variant: :default, size: :default, icon: nil, is_responsive: false, extra_classes: "", **html_options)
+  def initialize(title:, url:, variant: :default, size: :default, icon: nil, is_responsive: false, extra_classes: "", **html_options) # rubocop:disable Metrics/ParameterLists
     @title = title
     @url = url
     @variant = variant&.to_sym
@@ -13,7 +13,6 @@ class ButtonComponent < ApplicationComponent
     @is_responsive = is_responsive
     @extra_classes = extra_classes
     @html_options = html_options
-
 
     raise ArgumentError, "'#{@variant.inspect}' is not a valid variant type" unless VARIANTS.include?(@variant)
     raise ArgumentError, "'#{@size.inspect}' is not a valid size type" unless SIZES.include?(@size)
