179a4b2f8666f54f1a2b1be3d2cfd6b105495df9
B-Rass <B-Rass@users.noreply.github.com>
2024-10-29 15:15:00 +0100

    341624 add status attribute to room (#15)

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -131,6 +131,6 @@ class RoomsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def room_params
-    params.require(:room).permit(:name, :description, :display_on_home_page, :position, :site_id)
+    params.require(:room).permit(:name, :description, :display_on_home_page, :position, :status, :site_id)
   end
 end
