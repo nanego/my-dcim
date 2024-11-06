42641df9ace2d8c47d9a267fad856e3d25412998
Nicolas Brousse <n.brousse@pantographe.studio>
2024-11-06 12:01:08 +0100

    fix: move server and changelog entry pagination inside frames

--- app/controllers/changelog_entries_controller.rb
+++ app/controllers/changelog_entries_controller.rb
@@ -1,6 +1,6 @@
 class ChangelogEntriesController < ApplicationController
   def index
-    @changelog_entries = sorted(changelog_scope.order(created_at: :desc))
+    @changelog_entries = sorted(changelog_scope.includes(:author, :object).order(created_at: :desc))
     @pagy, @changelog_entries = pagy(@changelog_entries)
   end
 
