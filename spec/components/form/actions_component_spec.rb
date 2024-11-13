e3581f18031a3e5d6b6482e7a29bde76f26a5510
B-Rass <B-Rass@users.noreply.github.com>
2024-11-13 10:32:03 +0100

    239 verify newedit form submit redirections (#257)

---
+++ spec/components/form/actions_component_spec.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe Form::ActionsComponent, type: :component do
+  let(:component) { described_class.new(form) }
+  let(:rendered_component) { render_inline(component, &block).to_html }
+  let(:object) { Islet.new }
+  let(:form) { form_with(object) }
+  let(:block) { nil }
+
+  before { object.validate }
+
+  context "with new object" do
+    it { expect(rendered_component).not_to be_nil }
+
+    it do # rubocop:disable RSpec/ExampleLength
+      expect(rendered_component).to have_tag("div.col-12.sticky-bottom") do
+        with_tag("a", href: "/islets", text: "Annuler")
+        with_tag("a", text: "Annuler", count: 1)
+        with_tag("input.btn-success", type: "submit", value: "Créer")
+        without_tag("input.btn-info", type: "submit", value: "Modifier")
+      end
+    end
+  end
+
+  context "with persisted object" do
+    let(:object) { islets(:one) }
+
+    it { expect(rendered_component).not_to be_nil }
+
+    it do # rubocop:disable RSpec/ExampleLength
+      expect(rendered_component).to have_tag("div.col-12.sticky-bottom") do
+        with_tag("a", href: "/islets/1", text: "Annuler")
+        with_tag("a", text: "Annuler", count: 1)
+        without_tag("input.btn-success", type: "submit", value: "Créer")
+        with_tag("input.btn-info", type: "submit", value: "Modifier")
+      end
+    end
+  end
+end
--- spec/support/view_component.rb
+++ spec/support/view_component.rb
@@ -2,10 +2,28 @@
 
 require "view_component/test_helpers"
 
+module ViewComponent
+  module FormHelpers
+    def form_with(object, options = {})
+      ActionView::Helpers::FormBuilder.new(object_name, object, template, options)
+    end
+
+    def object_name
+      object.to_model.model_name.param_key
+    end
+
+    def template
+      lookup_context = ActionView::LookupContext.new(ActionController::Base.view_paths)
+      ActionView::Base.new(lookup_context, {}, ApplicationController.new)
+    end
+  end
+end
+
 RSpec.configure do |config|
   config.include RSpecHtmlMatchers, type: :component
 
   config.include ViewComponent::TestHelpers, type: :component
+  config.include ViewComponent::FormHelpers, type: :component
 
   config.include Capybara::RSpecMatchers, type: :component
 end
