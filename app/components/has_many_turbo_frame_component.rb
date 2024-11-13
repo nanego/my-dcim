378ce07e6e76da047a12b6753c7e0b325e12ef83
Nicolas Brousse <n.brousse@pantographe.studio>
2024-11-13 18:13:21 +0100

    Cleanup

--- app/components/has_many_turbo_frame_component.rb
+++ app/components/has_many_turbo_frame_component.rb
@@ -3,14 +3,20 @@
 class HasManyTurboFrameComponent < ApplicationComponent
   include Turbo::FramesHelper
 
+  renders_one :actions
+
   erb_template <<~ERB
     <%= render CardComponent.new(type: @type, extra_classes: @extra_classes, **@html_options) do |c| %>
       <% c.with_header do %>
         <div class="d-flex justify-content-between">
           <span><%= @title %></span>
 
-          <%= link_to @url, class:"link-light" do %>
-            <%= t(".see_more_filters") %>
+          <% if actions? %>
+            <%= actions %>
+          <% else %>
+            <%= link_to @url, class: "link-light" do %>
+              <%= t(".see_more_filters") %>
+              <% end %>
           <% end %>
         </div>
       <% end %>
@@ -18,7 +24,7 @@ class HasManyTurboFrameComponent < ApplicationComponent
       <%= turbo_frame_tag(@frame_id, loading: :lazy, src: @url) do %>
         <div class="w-100 d-flex justify-content-center align-items-center gap-3">
           <span role="status"><%= t(".loading") %></span>
-          <span class="spinner-grow text-primary" aria-hidden="true"></span>
+          <span class="spinner-grow text-<%= @type %>" aria-hidden="true"></span>
         </div>
       <% end %>
     <% end %>
