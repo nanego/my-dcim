48c64adb7d9cfbe3f8f7f90b9fa6595ee947d541
B-Rass <B-Rass@users.noreply.github.com>
2024-11-15 12:05:43 +0100

    Add warning icon on bay if it has no frame associated (#256)

---
+++ app/decorators/bay_decorator.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal: true
+
+class BayDecorator < ApplicationDecorator
+  include ActionView::Context
+  include ActionView::Helpers::AssetTagHelper
+  include ActionView::Helpers::TextHelper
+
+  def no_frame_warning_icon
+    tag.span class: "bay-with-no-frame-warning ms-2" do
+      concat(tag.i(class: "bi bi-exclamation-triangle-fill text-warning",
+                   title: I18n.t(".bays.decorator.no_frame_warning_text"),
+                   aria: { hidden: true },
+                   data: { controller: "tooltip", bs_placement: "right" }))
+      concat(tag.span(I18n.t(".bays.decorator.no_frame_warning_text"), class: "visually-hidden"))
+    end
+  end
+end
