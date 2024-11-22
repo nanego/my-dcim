e608c3b0edaa42a69d98896d89bcf20c3a492916
Nicolas Brousse <nicolas-brousse@users.noreply.github.com>
2024-11-22 12:48:19 +0100

    fix: use correct object for ChangelogEntry index and show pages (#268)

--- spec/decorators/changelog_entry_decorator_spec.rb
+++ spec/decorators/changelog_entry_decorator_spec.rb
@@ -3,8 +3,33 @@
 require 'rails_helper'
 
 RSpec.describe ChangelogEntryDecorator, type: :decorator do
-  let(:object) { ChangelogEntry.new(object_type: "colors", object_changed_attributes: { id: [nil, 42] }) }
+  let(:object) { ChangelogEntry.new(object_type: "Color", object_changed_attributes: { id: [nil, 42] }) }
   let(:decorated_changelog_entry) { described_class.decorate(object) }
+
+  describe "#object_link_to" do
+    let(:view_context) do
+      controller_class = Class.new(ActionView::TestCase::TestController)
+      controller_class.new.view_context
+    end
+
+    context "with existing object" do
+      let(:object) { ChangelogEntry.new(object: colors(:one)) }
+
+      it do
+        expect(decorated_changelog_entry.object_link_to(view_context))
+          .to eq(%(<a href="/colors/#{object.object.id}">Couleur: ##{object.object.id}</a>))
+      end
+    end
+
+    context "with destroyed object" do
+      let(:object) { ChangelogEntry.new(object_type: "Color", object_id: 999_999_999) }
+
+      it do
+        expect(decorated_changelog_entry.object_link_to(view_context))
+          .to eq("Couleur: #999999999")
+      end
+    end
+  end
 
   describe '#object_pre_change_attributes' do
     it { expect(decorated_changelog_entry.object_pre_change_attributes).to eq({ "id" => nil }) }
