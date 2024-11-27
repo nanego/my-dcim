b011509f0b7c6352164b39d3ecb95feb0af3e252
Nicolas Brousse <n.brousse@pantographe.studio>
2024-11-27 18:14:41 +0100

    wip

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ Bundler.require(*Rails.groups)
 module Demenagement
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.2
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -44,7 +44,7 @@ module Demenagement
     config.i18n.fallbacks = true
 
     config.generators do |g|
-      g.test_framework :rspec
+      g.test_framework :rspec, fixtures: true
     end
   end
 end
