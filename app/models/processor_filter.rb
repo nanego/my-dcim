bd384c519908a77dde6bdca7bac4e4770af2c79a
Nicolas Brousse <n.brousse@pantographe.studio>
2024-12-11 17:19:32 +0100

    style: code cleanup

--- app/models/processor_filter.rb
+++ app/models/processor_filter.rb
@@ -3,7 +3,7 @@
 class ProcessorFilter < Filter
   def initialize(records, params, with: nil)
     @records = records
-    @rubanok_class = with || "#{records&.klass&.to_s&.pluralize}Processor".safe_constantize
+    @rubanok_class = with || guess_processor_from_records
 
     raise "Processor class missing" unless @rubanok_class
 
@@ -20,5 +20,13 @@ class ProcessorFilter < Filter
 
   def results_count
     @results_count ||= results.count
+  end
+
+  private
+
+  def guess_processor_from_records(records)
+    return if records.blank?
+
+    "#{records.klass&.to_s&.pluralize}Processor".safe_constantize
   end
 end
