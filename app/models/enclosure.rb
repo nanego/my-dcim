e036a38dd4cf26f173ac0ef81ddba437fa071c96
B_Rass <b.rasschaert@pantographe.studio>
2024-12-17 17:01:51 +0100

    Manage nested_attributes destroy on preview action

--- app/models/enclosure.rb
+++ app/models/enclosure.rb
@@ -7,11 +7,11 @@ class Enclosure < ApplicationRecord
   acts_as_list scope: [:modele_id]
 
   belongs_to :modele
-  has_many :composants # , -> { order(position: :asc) }
+  has_many :composants, dependent: :destroy
 
   accepts_nested_attributes_for :composants,
-                                :allow_destroy => true,
-                                :reject_if     => :all_blank
+                                allow_destroy: true,
+                                reject_if: :all_blank
 
   def deep_dup
     dup.tap do |enclosure|
