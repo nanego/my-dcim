8c45b134f1800c058a01ea5917b6cfa7adc391d8
Nicolas Brousse <nicolas-brousse@users.noreply.github.com>
2024-12-19 17:53:06 +0100

    Add settings page to GLPI sync (#303)
    * Add settings page to GLPI sync
    
    * Clean buttons and locales
    
    ---------
    
    Co-authored-by: B_Rass <b.rasschaert@pantographe.studio>

---
+++ spec/models/external_app_record_setting_spec.rb
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ExternalAppRecordSetting do
+  subject(:settings) { described_class.new(attributes) }
+
+  let(:attributes) { {} }
+
+  describe "#category_ids" do
+    it { expect(settings.category_ids).to eq([1]) }
+  end
+
+  describe "#save" do
+    let(:attributes) { { category_ids: [2] } }
+
+    it :aggregate_failures do # rubocop:disable RSpec/ExampleLength
+      expect(categories(:one).is_glpi_synchronizable).to be(true)
+      expect(categories(:two).is_glpi_synchronizable).to be(false)
+
+      settings.save
+      categories(:one).reload
+      categories(:two).reload
+
+      expect(categories(:one).is_glpi_synchronizable).to be(false)
+      expect(categories(:two).is_glpi_synchronizable).to be(true)
+    end
+  end
+end
