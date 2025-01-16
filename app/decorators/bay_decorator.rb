87dc1effa57f236a9b7cb51b899856061087e5c2
B_Rass <b.rasschaert@pantographe.studio>
2025-01-16 19:43:46 +0100

    RSpec: Use htmlmatcher in decorator

--- app/decorators/bay_decorator.rb
+++ app/decorators/bay_decorator.rb
@@ -19,10 +19,10 @@ class BayDecorator < ApplicationDecorator
 
   def no_frame_warning_icon
     tag.span class: "bay-with-no-frame-warning ms-2" do
-      concat(tag.i(class: "bi bi-exclamation-triangle-fill text-warning",
-                   title: I18n.t(".bays.decorator.no_frame_warning_text"),
-                   aria: { hidden: true },
-                   data: { controller: "tooltip", bs_placement: "right" }))
+      concat(tag.span(class: "bi bi-exclamation-triangle-fill text-warning",
+                      title: I18n.t(".bays.decorator.no_frame_warning_text"),
+                      aria: { hidden: true },
+                      data: { controller: "tooltip", bs_placement: "right" }))
       concat(tag.span(I18n.t(".bays.decorator.no_frame_warning_text"), class: "visually-hidden"))
     end
   end
