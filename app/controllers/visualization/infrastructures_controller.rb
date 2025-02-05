a4c403a074e4d9abb40500c770f6304e4c160941
Nicolas Brousse <n.brousse@pantographe.studio>
2025-02-05 16:53:03 +0100

    wip

--- app/controllers/visualization/infrastructures_controller.rb
+++ app/controllers/visualization/infrastructures_controller.rb
@@ -22,40 +22,29 @@ module Visualization
 
       @servers = Server.where.not(network_types: [])
       # .includes(:cards, :ports => [:connection => [:port, :cable =>[:connections => [:port => :card]]]]).
-      @concentrateurs_ids = [383, 384, 1043, 1044]
-      @concentrateurs = Server.where(id: @concentrateurs_ids)
 
       fresh_when last_modified: [@servers.maximum(:updated_at), @concentrateurs.maximum(:updated_at)].max
 
       @servers = @servers.includes(:frame, :stack, :ports, cards: [:ports])
-      @concentrateurs = @concentrateurs.includes(ports: :connection, cards: [ports: :connection])
+      @hubs = RoomHub.for_room(@room, network_types: @filter.network_type)
 
-      @switchs_lan_ids = @concentrateurs_ids | @servers.pluck(:id) # Switch LAN
-      # TODO: Remove hard-coded values
-      @hubs = {}
-
-      unless Rails.env.test?
-        @hubs = { "gbe" => { 4 => Server.find(383), 3 => Server.find(384) }, "10gbe" => { 4 => Server.find(1043), 3 => Server.find(1044) } } # Concentrateurs per room
-      end
+      @switchs_lan_ids = (@hubs.pluck(:server_a) + @hubs.pluck(:server_b)).compact | @servers.pluck(:id) # Switch LAN
 
       @connections = {}
       @stacks = @servers.map(&:stack).uniq.compact
       @servers.each do |server|
         @connections[server.id] = server.directly_connected_servers_ids_with_color.reject { |conn| @switchs_lan_ids.exclude?(conn[:server_id]) }
       end
-      @concentrateurs.each do |hub|
-        @connections[hub.id] = hub.connected_servers_ids_through_twin_cards_with_color.reject { |conn| @switchs_lan_ids.exclude?(conn[:server_id]) }
+      @hubs.each do |hub|
+        @connections[hub.server_a.id] = hub.server_a.connected_servers_ids_through_twin_cards_with_color.reject { |conn| @switchs_lan_ids.exclude?(conn[:server_id]) }
+        @connections[hub.server_b.id] = hub.server_b.connected_servers_ids_through_twin_cards_with_color.reject { |conn| @switchs_lan_ids.exclude?(conn[:server_id]) }
       end
 
-      # TODO: remove when hard-coded system will be removed
-      @network_types = Modele::Network::TYPES.excluding("fiber")
       @network = @filter.network_type # TODO: take from params and raise error if not good
 
-      return unless [4, 3].include?(@room.id)
-
-      @hub = @hubs[@network][@room.id]
-      @second_room = Room.find(@room.id == 4 ? 3 : 4)
-      @second_hub = @hubs[@network][@second_room.id]
+      # @hub = @hubs[@network][@room.id]
+      # @second_room = Room.find(@room.id == 4 ? 3 : 4)
+      # @second_hub = @hubs[@network][@second_room.id]
     end
   end
 end
