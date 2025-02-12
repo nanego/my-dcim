a0f9e1d9b0b782acf082dfeca3522f61e65f2319
Maximilien Bausson <m.bausson@pantographe.studio>
2025-02-12 15:41:11 +0100

    refactoring

--- app/decorators/server_decorator.rb
+++ app/decorators/server_decorator.rb
@@ -1,20 +1,9 @@
 # frozen_string_literal: true
 
 class ServerDecorator < ApplicationDecorator
-  DEFAULT_COLUMNS_PREFERENCE = %w[name numero category islet bay network_types].freeze
-
   def network_types_to_human
     return Modele.human_attribute_name("network_types.blank") unless (n_t = network_types.presence)
 
     n_t.map { |type| Modele.human_attribute_name("network_types.#{type}") }.join(", ")
-  end
-
-  class << self
-    # List of columns that can be displayed on Servers#Index
-    def columns_preference
-      %w[name numero category islet bay network_types].index_with do |col|
-        Server.human_attribute_name(col)
-      end
-    end
   end
 end
