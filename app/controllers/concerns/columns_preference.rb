b9b3c630c3176358eb77c5f2a44d1abd180a8cc4
Maximilien Bausson <m.bausson@pantographe.studio>
2025-02-12 17:48:20 +0100

    wip associations loading

--- app/controllers/concerns/columns_preference.rb
+++ app/controllers/concerns/columns_preference.rb
@@ -9,7 +9,14 @@ module ColumnsPreference
     private
 
     def set_displayed_columns
-      @displayed_columns = search_params[:columns] || controller_path.classify.constantize::DEFAULT_COLUMNS_PREFERENCE
+      model = controller_path.classify.constantize
+
+      @displayed_columns = search_params[:columns] || model::DEFAULT_COLUMNS_PREFERENCE
+      @displayed_associations_columns = @displayed_columns.select { |col| col.include? "." }
+                                                          .map do |col|
+                                                            k, v = col.split('.', 2)
+                                                            { k.to_sym => v.to_sym }
+                                                          end
     end
   end
 end
