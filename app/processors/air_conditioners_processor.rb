6bf1ab9928bdc423225c312e0ff8563e25fd65cd
Nicolas Brousse <n.brousse@pantographe.studio>
2025-02-26 12:05:58 +0100

    rework AirConditioner processor

--- app/processors/air_conditioners_processor.rb
+++ app/processors/air_conditioners_processor.rb
@@ -9,11 +9,11 @@ class AirConditionersProcessor < ApplicationProcessor
   end
 
   map :room_ids, filter_with: :non_empty_array do |room_ids:|
-    raw.joins(:room).where(rooms: { id: room_ids })
+    raw.joins(bay: { islet: :room }).where(rooms: { id: room_ids })
   end
 
   map :islet_ids, filter_with: :non_empty_array do |islet_ids:|
-    raw.joins(:islet).where(islets: { id: islet_ids })
+    raw.joins(bay: :islet).where(islets: { id: islet_ids })
   end
 
   sortable fields: SORTABLE_FIELDS
--- spec/processors/air_conditioners_processor_spec.rb
+++ spec/processors/air_conditioners_processor_spec.rb
@@ -8,6 +8,22 @@ RSpec.describe AirConditionersProcessor do
   let(:input) { AirConditioner.all }
   let(:params) { {} }
 
+  describe "when searching" do
+    let(:bay) { bays(:one) }
+    let(:air_conditioner_model) { air_conditioner_models(:one) }
+
+    let(:params) { { q: "wood" } }
+
+    before do
+      AirConditioner.create!(name: "brick", bay:, air_conditioner_model:, status: :on, position: :left)
+      AirConditioner.create!(name: "wood", bay:, air_conditioner_model:, status: :on, position: :left)
+      AirConditioner.create!(name: "wooden", bay:, air_conditioner_model:, status: :on, position: :left)
+    end
+
+    # IMPROVE
+    it { expect(result.size).to eq(2) }
+  end
+
   describe "when filtering by room_ids" do
     let(:room) { Room.create!(name: "A", site: sites(:one)) }
     let(:bay)  { Bay.create!(room: room, bay_type: bay_types(:one)) }
@@ -52,7 +68,7 @@ RSpec.describe AirConditionersProcessor do
   end
 
   describe "when filtering by islet_ids" do
-    let(:islet) { Islet.create!(name: "S1", room: rooms(:one)) }
+    let(:islet) { Islet.create!(name: "I1", room: rooms(:one)) }
     let(:bay)   { Bay.create!(islet: islet, bay_type: bay_types(:one)) }
     let(:air_conditioner) do
       AirConditioner.create!(
@@ -75,7 +91,7 @@ RSpec.describe AirConditionersProcessor do
     end
 
     context "with many islet_ids" do # rubocop:disable RSpec/MultipleMemoizedHelpers
-      let(:islet_second) { Islet.create!(name: "S2", room: rooms(:two)) }
+      let(:islet_second) { Islet.create!(name: "I2", room: rooms(:two)) }
       let(:bay_second)   { Bay.create!(islet: islet_second, bay_type: bay_types(:one)) }
       let(:air_conditioner_second) do
         AirConditioner.create!(
@@ -96,5 +112,32 @@ RSpec.describe AirConditionersProcessor do
 
   describe "when sorting" do
     pending "TODO"
+  end
+
+  describe "When searching on every fields" do
+    let(:room)  { Room.create!(name: "A", site: sites(:one)) }
+    let(:islet) { Islet.create!(name: "I1", room:) }
+    let(:bay)   { Bay.create!(islet:, bay_type: bay_types(:one)) }
+    let(:air_conditioner) do
+      AirConditioner.create!(
+        name: "wood", bay:, air_conditioner_model: air_conditioner_models(:one), status: :on, position: :left,
+      )
+    end
+
+    let(:params) { { q: "wood", room_ids: room.id, islet_ids: islet.id } }
+
+    before { air_conditioner }
+
+    it { expect(result.size).to eq(1) }
+    it { is_expected.to contain_exactly(air_conditioner) }
+
+    described_class::SORTABLE_FIELDS.each do |field|
+      context "and sort on #{field}" do # rubocop:disable RSpec/ContextWording
+        let(:params) { { q: "wood", room_ids: room.id, islet_ids: islet.id, sort_by: field } }
+
+        it { expect(result.size).to eq(1) }
+        it { is_expected.to contain_exactly(air_conditioner) }
+      end
+    end
   end
 end
