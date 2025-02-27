f342527cc407bfb5ab53a701eebe3a6ecec2ca89
Maximilien Bausson <25268481+MBausson@users.noreply.github.com>
2025-02-27 17:43:42 +0100

    358734 - Add filters on GLPI synchronization index (#328)
    * wip
    
    * wip tests (not working)
    
    * test fic
    
    * wip
    
    * wip error
    
    * wip
    
    * updated specs
    
    * wip fix
    
    * fix
    
    * fix
    
    * wip
    
    * testfix
    
    * rubocop fix
    
    * fixes
    
    * test: rework ExternalAppRecordProcessor tests
    
    ---------
    
    Co-authored-by: Maximilien Bausson <m.bausson@pantographe.studio>
    Co-authored-by: Nicolas Brousse <n.brousse@pantographe.studio>

--- app/processors/external_app_records_processor.rb
+++ app/processors/external_app_records_processor.rb
@@ -10,6 +10,14 @@ class ExternalAppRecordsProcessor < ApplicationProcessor
     raw.joins(server: :frame).where(frames: { id: frame_ids })
   end
 
+  map :modele_ids, filter_with: :non_empty_array do |modele_ids:|
+    raw.joins(:server).where(servers: { modele_id: modele_ids })
+  end
+
+  map :server_name do |server_name:|
+    raw.joins(:server).where(Server.arel_table[:name].matches("%#{server_name}%"))
+  end
+
   match :external_serial_status, fail_when_no_matches: true do
     having "found" do
       raw.where.not(external_serial: nil).where.not(external_serial: "")
