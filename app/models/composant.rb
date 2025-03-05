ad786b7b3bda9c41754b8237342129675f6cb82f
Nicolas Brousse <n.brousse@pantographe.studio>
2025-03-05 16:49:37 +0100

    wip

--- app/models/composant.rb
+++ app/models/composant.rb
@@ -2,17 +2,16 @@
 
 class Composant < ApplicationRecord
   has_changelog
-  acts_as_list scope: %i[enclosure_id type_composant_id]
+  acts_as_list scope: %i[enclosure_id]
 
   belongs_to :enclosure
-  belongs_to :type_composant
   has_one :modele, through: :enclosure
 
   has_many :cards
 
   validates :name, format: { without: /\s/ }, allow_blank: true
 
-  scope :slots, -> { where(type_composant: TypeComposant.find_by_name('SLOT')).order("composants.position ASC") }
+  scope :slots, -> {order("composants.position ASC") }
   scope :ordered, -> { order(position: :asc) }
 
   def to_s
