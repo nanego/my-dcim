ff7f6613ca3a65c1fbed605ff9912439ee65b57e
B_Rass <b.rasschaert@pantographe.studio>
2025-03-12 15:40:37 +0100

    Update LabelComponent to BadgeComponent (name + add pill type)

--- spec/components/badge_component_spec.rb
+++ spec/components/badge_component_spec.rb
@@ -2,7 +2,7 @@
 
 require "rails_helper"
 
-RSpec.describe LabelComponent, type: :component do
+RSpec.describe BadgeComponent, type: :component do
   context "with html content" do
     let(:component) { described_class.new(text, **kwargs) }
     let(:rendered_component) { render_inline(component, &block) }
@@ -13,7 +13,7 @@ RSpec.describe LabelComponent, type: :component do
     context "with text" do
       it do
         expect(rendered_component.to_html).to have_tag(
-          "span.text-default-emphasis.bg-default-subtle.border.border-default-subtle",
+          "span.text-primary-emphasis.bg-primary-subtle.border.border-primary-subtle",
           text: /Text as argument/
         )
       end
@@ -24,18 +24,35 @@ RSpec.describe LabelComponent, type: :component do
 
       it do
         expect(rendered_component.to_html).to have_tag(
-          "span.text-default-emphasis.bg-default-subtle.border.border-default-subtle",
+          "span.text-primary-emphasis.bg-primary-subtle.border.border-primary-subtle",
           text: /Text as block/
         )
       end
     end
 
-    context "with type" do
-      let(:kwargs) { { type: :success } }
+    context "with color" do
+      let(:kwargs) { { color: :success } }
 
       it do
         expect(rendered_component.to_html).to have_tag(
           "span.text-success-emphasis.bg-success-subtle.border.border-success-subtle",
+          text: /Text as argument/
+        )
+      end
+    end
+
+    context "with not valid color" do
+      let(:kwargs) { { color: :unknow } }
+
+      it { expect { component }.to raise_error(ArgumentError) }
+    end
+
+    context "with type" do
+      let(:kwargs) { { type: :pill } }
+
+      it do
+        expect(rendered_component.to_html).to have_tag(
+          "span.badge.rounded-pill",
           text: /Text as argument/
         )
       end
