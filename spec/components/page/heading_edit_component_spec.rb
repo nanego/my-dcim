ffe90d389b93f221d3054a47657703b1515beb05
B-Rass <B-Rass@users.noreply.github.com>
2025-03-27 16:16:16 +0100

    376521 servers show connections tab: add toggle all vlans button (#365)

--- spec/components/page/heading_edit_component_spec.rb
+++ spec/components/page/heading_edit_component_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe Page::HeadingEditComponent, type: :component do
       expect(rendered_component).to have_tag("div.col-12.bg-body") do
         with_tag("div.back-button-container") do
           with_tag("a.btn.back-button", title: "Retour", href: "http://test.host/sites") do
-            with_tag("i.bi-chevron-left")
+            with_tag("span.bi-chevron-left")
             with_tag("span.ms-2", text: "Retour")
           end
         end
@@ -32,7 +32,7 @@ RSpec.describe Page::HeadingEditComponent, type: :component do
         with_tag("div.d-flex") do
           with_tag("div.align-self-center.d-inline-flex") do
             with_tag("a.btn-primary", title: "Voir", href: "http://test.host/sites/1") do
-              with_tag("i.bi-eye")
+              with_tag("span.bi-eye")
               with_tag("span.ms-2.d-none", text: "Voir")
             end
           end
@@ -56,7 +56,7 @@ RSpec.describe Page::HeadingEditComponent, type: :component do
           with_tag("a.btn", text: "Button")
           with_tag("div.align-self-center.d-inline-flex") do
             with_tag("a.btn-primary", title: "Voir", href: "http://test.host/sites/1") do
-              with_tag("i.bi-eye")
+              with_tag("span.bi-eye")
               with_tag("span.ms-2.d-none", text: "Voir")
             end
           end
