eb0dbb8d36c1a4b476e73ba62c14aeb11ede3cd6
Maximilien Bausson <m.bausson@pantographe.studio>
2025-04-09 11:55:03 +0200

    Applying preferences without saving

--- app/controllers/columns_controller.rb
+++ app/controllers/columns_controller.rb
@@ -4,9 +4,12 @@ class ColumnsController < ApplicationController
   before_action :set_model, only: %i[save reset]
 
   def save
-    session[@model] = columns_params[:columns]
-
-    redirect_back_or_to root_path
+    if save_preferences?
+      session[@model] = columns_params[:columns]
+      redirect_back_or_to root_path
+    else
+      redirect_back_with_params(columns: columns_params[:columns])
+    end
   end
 
   def reset
@@ -22,6 +25,15 @@ class ColumnsController < ApplicationController
   end
 
   def columns_params
-    params.permit(:model, columns: [])
+    params.permit(:model, :save, columns: [])
+  end
+
+  def save_preferences?
+    columns_params[:save] == "1"
+  end
+
+  def redirect_back_with_params(params = {})
+    referer = request.referer || root_path
+    redirect_to("#{referer}?#{params.to_query}")
   end
 end
