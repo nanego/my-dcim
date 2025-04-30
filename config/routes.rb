975a87961809940897c549664058380e786d3868
Maximilien Bausson <m.bausson@pantographe.studio>
2025-04-30 11:56:05 +0200

    Bays

--- config/routes.rb
+++ config/routes.rb
@@ -116,7 +116,7 @@ Rails.application.routes.draw do
                                     sessions: 'users/sessions' }
 
   namespace :bulk do
-    %i[servers sites rooms islets].each do |res|
+    %i[servers sites rooms islets bays].each do |res|
       resource res, only: :destroy
     end
   end
