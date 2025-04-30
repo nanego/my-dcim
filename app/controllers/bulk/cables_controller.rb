269c1546f32cbbdb2574e67487469f170065ed3c
Maximilien Bausson <m.bausson@pantographe.studio>
2025-04-30 15:39:22 +0200

    Cables

---
+++ app/controllers/bulk/cables_controller.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+module Bulk
+  class CablesController < BaseController
+    before_action :set_cables
+
+    def destroy
+      respond_to do |format|
+        if @cables.map(&:destroy).all?
+          format.html { redirect_to cables_path, notice: t("bulk.resource.destroy.flashes.destroyed", resource: Cable.model_name.human.pluralize), status: :see_other }
+        else
+          # TODO: tell which records has not been removed
+          format.html { redirect_to cables_path, alert: t("bulk.resource.destroy.flashes.not_destroyed", resource: Cable.model_name.human), status: :see_other }
+        end
+      end
+    end
+
+    private
+
+    def set_cables
+      @cables = Cable.where(id: params[:ids])
+    end
+  end
+end
---
+++ spec/requests/bulk/cables_request_spec.rb
@@ -0,0 +1,22 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "/bulk/cables" do
+  before { sign_in users(:one) }
+
+  describe "DELETE /destroy" do
+    context "with cables without associations" do
+      it do
+        expect do
+          delete bulk_cables_path(ids: [cables(:one).id, cables(:two).id])
+        end.to change(Cable, :count).by(-2)
+      end
+
+      it do
+        delete bulk_cables_path(ids: [cables(:one).id, cables(:two).id])
+        expect(response).to redirect_to(cables_path)
+      end
+    end
+  end
+end
