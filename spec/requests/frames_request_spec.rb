60104b8b4fe625835eed133bf3f686e2f21f1662
Vincent <contact@vincent-robert.com>
2025-05-13 08:48:46 +0200

    Add missing specs for frames network view

--- spec/requests/frames_request_spec.rb
+++ spec/requests/frames_request_spec.rb
@@ -4,12 +4,43 @@ require "rails_helper"
 
 RSpec.describe "Frames" do
   let(:frame) { frames(:one) }
+  let(:coupled_frame) { frames(:two) }
+  let(:network_frame) { frames(:three) }
+
+  describe "GET #network" do
+    subject(:response) do
+      get network_frame_path(frame, network_frame_id: network_frame.slug)
+      @response # rubocop:disable RSpec/InstanceVariable
+    end
+
+    include_context "with authenticated user"
+
+    it { is_expected.to have_http_status(:success) }
+    it { is_expected.to render_template(:network) }
+
+    context 'when frame is the coupled frame' do
+      subject(:response) do
+        get network_frame_path(coupled_frame, network_frame_id: network_frame.slug)
+        @response # rubocop:disable RSpec/InstanceVariable
+      end
+
+      it 'renders successfully' do
+        expect(response).to have_http_status(:success)
+      end
+    end
+
+    context 'with non-existent frame' do
+      it 'raises not found error' do
+        expect {
+          get network_frame_path('non-existent-frame', network_frame_id: network_frame.slug)
+        }.to raise_error(ActiveRecord::RecordNotFound)
+      end
+    end
+  end
 
   describe "GET #print" do
     subject(:response) do
       get print_visualization_frame_path(frame)
-
-      # NOTE: used to simplify usage and custom test done in final spec file.
       @response # rubocop:disable RSpec/InstanceVariable
     end
 
