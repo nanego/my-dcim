168ed1f4685c91efcf0cde43e30ac751365e19e0
Nicolas Brousse <n.brousse@pantographe.studio>
2025-05-14 14:11:56 +0200

    Create MovesProject model

---
+++ spec/models/moves_project_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MovesProject do
+  # it_behaves_like "changelogable", new_attributes: {  }
+
+  subject(:move) { described_class.new(name: "A") }
+
+  describe "validations" do
+    it { is_expected.to be_valid }
+
+    it { is_expected.to validate_presence_of(:name) }
+  end
+
+  describe "#to_s" do
+    it { expect(move.to_s).to eq("A") }
+  end
+end
