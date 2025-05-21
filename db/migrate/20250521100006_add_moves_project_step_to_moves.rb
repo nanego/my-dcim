903749815c5069bdf7b8f3779c160ed33c941eb9
Nicolas Brousse <n.brousse@pantographe.studio>
2025-05-21 18:10:37 +0200

    Cleanup

--- db/migrate/20250521100006_add_moves_project_step_to_moves.rb
+++ db/migrate/20250521100006_add_moves_project_step_to_moves.rb
@@ -2,6 +2,6 @@
 
 class AddMovesProjectStepToMoves < ActiveRecord::Migration[8.0]
   def change
-    add_reference :moves, :moves_project_step, null: false, foreign_key: true # rubocop:disable Rasil/NotNullColumn
+    add_reference :moves, :moves_project_step, null: false, foreign_key: true # rubocop:disable Rails/NotNullColumn
   end
 end
