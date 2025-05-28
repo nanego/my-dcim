5529d39e6be3a1a7285e04ea6d2ffc5dd2ed82cd
Maximilien Bausson <m.bausson@pantographe.studio>
2025-05-28 11:05:40 +0200

    wip drag-n-drop

---
+++ app/javascript/controllers/step_sortable_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from "sortablejs"
+
+export default class StepSortable extends Controller {
+  static targets = ["container"]
+
+  connect() {
+    this.sortable = Sortable.create(this.containerTarget, {
+      animation: 150,
+      onEnd: this.reorder.bind(this)
+    })
+  }
+
+  reorder() {
+    this.containerTarget.querySelectorAll("[data-step-sortable-item]").forEach((item, index) => {
+      const positionField = item.querySelector("[data-position-field]")
+      positionField.value = index + 1
+    })
+  }
+}
