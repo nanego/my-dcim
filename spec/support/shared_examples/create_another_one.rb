1a1c0c958b9e5f66bf4e5887f51a5f68ece5bb76
Maximilien Bausson <m.bausson@pantographe.studio>
2025-06-11 15:35:19 +0200

    Updated tests

--- spec/support/shared_examples/create_another_one.rb
+++ spec/support/shared_examples/create_another_one.rb
@@ -5,5 +5,5 @@ RSpec.shared_examples "with create another one" do
     params[:create_another_one] = true
   end
 
-  it { expect(response).to redirect_to(url_for(action: :new)) }
+  it { expect(response).to redirect_to(url_for(action: :new, create_another_one: "1")) }
 end
