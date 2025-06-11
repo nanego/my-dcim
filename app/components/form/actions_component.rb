3771b069df9d78f3b3285f5c7d65e0e75e52a16c
B_Rass <b.rasschaert@pantographe.studio>
2025-06-11 14:12:45 +0200

    Clean UI

--- app/components/form/actions_component.rb
+++ app/components/form/actions_component.rb
@@ -15,16 +15,18 @@ module Form
       cancel_url = @is_edit ? @form.object : polymorphic_path(@form.object.class)
 
       tag.div class: "col-12 py-4 mt-4 text-end sticky-bottom bg-body-tertiary border-top" do
-        concat(render_another_one_checkbox) if @display_create_another_one
-        concat(link_to(t("action.cancel"), cancel_url, class: "btn btn-outline-secondary me-2"))
-        concat(@form.submit(class: class_names("btn", "btn-info": @is_edit, "btn-success": @is_new)))
+        tag.span class: "d-inline-flex align-items-center me-auto" do
+          concat(render_another_one_checkbox) if @display_create_another_one
+          concat(link_to(t("action.cancel"), cancel_url, class: "btn btn-outline-secondary me-2"))
+          concat(@form.submit(class: class_names("btn", "btn-info": @is_edit, "btn-success": @is_new)))
+        end
       end
     end
 
     private
 
     def render_another_one_checkbox
-      tag.span(class: "me-2 form-check form-check-inline") do
+      tag.span(class: "me-3 form-check form-check-inline") do
         concat(check_box_tag(:create_another_one, class: "form-check-input"))
         concat(label_tag(:create_another_one, t("action.create_more"), class: "form-check-label"))
       end
