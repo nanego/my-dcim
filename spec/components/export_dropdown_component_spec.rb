82fc955d3cdf45e2ef836363b933af0f78d93700
Maximilien Bausson <m.bausson@pantographe.studio>
2025-06-18 15:14:11 +0200

    Added tests

--- spec/components/export_dropdown_component_spec.rb
+++ spec/components/export_dropdown_component_spec.rb
@@ -3,14 +3,14 @@
 require "rails_helper"
 
 RSpec.describe ExportDropdownComponent, type: :component do
-  let(:component) { described_class.new(url: "basic_url", page_url: "page_url") }
+  let(:component) { described_class.new(url: :root_path) }
   let(:rendered_component) { render_inline(component) }
 
   it "renders the component" do # rubocop:disable RSpec/ExampleLength
     expect(rendered_component.to_html).to have_tag("div.dropdown") do
       with_tag("ul.dropdown-menu") do
-        with_tag("a[href=\"page_url\"]", count: 1)
-        with_tag("a[href=\"basic_url\"]", count: 1)
+        with_tag("a[href=\"/?format=csv\"]", count: 1)
+        with_tag("a[href=\"/?format=csv&page=1\"]", count: 1)
       end
     end
   end
