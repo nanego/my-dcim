ad8376685630ab8c6f4ea85e5c759cb1d3df0be6
Nicolas Brousse <n.brousse@pantographe.studio>
2025-06-20 15:45:59 +0200

    make move frame and move form preview show servers planned to move

--- spec/models/moves_project_step_spec.rb
+++ spec/models/moves_project_step_spec.rb
@@ -44,6 +44,9 @@ RSpec.describe MovesProjectStep do
   end
 
   describe "#servers_moves_for_frame_at_current_step" do
-    pending
+    it do
+      expect(moves_project_steps(:planned).servers_moves_for_frame_at_current_step(frames(:one)))
+        .to contain_exactly(servers(:two), servers(:with_cluster))
+    end
   end
 end
