89d5b541d8dddc040f6fa795c32a221d5dfaff94
Maximilien Bausson <m.bausson@pantographe.studio>
2025-06-25 15:06:02 +0200

    Added polici specs

--- app/policies/user_policy.rb
+++ app/policies/user_policy.rb
@@ -4,4 +4,8 @@ class UserPolicy < ApplicationPolicy
   def edit_user?
     user.admin?
   end
+
+  def update_user?
+    user.admin?
+  end
 end
---
+++ spec/policies/user_policy_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe UserPolicy, type: :policy do
+  let(:user) { users(:one) }
+  let(:record) { users(:two) }
+  let(:context) { { user: } }
+  let(:role) { :admin }
+
+  before { context[:user].role = role  }
+
+  describe_rule :edit_user? do
+    succeed "when an admin user asks"
+
+    %i[user vip].each do |unauthorized_role|
+      failed "when user is #{unauthorized_role}" do
+        let(:role) { unauthorized_role }
+      end
+    end
+  end
+
+  describe_rule :update_user? do
+    succeed "when an admin user asks"
+
+    %i[user vip].each do |unauthorized_role|
+      failed "when user is #{unauthorized_role}" do
+        let(:role) { unauthorized_role }
+      end
+    end
+  end
+end
