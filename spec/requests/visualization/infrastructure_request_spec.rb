c5a72cc245e3a56346e4d9f1e1ff2a77ee31cffd
Maximilien Bausson <m.bausson@pantographe.studio>
2025-06-26 15:54:42 +0200

    Added a test

--- spec/requests/visualization/infrastructure_request_spec.rb
+++ spec/requests/visualization/infrastructure_request_spec.rb
@@ -15,4 +15,25 @@ RSpec.describe "Visualization::InfrastructuresController" do
       it { expect(response).to redirect_to(visualization_infrastructure_path(islet_id: 1, network_type: :gbe)) }
     end
   end
+
+  describe "PATCH #edit_room_clusters" do
+    subject(:response) do
+      patch(edit_room_clusters_visualization_infrastructure_path, params:)
+
+      @repsonse # rubocop:disable RSpec/InstanceVariable
+    end
+
+    let(:room) { rooms(:one) }
+    let(:cluster) { clusters(:cloud_c1) }
+    let(:params) { { room: { id: room.id, network_cluster_ids: [cluster.id] } } }
+
+    it { expect(response).to redirect_to(root_path) }
+
+    it do
+      expect do
+        response
+        room.reload
+      end.to change(room, :network_cluster_ids).from([]).to([cluster.id])
+    end
+  end
 end
