b2363b8d845768ca43a76872885e65d91fc21647
Maximilien Bausson <m.bausson@pantographe.studio>
2025-07-23 14:23:16 +0200

    Fix test

--- app/controllers/modeles_controller.rb
+++ app/controllers/modeles_controller.rb
@@ -21,7 +21,7 @@ class ModelesController < ApplicationController
   end
 
   def new
-    @modele = Modele.new(new_modele_params)
+    @modele = Modele.new(params.key?(:modele) ? modele_params : {})
     @modele.composants.build(:name => "ALIM")
     @modele.composants.build(:name => "IPMI")
     @modele.composants.build(:name => "CM")
