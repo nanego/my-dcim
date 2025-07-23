1dd706612acad8e875f89548123d4d5a89532d69
Maximilien Bausson <25268481+MBausson@users.noreply.github.com>
2025-07-23 12:19:43 +0200

    Add test "it_behaves_like "with create another one" on all controllers (#445)

--- spec/requests/bays_request_spec.rb
+++ spec/requests/bays_request_spec.rb
@@ -18,6 +18,61 @@ RSpec.describe "Bays" do
     it { expect(response).to render_template(:index) }
   end
 
+  describe "GET #new" do
+    subject(:response) do
+      get new_bay_path
+
+      # NOTE: used to simplify usage and custom test done in final spec file.
+      @response # rubocop:disable RSpec/InstanceVariable
+    end
+
+    include_context "with authenticated user"
+
+    it { expect(response).to have_http_status(:success) }
+    it { expect(response).to render_template(:new) }
+  end
+
+  describe "POST #create" do
+    subject(:response) do
+      post(bays_path, params:)
+
+      # NOTE: used to simplify usage and custom test done in final spec file.
+      @response # rubocop:disable RSpec/InstanceVariable
+    end
+
+    let(:params) do
+      { bay: { name: "Bay 1", bay_type_id: bay_types(:one).id, islet_id: islets(:one).id } }
+    end
+
+    include_context "with authenticated user"
+    it_behaves_like "with create another one"
+
+    context "with valid parameters" do
+      it { expect(response).to have_http_status(:redirect) }
+      it { expect(response).to redirect_to(bay_path(assigns(:bay))) }
+      it { expect { response }.to change(Bay, :count).by(1) }
+    end
+
+    context "with invalid parameters" do
+      let(:params) { { bay: { name: "Bay 1", bay_type_id: 9999 } } }
+
+      it { expect(response).to render_template(:new) }
+      it { expect { response }.not_to change(Bay, :count) }
+    end
+
+    context "without attributes" do
+      let(:params) { { bay: {} } }
+
+      it { expect { response }.to raise_error(ActionController::ParameterMissing) }
+    end
+
+    context "without parameters" do
+      let(:params) { {} }
+
+      it { expect { response }.to raise_error(ActionController::ParameterMissing) }
+    end
+  end
+
   describe "GET #print" do
     subject(:response) do
       get print_visualization_bay_path(bay)
---
+++ spec/requests/manufacturers_request_spec.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe "/manufacturers" do
+  describe "GET #new" do
+    subject(:response) do
+      get new_manufacturer_path
+
+      # NOTE: used to simplify usage and custom test done in final spec file.
+      @response # rubocop:disable RSpec/InstanceVariable
+    end
+
+    include_context "with authenticated user"
+
+    it { expect(response).to have_http_status(:success) }
+    it { expect(response).to render_template(:new) }
+  end
+
+  describe "POST #create" do
+    subject(:response) do
+      post(manufacturers_path, params:)
+
+      # NOTE: used to simplify usage and custom test done in final spec file.
+      @response # rubocop:disable RSpec/InstanceVariable
+    end
+
+    let(:params) { { manufacturer: { name: "Manufacturer 1", description: "Description 1" } } }
+
+    include_context "with authenticated user"
+    it_behaves_like "with create another one"
+
+    context "with valid parameters" do
+      it { expect(response).to have_http_status(:redirect) }
+      it { expect(response).to redirect_to(manufacturer_path(assigns(:manufacturer))) }
+      it { expect { response }.to change(Manufacturer, :count).by(1) }
+    end
+
+    context "without attributes" do
+      let(:params) { { manufacturer: {} } }
+
+      it { expect { response }.to raise_error(ActionController::ParameterMissing) }
+    end
+
+    context "without parameters" do
+      let(:params) { {} }
+
+      it { expect { response }.to raise_error(ActionController::ParameterMissing) }
+    end
+  end
+end
