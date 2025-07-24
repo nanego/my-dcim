9bacc35ecd577ee186dd15165d1058e5d58ec92c
Maximilien Bausson <m.bausson@pantographe.studio>
2025-07-24 14:02:18 +0200

    wip created files

---
+++ app/policies/domaine_policy.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class DomainePolicy < ApplicationPolicy
+  def manage?
+  end
+
+  def show?
+  end
+end
--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,60 +1,31 @@
 # frozen_string_literal: true
 
-require "rails_helper"
-
-RSpec.describe UserPolicy, type: :policy do # rubocop:disable RSpec/EmptyExampleGroup
-  let(:user) { users(:one) }
-  let(:record) { users(:two) }
-  let(:context) { { user: } }
-  let(:is_admin) { true }
-
-  before { context[:user].is_admin = is_admin }
-
-  describe_rule :index? do
-    succeed "when an admin user asks"
-
-    failed "when user is not admin" do
-      let(:is_admin) { false }
-    end
+class UserPolicy < ApplicationPolicy
+  def index?
+    user.admin?
   end
 
-  describe_rule :show? do
-    succeed "when an admin user asks"
-    succeed "when user asks for itself" do
-      let(:record) { user }
-    end
-
-    failed "when user is not admin" do
-      let(:is_admin) { false }
-    end
+  def show?
+    user.admin? || user == record
   end
 
-  describe_rule :new? do
-    succeed "when an admin user asks"
-
-    failed "when user is not admin" do
-      let(:is_admin) { false }
-    end
+  def new?
+    user.admin?
   end
 
-  describe_rule :manage? do
-    succeed "when an admin user asks"
-
-    failed "when user is not admin" do
-      let(:is_admin) { false }
-    end
+  def destroy?
+    user.admin? && user != record
   end
 
-  %i[destroy? suspend? unsuspend?].each do |rule|
-    describe_rule rule do
-      succeed "when an admin user asks"
-      failed "when user asks for itself" do
-        let(:record) { user }
-      end
+  def manage?
+    user.admin?
+  end
 
-      failed "when user is not admin" do
-        let(:is_admin) { false }
-      end
-    end
+  def suspend?
+    user.admin? && user != record
+  end
+
+  def unsuspend?
+    suspend?
   end
 end
