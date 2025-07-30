f7bbcc4cf641815dee8acf80ed561f347d9aab2a
Maximilien Bausson <m.bausson@pantographe.studio>
2025-07-30 15:03:52 +0200

    Added tests

--- spec/policies/port_type_policy_spec.rb
+++ spec/policies/port_type_policy_spec.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
-class PortTypePolicy < ApplicationPolicy
-  def manage?
-  end
+require "rails_helper"
 
-  def show?
-  end
+RSpec.describe PortTypePolicy, type: :policy do
+  it_behaves_like "with default resource policies"
 end
