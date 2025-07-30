4c518fd4c8eefd6f642d320f087cbc9c82a758f6
Maximilien Bausson <m.bausson@pantographe.studio>
2025-07-30 14:09:15 +0200

    Fixed user policy spec

--- spec/policies/user_policy_spec.rb
+++ spec/policies/user_policy_spec.rb
@@ -1,31 +1,60 @@
 # frozen_string_literal: true
 
-class UserPolicy < ApplicationPolicy
-  def index?
-    user.admin?
+require "rails_helper"
+
+RSpec.describe UserPolicy, type: :policy do # rubocop:disable RSpec/EmptyExampleGroup
+  let(:user) { users(:one) }
+  let(:record) { users(:two) }
+  let(:context) { { user: } }
+  let(:is_admin) { true }
+
+  before { context[:user].is_admin = is_admin }
+
+  describe_rule :index? do
+    succeed "when an admin user asks"
+
+    failed "when user is not admin" do
+      let(:is_admin) { false }
+    end
   end
 
-  def show?
-    user.admin? || user == record
+  describe_rule :show? do
+    succeed "when an admin user asks"
+    succeed "when user asks for itself" do
+      let(:record) { user }
+    end
+
+    failed "when user is not admin" do
+      let(:is_admin) { false }
+    end
   end
 
-  def new?
-    user.admin?
+  describe_rule :new? do
+    succeed "when an admin user asks"
+
+    failed "when user is not admin" do
+      let(:is_admin) { false }
+    end
   end
 
-  def destroy?
-    user.admin? && user != record
+  describe_rule :manage? do
+    succeed "when an admin user asks"
+
+    failed "when user is not admin" do
+      let(:is_admin) { false }
+    end
   end
 
-  def manage?
-    user.admin?
-  end
+  %i[destroy? suspend? unsuspend?].each do |rule|
+    describe_rule rule do
+      succeed "when an admin user asks"
+      failed "when user asks for itself" do
+        let(:record) { user }
+      end
 
-  def suspend?
-    user.admin? && user != record
-  end
-
-  def unsuspend?
-    suspend?
+      failed "when user is not admin" do
+        let(:is_admin) { false }
+      end
+    end
   end
 end
