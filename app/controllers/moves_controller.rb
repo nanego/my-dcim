fc83c68749e329701272ce70f518f7e156c62fb0
Nicolas Brousse <n.brousse@pantographe.studio>
2025-08-06 17:47:25 +0200

    wip

--- app/controllers/moves_controller.rb
+++ app/controllers/moves_controller.rb
@@ -28,16 +28,28 @@ class MovesController < ApplicationController # rubocop:disable Metrics/ClassLen
 
   def new
     @move = @moves_project_step.moves.build(moveable_type: "Server")
+    @move.moveable = Server.friendly.select(:id, :slug).find(params[:server_id]) if params[:server_id]
 
-    if params[:server_id].present?
-      @move.moveable = Server.friendly.select(:id, :slug).find(params[:server_id])
-      render :new_with_server
-    end
+    render :new_unscoped unless @moves_project_step.persisted?
   end
 
   def edit; end
 
   def create
+    if params[:unscoped]
+      @move = Move.new(moveable_type: "Server", position: 1, frame: Frame.new, prev_frame: Frame.new)
+      @move.assign_attributes(unscoped_move_params)
+
+      if @move.valid?
+        redirect_to new_moves_project_step_move_path(@move.step, server_id: @move.moveable)
+      else
+        load_form_data
+        render :new_unscoped
+      end
+
+      return
+    end
+
     @move = @moves_project_step.moves.build(move_params)
 
     if params[:move][:remove_connections] == 'Oui'
@@ -179,9 +191,9 @@ class MovesController < ApplicationController # rubocop:disable Metrics/ClassLen
   def set_moves_project_step
     @moves_project_step = MovesProjectStep.new
 
-    if params[:moves_project_step_id]
-      @moves_project_step = MovesProjectStep.find(params[:moves_project_step_id])
-    end
+    return unless params[:moves_project_step_id]
+
+    @moves_project_step = MovesProjectStep.find(params[:moves_project_step_id])
   end
 
   def redirect_if_archived
@@ -199,6 +211,10 @@ class MovesController < ApplicationController # rubocop:disable Metrics/ClassLen
   # Never trust parameters from the scary internet, only allow the white list through.
   def move_params
     params.expect(move: %i[moveable_type moveable_id frame_id position])
+  end
+
+  def unscoped_move_params
+    params.expect(move: %i[moveable_type moveable_id moves_project_step_id])
   end
 
   def moved_connection_params
