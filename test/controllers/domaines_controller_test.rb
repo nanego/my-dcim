0d97b4e6c0ff566b20e0778b0e194656e6168c75
Maximilien Bausson <m.bausson@pantographe.studio>
2025-08-06 11:39:38 +0200

    Fixed test cases

--- test/controllers/domaines_controller_test.rb
+++ test/controllers/domaines_controller_test.rb
@@ -4,7 +4,7 @@ require File.expand_path("../../test_helper", __FILE__)
 
 class DomainesControllerTest < ActionController::TestCase
   setup do
-    sign_in users(:one)
+    sign_in users(:admin)
     @domaine = domaines(:switch)
   end
 
