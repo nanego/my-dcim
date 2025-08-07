d9de2ad33c0a10b5df262e041304eba548267d36
Maximilien Bausson <m.bausson@pantographe.studio>
2025-08-07 17:49:51 +0200

    CI fix

--- app/controllers/data_import_controller.rb
+++ app/controllers/data_import_controller.rb
@@ -4,22 +4,16 @@ class DataImportController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:ansible]
 
   def index
-    authorize!
+    authorize! with: DataImportPolicy
   end
 
   def ansible
-    authorize!
+    authorize! with: DataImportPolicy
 
     respond_to do |format|
       format.html
       format.js
       format.json
     end
-  end
-
-  private
-
-  def default_authorization_policy_class
-    DataImportPolicy
   end
 end
