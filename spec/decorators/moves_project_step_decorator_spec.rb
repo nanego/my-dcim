a55a738f2e1c6c1be64fc90aa908b6422b99925a
Nicolas Brousse <n.brousse@pantographe.studio>
2025-08-13 15:00:18 +0200

    tests

--- spec/decorators/moves_project_step_decorator_spec.rb
+++ spec/decorators/moves_project_step_decorator_spec.rb
@@ -1,17 +1,44 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
+require "rails_helper"
 
-# Example:
-#
-# describe MovesProjectStepDecorator, type: :decorator do
-#   let(:object) { User.new(first_name: "John", last_name: "Doe") }
-#   let(:decorated_user) { described_class.new(object) }
-#
-#   describe "#full_name" do
-#     it { expect(decorated_user.full_name).to eq("John Doe") }
-#   end
-# end
 RSpec.describe MovesProjectStepDecorator, type: :decorator do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe ".grouped_by_project_options_for_select" do
+    it do
+      expect(described_class.grouped_by_project_options_for_select)
+        .to have_tag("optgroup", with: { label: "A" }) do
+          with_tag("option", with: { value: "1" }, text: "A")
+        end
+    end
+
+    it do
+      expect(described_class.grouped_by_project_options_for_select)
+        .to have_tag("optgroup", with: { label: "B" }) do
+          with_tag("option", with: { value: "2" }, text: "B")
+        end
+    end
+
+    it do
+      expect(described_class.grouped_by_project_options_for_select)
+        .to have_tag("optgroup", with: { label: "C" }) do
+          with_tag("option", with: { value: "3" }, text: "C")
+        end
+    end
+
+    it do
+      expect(described_class.grouped_by_project_options_for_select)
+        .to have_tag("optgroup", with: { label: "D" }) do
+          with_tag("option", with: { value: "4" }, text: "D")
+        end
+    end
+
+    it do
+      expect(described_class.grouped_by_project_options_for_select)
+        .to have_tag("optgroup", with: { label: "F" }) do
+          with_tag("option", with: { value: "6" }, text: "Step 1")
+          with_tag("option", with: { value: "7" }, text: "Step 2")
+          with_tag("option", with: { value: "8" }, text: "Step 3")
+        end
+    end
+  end
 end
