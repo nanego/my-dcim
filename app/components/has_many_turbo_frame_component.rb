87978271a8533bf2dd022d493dceb67953ced437
B_Rass <b.rasschaert@pantographe.studio>
2025-08-20 15:35:07 +0200

    Update button skin + has_many frame design

--- app/components/has_many_turbo_frame_component.rb
+++ app/components/has_many_turbo_frame_component.rb
@@ -7,31 +7,28 @@ class HasManyTurboFrameComponent < ApplicationComponent
 
   erb_template <<~ERB
     <%= render CardComponent.new(type: @type, extra_classes: @extra_classes, **@html_options) do |c| %>
-      <% c.with_header do %>
-        <div class="d-flex justify-content-between">
-          <span><%= @title %></span>
-
-          <% if actions? %>
-            <%= actions %>
-          <% else %>
-          <span>
-            <%= link_to @url, class: class_names("link-light", "link-dark": @type == :warning) do %>
-              <%= t(".see_more_filters") %>
-              <% end %>
-            <% end %>
-
-            <% if @new_path %>
-              <%= render ButtonComponent.new(@new_label,
-                                            url: @new_path,
-                                            icon: "plus-lg",
-                                            size: :sm,
-                                            variant: :light,
-                                            is_responsive: true,
-                                            extra_classes: "ms-3") %>
-            <% end %>
-          </span>
-        </div>
+      <div class="d-flex justify-content-between align-items-center mb-3">
+        <span class="d-inline-flex align-items-center">
+          <h4 class="mb-0"><%= @title %></h4>
+          <%= link_to @url, class: "ms-2 link-secondary" do %>
+            <span class="bi bi-funnel-fill"
+                  title="<%= t(".see_more_filters") %>"
+                  aria-hidden="true"
+                  data-controller="tooltip"
+                  data-bs-placement="right"
+            ></span>
+          <% end %>
+        </span>
+      <% if actions? %>
+        <%= actions %>
+      <% elsif @new_path %>
+        <%= link_to @new_path,
+                    class: "link-success link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover small" do %>
+          <span class="bi bi-plus-lg"></span>
+          <span><%= @new_label %></span>
+        <% end %>
       <% end %>
+    </div>
 
       <%= turbo_frame_tag(@frame_id, loading: :lazy, src: @url) do %>
         <div class="w-100 d-flex justify-content-center align-items-center gap-3">
