4b1027aad1ab30770619e9209b97b2225751d058
Maximilien Bausson <m.bausson@pantographe.studio>
2025-08-20 15:45:44 +0200

    Added export to servers

--- app/decorators/moved_connection_decorator.rb
+++ app/decorators/moved_connection_decorator.rb
@@ -2,8 +2,8 @@
 
 class MovedConnectionDecorator < ApplicationDecorator
   def description
-    "Connexion entre #{port_from.server.try(:modele).try(:category)} #{port_from.server} (port ##{port_from_id}) et" \
-      "#{port_to.try(:server).try(:modele).try(:category)} #{port_to.try(:server)} (port ##{port_to_id}) => vlans:#{vlans}" \
+    "Connexion entre #{port_from.server.try(:modele).try(:category)} #{port_from.server} (port ##{port_from_id}) et " \
+      "#{port_to.try(:server).try(:modele).try(:category)} #{port_to.try(:server)} (port ##{port_to_id}) => vlans:#{vlans} " \
       "cablename:#{cablename} couleur:#{color}"
   end
 end
