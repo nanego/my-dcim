3c0e89f87d19b93561395edc7cb92606ee4e9e70
Maximilien Bausson <m.bausson@pantographe.studio>
2025-08-20 14:44:05 +0200

    Added export view

---
+++ app/decorators/moved_connection_decorator.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class MovedConnectionDecorator < ApplicationDecorator
+  def description
+    "Connexion entre #{port_from.server.try(:modele).try(:category)} #{port_from.server} (port ##{port_from_id}) et" \
+      "#{port_to.try(:server).try(:modele).try(:category)} #{port_to.try(:server)} (port ##{port_to_id}) => vlans:#{vlans}" \
+      "cablename:#{cablename} couleur:#{color}"
+  end
+end
