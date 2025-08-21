af15cccd2671e3ab4c43bd3211e5722bc9d7d457
Maximilien Bausson <m.bausson@pantographe.studio>
2025-08-21 16:54:34 +0200

    Refactored policies spec to use new shared examples

--- spec/policies/moves_project_step_policy_spec.rb
+++ spec/policies/moves_project_step_policy_spec.rb
@@ -3,77 +3,11 @@
 require "rails_helper"
 
 RSpec.describe MovesProjectStepPolicy, type: :policy do
-  let(:user) { users(:one) }
-  let(:context) { { user: } }
-  let(:is_admin) { false }
-  let(:role) { nil }
-
-  before do
-    user.is_admin = is_admin
-    user.role = role
-  end
-
   it_behaves_like "with default index policy"
   it_behaves_like "with default create policy"
   it_behaves_like "with default manage policy"
 
-  describe_rule :frame? do
-    context "when user is admin" do # rubocop:disable Spec/EmptyExampleGroup
-      succeed "when an admin user asks" do
-        let(:is_admin) { true }
-      end
-    end
-
-    context "when user is not admin" do # rubocop:disable Spec/EmptyExampleGroup
-      failed "when user with no role asks"
-
-      succeed "when a reader user asks" do
-        let(:role) { :reader }
-      end
-
-      succeed "when a writer user asks" do
-        let(:role) { :writer }
-      end
-    end
-  end
-
-  describe_rule :print? do
-    context "when user is admin" do # rubocop:disable Spec/EmptyExampleGroup
-      succeed "when an admin user asks" do
-        let(:is_admin) { true }
-      end
-    end
-
-    context "when user is not admin" do # rubocop:disable Spec/EmptyExampleGroup
-      failed "when user with no role asks"
-
-      succeed "when a reader user asks" do
-        let(:role) { :reader }
-      end
-
-      succeed "when a writer user asks" do
-        let(:role) { :writer }
-      end
-    end
-  end
-
-  describe_rule :execute? do
-    context "when user is admin" do # rubocop:disable Spec/EmptyExampleGroup
-      succeed "when an admin user asks" do
-        let(:is_admin) { true }
-      end
-    end
-
-    context "when user is not admin" do # rubocop:disable Spec/EmptyExampleGroup
-      failed "when user with no role asks"
-
-      failed "when a reader user asks" do
-        let(:role) { :reader }
-      end
-
-      succeed "when a writer user asks" do
-        let(:role) { :writer }
-      end
-    end
-  end
+  it_behaves_like "act as index policy", for: :frame?
+  it_behaves_like "act as index policy", for: :print?
+  it_behaves_like "act as manage policy", for: :execute?
 end
