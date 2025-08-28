e21bef1707639e101a77aedc85f1553460125d6f
Maximilien Bausson <m.bausson@pantographe.studio>
2025-08-28 15:07:33 +0200

    Quality fix

--- app/decorators/moved_connection_decorator.rb
+++ app/decorators/moved_connection_decorator.rb
@@ -2,17 +2,15 @@
 
 class MovedConnectionDecorator < ApplicationDecorator
   def description
-    MovedConnection.human_attribute_name(
-      :description,
-      from_server_category: port_from.server.try(:modele).try(:category),
-      from_server: port_from.server,
-      from_port_id: port_from_id,
-      to_server_category: port_to.try(:server).try(:modele).try(:category),
-      to_server: port_to.try(:server),
-      to_port_id: port_to_id,
-      vlans:,
-      cablename:,
-      color:
-    )
+    MovedConnection.human_attribute_name(:description,
+                                         from_server_category: port_from.server.try(:modele).try(:category),
+                                         from_server: port_from.server,
+                                         from_port_id: port_from_id,
+                                         to_server_category: port_to.try(:server).try(:modele).try(:category),
+                                         to_server: port_to.try(:server),
+                                         to_port_id: port_to_id,
+                                         vlans:,
+                                         cablename:,
+                                         color:)
   end
 end
