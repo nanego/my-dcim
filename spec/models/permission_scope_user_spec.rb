61ed99a73595f290be65a8f600499127b970ea93
Nicolas Brousse <n.brousse@pantographe.studio>
2025-09-12 16:01:29 +0200

    tests

--- spec/models/permission_scope_user_spec.rb
+++ spec/models/permission_scope_user_spec.rb
@@ -3,5 +3,16 @@
 require "rails_helper"
 
 RSpec.describe PermissionScopeUser do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:permission_scope_user) do
+    described_class.new(permission_scope: permission_scopes(:one), user: users(:one))
+  end
+
+  describe "associations" do
+    it { is_expected.to belong_to(:permission_scope) }
+    it { is_expected.to belong_to(:user) }
+  end
+
+  describe "validations" do
+    it { is_expected.to be_valid }
+  end
 end
