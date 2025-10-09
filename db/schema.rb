a866e8293727464e14fc865dfd01645754716d72
B-Rass <B-Rass@users.noreply.github.com>
2025-10-09 19:08:27 +0200

    User: improvments on index and model (#487)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_10_01_135052) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_01_154348) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -514,7 +514,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_10_01_135052) do
   end
 
   create_table "users", id: :serial, force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at", precision: nil
