8ba034b9836beff0fba2b68e3705ef07c1972958
B_Rass <b.rasschaert@pantographe.studio>
2025-10-09 18:14:23 +0200

    CardEmptyDataComponent: update to be able to send custom text

--- app/components/card_empty_data_component.rb
+++ app/components/card_empty_data_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class CardEmptyDataComponent < ApplicationComponent
-  def initialize(icon: :table, **_options)
-    @icon = icon
+  def initialize(icon: :table, text: I18n.t("card_empty_data_component.title"), **_options)
+    @icon = icon.to_s.dasherize
+    @text = text
 
     super()
   end
@@ -10,7 +11,7 @@ class CardEmptyDataComponent < ApplicationComponent
   def call
     render CardComponent.new(extra_classes: "text-center text-secondary-emphasis") do
       concat(tag.span(class: "bi bi-#{@icon} fs-1 text-secondary text-opacity-25"))
-      concat(tag.h5(t(".title"), class: "card-title mt-3"))
+      concat(tag.h5(@text, class: "card-title mt-3")) if @text.present?
     end
   end
 end
