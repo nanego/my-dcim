8d5c918ab826a0bf6eb98022bd2505d7f3fa6048
Nicolas Brousse <n.brousse@pantographe.studio>
2025-10-15 15:44:20 +0200

    scope cables

--- app/policies/cable_policy.rb
+++ app/policies/cable_policy.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 class CablePolicy < ApplicationPolicy
+  relation_scope do |relation|
+    return relation if user.admin?
+
+    relation.joins(:servers).where(servers: authorized_scope(Server.all))
+  end
 end
