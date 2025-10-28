3e20f6461a2e9b5483684a55fdef135056e5841f
Félix <ffilmont@MacBook-Pro.local>
2025-10-28 13:38:36 +0100

    test for delete_dependency_component

---
+++ spec/components/delete_dependency_component_spec.rb
@@ -0,0 +1,67 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe DeleteDependencyComponent, type: :component do
+  let(:confirmation_path) { "/confirmation-path" }
+  let(:component) do
+    # stubbing url_for since it cannot found url for stubbed records
+    component = described_class.new(record, confirmation_path)
+    allow(component).to receive(:url_for).and_return("/fake/url")
+    component
+  end
+
+  let(:rendered_component) { render_inline(component).to_html }
+
+  context "with restricting dependency" do
+    let(:record) do
+      server = Server.new(name: "Dummy name")
+      document = Document.new
+      allow(document).to receive_messages(
+        document: instance_double(DocumentUploader::UploadedFile, metadata: { filename: "this is a filename" }),
+        document_url: "/fake/url",
+      )
+
+      allow(server).to receive_messages(
+        external_app_record: [],
+        documents: [document],
+        moves: [],
+      )
+
+      server
+    end
+
+    it { expect(rendered_component).to have_tag("h4", title: Document.model_name.human) }
+    it { expect(rendered_component).to have_tag("a[target=_blank]", href: "/fake/url", title: "this is a filename") }
+    it { expect(rendered_component).not_to have_tag("a.btn-danger") }
+    it { expect(rendered_component).not_to have_tag("a.btn-default") }
+  end
+
+  context "without restricting dependency" do
+    describe "with destroy dependency" do
+      let(:record) do
+        contact = Contact.new(first_name: "Dummy", last_name: "name")
+        allow(contact).to receive(:contact_assignments).and_return([ContactAssignment.new])
+        contact
+      end
+
+      it { expect(rendered_component).to have_tag("a.btn-danger", href: "confirmation-path") }
+      it { expect(rendered_component).to have_tag("a.btn-default") }
+      it { expect(rendered_component).to have_tag("h4", title: ContactAssignment.model_name.human) }
+      it { expect(rendered_component).to have_tag("a[target=_blank]", href: "/fake/url", title: ContactAssignment.new.to_s) }
+    end
+
+    describe "without destroy dependency" do
+      let(:record) do
+        contact = Contact.new(first_name: "Dummy", last_name: "name")
+        allow(contact).to receive(:contact_assignments).and_return([])
+        contact
+      end
+
+      it { expect(rendered_component).to have_tag("a.btn-danger", href: "confirmation-path") }
+      it { expect(rendered_component).to have_tag("a.btn-default") }
+      it { expect(rendered_component).not_to have_tag("h4", title: ContactAssignment.model_name.human) }
+      it { expect(rendered_component).not_to have_tag("a[target=_blank]", href: "/fake/url", title: ContactAssignment.new.to_s) }
+    end
+  end
+end
