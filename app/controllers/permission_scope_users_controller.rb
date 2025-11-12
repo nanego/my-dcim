0489736cb8d5dcd80de59b7ac14c6c10d4ce3052
Nicolas Brousse <n.brousse@pantographe.studio>
2025-11-12 15:44:00 +0100

    wip

--- app/controllers/permission_scope_users_controller.rb
+++ app/controllers/permission_scope_users_controller.rb
@@ -6,47 +6,46 @@ class PermissionScopeUsersController < ApplicationController
 
   # POST /permission_scope_users
   def create
-    @permission_scope_user = authorize! PermissionScopeUser.new(permission_scope_user_params)
+    @permission_scope_user = authorize! @permission_scope.permission_scope_users.build(permission_scope_user_params)
 
     respond_to do |format|
       if @permission_scope_user.save
-        format.html { redirect_to @permission_scope, notice: t(".flashes.created") }
-        format.turbo_stream
+        flash[:notice] = t(".flashes.created")
+
+        format.html { redirect_to @permission_scope }
+        format.turbo_stream { render status: :see_other }
       else
         format.html { render :new, status: :unprocessable_content }
-        format.turbo_stream
+        format.turbo_stream { render :new, status: :unprocessable_content }
       end
     end
   end
 
   # DELETE /permission_scope_users/1
   def destroy
-    @permission_scope.destroy!
+    @permission_scope_user.destroy!
+
+    flash[:notice] = t(".flashes.destroyed") # rubocop:disable Rails/ActionControllerFlashBeforeRender
 
     respond_to do |format|
-      format.html { redirect_to permission_scope_path(@permission_scope), status: :see_other, notice: t(".flashes.destroyed") }
-      format.turbo_stream
+      format.html { redirect_to permission_scope_path(@permission_scope), status: :see_other }
+      format.turbo_stream { render status: :see_other }
     end
   end
 
   private
 
   def set_permission_scope
-    @permission_scope = authorize! PermissionScope.find(params[:permission_scope_id])
+    @permission_scope = PermissionScope.find(params[:permission_scope_id])
   end
 
   # Use callbacks to share common setup or constraints between actions.
   def set_permission_scope_user
-    @permission_scope_user = @permission_scope.permission_scope_users.find(params[:id])
+    @permission_scope_user = authorize! @permission_scope.permission_scope_users.find(params[:id])
   end
 
   # Only allow a list of trusted parameters through.
   def permission_scope_user_params
-    params.expect(
-      permission_scope: [
-        :name, :role, :all_domains,
-        { domaine_ids: [], user_ids: [] },
-      ],
-    )
+    params.expect(permission_scope_user: %i[user_id])
   end
 end
