6668a3b924491b6a312eb25cc32bfa70f769f11b
Nicolas Brousse <n.brousse@pantographe.studio>
2025-11-26 17:56:36 +0100

    Rework tests and code cleanup

--- spec/models/server_spec.rb
+++ spec/models/server_spec.rb
@@ -11,14 +11,16 @@ RSpec.describe Server do
 
   describe "associations" do
     it { is_expected.to belong_to(:frame) }
-    it { is_expected.to have_one(:bay).through(:frame) }
-    it { is_expected.to have_one(:islet).through(:frame) }
-    it { is_expected.to have_one(:room).through(:islet) }
     it { is_expected.to belong_to(:gestion).optional(true) }
     it { is_expected.to belong_to(:domaine).optional(true) }
     it { is_expected.to belong_to(:modele) }
     it { is_expected.to belong_to(:cluster).optional(true) }
     it { is_expected.to belong_to(:stack).optional(true) }
+
+    it { is_expected.to have_one(:bay).through(:frame) }
+    it { is_expected.to have_one(:islet).through(:frame) }
+    it { is_expected.to have_one(:room).through(:islet) }
+
     it { is_expected.to have_many(:cards) }
     it { is_expected.to have_many(:card_types).through(:cards) }
     it { is_expected.to have_many(:ports).through(:cards) }
