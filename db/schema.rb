e879811c816fa93c6cd8f0d42ffab1d72cfded7d
B-Rass <B-Rass@users.noreply.github.com>
2025-12-10 17:28:48 +0100

    PermissionScopes: add is_system attr + update view to disable delete btn (#525)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_11_12_141428) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_03_163256) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -428,6 +428,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_11_12_141428) do
     t.boolean "all_domains", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_system", default: false, null: false
   end
 
   create_table "port_types", id: :serial, force: :cascade do |t|
