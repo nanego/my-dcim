66dccc689b1f93bba0193f3b0c40bf5e5c3d4c9e
Félix <ffilmont@MacBook-Pro.local>
2026-01-05 18:01:02 +0100

    basic manufacturer filter

--- app/processors/servers_processor.rb
+++ app/processors/servers_processor.rb
@@ -3,7 +3,7 @@
 class ServersProcessor < ApplicationProcessor
   include Sortable
 
-  SORTABLE_FIELDS = %w[name numero categories.name rooms.name islets.name manufacturers.name bays.id position critique comment critique side slug modele.u].freeze
+  SORTABLE_FIELDS = %w[name numero categories.name rooms.name islets.name manufacturers.name bays.id position modele.u slug side comment critique].freeze
 
   map :q do |q:|
     server_table = Server.arel_table
