a85a7176547e49d4bc9357418c5d50431c69fd38
Félix <ffilmont@MacBook-Pro.local>
2026-01-07 18:06:10 +0100

    updating UI about categories in forms and tables and adding decorator for glpi_sync

--- app/models/external_app_record_setting.rb
+++ app/models/external_app_record_setting.rb
@@ -3,18 +3,20 @@
 class ExternalAppRecordSetting
   include ActiveModel::Model
 
-  attr_accessor :category_ids
+  attr_accessor :category_glpi_syncs
 
   def initialize(attributes = {})
-    @category_ids = Category.glpi_synchronizable.ids
+    @category_glpi_syncs = Category.glpi_synchronizable.to_h { |c| [c.id, c.glpi_sync] }
 
     super
   end
 
   def save
     Category.transaction do
-      Category.update_all(is_glpi_synchronizable: false) # rubocop:disable Rails/SkipsModelValidations
-      Category.where(id: @category_ids).update_all(is_glpi_synchronizable: true) # rubocop:disable Rails/SkipsModelValidations
+      Category.update_all(glpi_sync: 0) # rubocop:disable Rails/SkipsModelValidations
+      @category_glpi_syncs.each do |id, glpi_sync|
+        Category.where(id: id).update_all(glpi_sync: Category.glpi_syncs[glpi_sync] || 0) # rubocop:disable Rails/SkipsModelValidations
+      end
     end
   end
 end
--- app/processors/categories_processor.rb
+++ app/processors/categories_processor.rb
@@ -3,7 +3,7 @@
 class CategoriesProcessor < ApplicationProcessor
   include Sortable
 
-  SORTABLE_FIELDS = %w[id name modeles_count is_glpi_synchronizable].freeze
+  SORTABLE_FIELDS = %w[id name modeles_count glpi_sync].freeze
 
   sortable fields: SORTABLE_FIELDS
 end
