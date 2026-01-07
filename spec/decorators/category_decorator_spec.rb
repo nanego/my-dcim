9ace2586e1a0249087c3b2b23e26b8c4f9a5f04d
Félix <ffilmont@MacBook-Pro.local>
2026-01-07 18:06:10 +0100

    updating UI about categories in forms and tables and adding decorator for glpi_sync

--- spec/decorators/category_decorator_spec.rb
+++ spec/decorators/category_decorator_spec.rb
@@ -3,35 +3,12 @@
 require "rails_helper"
 
 RSpec.describe CategoryDecorator, type: :decorator do
-  let(:object) { categories(:one) }
+  let(:object) { Category.new(name: "some name", description: "some description", glpi_sync: :no) }
   let(:decorated_category) { described_class.decorate(object) }
 
-  describe ".glpi_sync_type_options_for_select" do
+  describe ".glpi_sync_human" do
     it do
-      expect(described_class.glpi_sync_type_options_for_select).to eq(
-        [["Aucun", "none"], ["Serveur", "server"], ["Équipement réseau", "network_equipment"]],
-      )
-    end
-  end
-
-  describe ".glpi_sync_type_human_for" do
-    it do
-      expect(described_class.glpi_sync_type_human_for(:no)).to eq(Category.human_attribute_name("glpi_sync_type.no"))
-    end
-  end
-
-  describe "#glpi_sync_type_human" do
-    it do
-      expect(decorated_category.glpi_sync_type_human).to eq(Category.human_attribute_name("glpi_sync_type.server"))
-    end
-
-    context "with glpi_sync_type is equal to no" do
-      before { object.glpi_sync_type = "none" }
-
-      it do
-        expect(decorated_category.glpi_sync_type_human)
-          .to have_tag("span.fst-italic.fw-light.text-body-secondary", text: "n/c")
-      end
+      expect(decorated_category).to eq(Category.human_attribute_name(:no_glpi_sync))
     end
   end
 end
