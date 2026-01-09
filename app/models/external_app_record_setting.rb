8d1ddbdd01b870084533d39d987109049c1276bd
Félix <ffilmont@MacBook-Pro.local>
2026-01-09 16:39:49 +0100

    modifying external_app_record to use new glpi enum and adding tests

--- app/models/external_app_record_setting.rb
+++ app/models/external_app_record_setting.rb
@@ -15,7 +15,7 @@ class ExternalAppRecordSetting
     Category.transaction do
       Category.update_all(glpi_sync: 0) # rubocop:disable Rails/SkipsModelValidations
       @category_glpi_syncs.each do |id, glpi_sync|
-        Category.where(id: id).update_all(glpi_sync: Category.glpi_syncs[glpi_sync] || 0) # rubocop:disable Rails/SkipsModelValidations
+        Category.find(id).update(glpi_sync: Category.glpi_syncs[glpi_sync] || 0)
       end
     end
   end
