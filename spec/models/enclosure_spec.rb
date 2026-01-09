b2bff134aed1eb8a50c527dbfd981e1a2a0267fd
infdev-fil <infdevFil@protonmail.com>
2026-01-09 15:29:13 +0100

    425932 multiple composants bug (#542)

--- spec/models/enclosure_spec.rb
+++ spec/models/enclosure_spec.rb
@@ -10,6 +10,41 @@ RSpec.describe Enclosure do
     it { is_expected.to have_many(:composants).dependent(:destroy) }
   end
 
+  describe ".with_composants" do
+    subject(:with_composants) { modele.enclosures.with_composants }
+
+    let(:modele)    { modeles(:four) }
+    let(:enclosure) { described_class.create!(position: 0, modele:) }
+
+    before { enclosure }
+
+    context "without composants in enclosure" do
+      it { is_expected.not_to include(enclosure) }
+    end
+
+    context "with two composants in enclosure" do
+      it do
+        expect do
+          Composant.create!(position: 0, name: "c1", enclosure:)
+          Composant.create!(position: 1, name: "c2", enclosure:)
+        end.to change(with_composants, :count).from(0).to(1)
+      end
+    end
+
+    context "with empty enclosure" do
+      before do
+        Composant.create!(position: 0, name: "c1", enclosure:)
+        Composant.create!(position: 1, name: "c2", enclosure:)
+      end
+
+      it do
+        expect do
+          described_class.create!(position: 0, modele: modeles(:four))
+        end.not_to change(with_composants, :count)
+      end
+    end
+  end
+
   describe "#deep_dup" do
     subject(:enclosure) { enclosures(:one) }
 
