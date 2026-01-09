b8d3e147e898c156e30d91463d57b5ace25241c8
Félix <ffilmont@MacBook-Pro.local>
2026-01-09 16:39:49 +0100

    modifying external_app_record to use new glpi enum and adding tests

--- spec/models/external_app_record_setting_spec.rb
+++ spec/models/external_app_record_setting_spec.rb
@@ -5,30 +5,30 @@ require "rails_helper"
 RSpec.describe ExternalAppRecordSetting do
   subject(:settings) { described_class.new(attributes) }
 
-  let(:category) { Category.create!(id: 20, name: "name", description: "description", glpi_sync_type: :network_equipment) }
+  let(:category) { Category.create!(id: 20, name: "name", description: "description", glpi_sync: :network_equipment) }
   let(:attributes) { {} }
 
   before { category }
 
   describe "#category_ids" do
-    it { expect(settings.category_glpi_sync_types).to eq({ 1 => "server", 20 => "network_equipment" }) }
+    it { expect(settings.category_glpi_syncs).to eq({ 1 => "server", 20 => "network_equipment" }) }
   end
 
   describe "#save" do
-    let(:attributes) { { category_glpi_sync_types: { 20 => :server } } }
+    let(:attributes) { { category_glpi_syncs: { 20 => :server } } }
 
     it :aggregate_failures do # rubocop:disable RSpec/ExampleLength
-      expect(categories(:one).glpi_sync_type_server?).to be(true)
-      expect(category.glpi_sync_type_network_equipment?).to be(true)
+      expect(categories(:one).server_glpi_sync?).to be(true)
+      expect(category.network_equipment_glpi_sync?).to be(true)
 
       settings.save
       categories(:one).reload
       category.reload
 
-      expect(categories(:one).glpi_sync_type_none?).to be(true)
-      expect(category.glpi_sync_type_server?).to be(true)
+      expect(categories(:one).no_glpi_sync?).to be(true)
+      expect(category.server_glpi_sync?).to be(true)
 
-      expect(described_class.new.category_glpi_sync_types).to eq({ 20 => "server" })
+      expect(described_class.new.category_glpi_syncs).to eq({ 20 => "server" })
     end
   end
 end
