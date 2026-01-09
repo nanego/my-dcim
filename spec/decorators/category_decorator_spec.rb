a26b1359e438cc7b619c525cc5496313b75376a7
Félix <ffilmont@MacBook-Pro.local>
2026-01-09 16:39:49 +0100

    modifying external_app_record to use new glpi enum and adding tests

--- spec/decorators/category_decorator_spec.rb
+++ spec/decorators/category_decorator_spec.rb
@@ -3,12 +3,22 @@
 require "rails_helper"
 
 RSpec.describe CategoryDecorator, type: :decorator do
-  let(:object) { Category.new(name: "some name", description: "some description", glpi_sync: :no) }
+  let(:object) { categories(:one) }
   let(:decorated_category) { described_class.decorate(object) }
 
   describe ".glpi_sync_human" do
     it do
-      expect(decorated_category).to eq(Category.human_attribute_name(:no_glpi_sync))
+      expect(decorated_category.glpi_sync_human).to eq(Category.human_attribute_name(:server_glpi_sync))
+    end
+  end
+
+  describe ".glpi_sync_options_for_select" do
+    it do
+      expect(described_class.glpi_sync_options_for_select).to eq(
+        %i[no server network_equipment].map do |s|
+          [Category.human_attribute_name("#{s}_glpi_sync"), s.to_s]
+        end,
+      )
     end
   end
 end
