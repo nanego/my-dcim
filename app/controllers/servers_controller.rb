05411ce71e726fd9933f6a0721796d2144c4ed60
Nicolas Brousse <n.brousse@pantographe.studio>
2026-01-20 17:09:17 +0100

    refactor: rework ServerProcessor SORTABLE_FIELDS and AVAILABLE_COLUMNS to be consistent
    And fix room column

--- app/controllers/servers_controller.rb
+++ app/controllers/servers_controller.rb
@@ -6,8 +6,8 @@ class ServersController < ApplicationController # rubocop:disable Metrics/ClassL
   include ColumnsPreferences
 
   DEFAULT_COLUMNS = %w[name numero modele_category_id islet_id bay_id network_types position].freeze
-  AVAILABLE_COLUMNS = %w[name numero modele_category_id islet_id manufacturer_id bay_id network_types position gestion_id
-                         frame_id cluster_id stack_id domaine_id modele_id u slug side color comment critique].freeze
+  AVAILABLE_COLUMNS = %w[name numero modele_category_id room_id islet_id manufacturer_id bay_id network_types position gestion_id
+                         domaine_id modele_id u frame_id cluster_id stack_id slug side color comment critique].freeze
 
   columns_preferences_with model: Server, default: DEFAULT_COLUMNS, available: AVAILABLE_COLUMNS, only: %i[index export]
 
