a434c3564f42a23bfef609d80d189dc65df06200
B-Rass <B-Rass@users.noreply.github.com>
2026-01-22 18:39:24 +0100

    421856 add preview visualization on server edit (#572)

--- app/controllers/power_distribution_units_controller.rb
+++ app/controllers/power_distribution_units_controller.rb
@@ -47,13 +47,21 @@ class PowerDistributionUnitsController < ApplicationController
   end
 
   def update
-    respond_to do |format|
-      if @pdu.update(pdu_params)
-        format.html { redirect_to power_distribution_unit_path(@pdu), notice: t(".flashes.updated") }
-        format.json { render :show, status: :ok, location: @pdu }
-      else
-        format.html { render :edit }
-        format.json { render json: @pdu.errors, status: :unprocessable_content }
+    @pdu.assign_attributes(pdu_params)
+
+    if params[:preview]
+      respond_to do |format|
+        format.turbo_stream { render :preview, status: :unprocessable_content }
+      end
+    else
+      respond_to do |format|
+        if @pdu.save
+          format.html { redirect_to power_distribution_unit_path(@pdu), notice: t(".flashes.updated") }
+          format.json { render :show, status: :ok, location: @pdu }
+        else
+          format.html { render :edit }
+          format.json { render json: @pdu.errors, status: :unprocessable_content }
+        end
       end
     end
   end
