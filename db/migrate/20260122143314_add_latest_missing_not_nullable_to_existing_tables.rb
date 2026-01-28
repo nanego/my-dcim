c10eb965802972a3e9bbaa4f9e0880cd8bc15a9a
Nicolas Brousse <n.brousse@pantographe.studio>
2026-01-28 14:51:50 +0100

    wip

---
+++ db/migrate/20260122143314_add_latest_missing_not_nullable_to_existing_tables.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class AddLatestMissingNotNullableToExistingTables < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :ports, :card_id, false
+
+    change_column_null :moved_connections, :port_from_id, false
+
+    change_column_null :modeles, :manufacturer_id, false
+    change_column_null :modeles, :architecture_id, false
+
+    change_column_null :connections, :port_id, false
+
+    change_column_null :cards, :composant_id, false
+
+    change_column_null :composants, :enclosure_id, false
+
+    change_column_null :servers, :modele_id, false
+  end
+end
