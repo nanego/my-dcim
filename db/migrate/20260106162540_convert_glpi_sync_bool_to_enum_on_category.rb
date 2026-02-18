7c57a673ef09156d250c426b5b31c6d0e4497566
Félix <ffilmont@MacBook-Pro.local>
2026-02-18 16:12:04 +0100

    applying review changes

--- db/migrate/20260106162540_convert_glpi_sync_bool_to_enum_on_category.rb
+++ db/migrate/20260106162540_convert_glpi_sync_bool_to_enum_on_category.rb
@@ -1,32 +1,21 @@
 # frozen_string_literal: true
 
 class ConvertGlpiSyncBoolToEnumOnCategory < ActiveRecord::Migration[8.0]
-  def up
-    # make sure table categories has no line with is_glpi_synchronizable NULL
-    execute "UPDATE categories SET is_glpi_synchronizable = false WHERE is_glpi_synchronizable IS NULL"
+  def change
+    add_column :categories, :glpi_sync, :integer, default: 0, null: false
 
-    change_table :categories, bulk: true do |t|
-      t.rename :is_glpi_synchronizable, :glpi_sync
+    up_only do
+      say_with_time "Backfilling glpi_sync from is_glpi_synchronizable" do
+        Category.reset_column_information
 
-      # remove default to let rails execute the
-      # change column then the change default
-      t.change_default :glpi_sync, nil
+        Category.where(is_glpi_synchronizable: true)
+          .update_all(glpi_sync: 1)
 
-      t.change :glpi_sync, :integer,
-               using: "CASE WHEN glpi_sync THEN 1 ELSE 0 END",
-               default: 0,
-               null: false
+        Category.where(is_glpi_synchronizable: [false, nil])
+          .update_all(glpi_sync: 0)
+      end
     end
-  end
 
-  def down
-    change_table :categories, bulk: true do |t|
-      t.rename :glpi_sync, :is_glpi_synchronizable
-
-      t.change :is_glpi_synchronizable, :boolean,
-               using: "CASE WHEN is_glpi_synchronizable = 1 THEN true ELSE false END",
-               default: false,
-               null: false
-    end
+    remove_column :categories, :is_glpi_synchronizable # rubocop:disable Rails/ReversibleMigration
   end
 end
