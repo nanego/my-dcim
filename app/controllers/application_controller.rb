c8346d824b7aeb9c5aae1864d4d0210719c12efb
Nicolas Brousse <n.brousse@pantographe.studio>
2026-04-08 16:05:55 +0200

    Missing config

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,9 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   # allow_browser versions: :modern
 
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, unless: -> { request.format.json? }
