fb0821e46aa210948571100df497f6de8556c0a6
Félix <ffilmont@MacBook-Pro.local>
2026-04-13 16:47:49 +0200

    correct rebase

--- spec/decorators/category_decorator_spec.rb
+++ spec/decorators/category_decorator_spec.rb
@@ -6,9 +6,23 @@ RSpec.describe CategoryDecorator, type: :decorator do
   let(:object) { categories(:one) }
   let(:decorated_category) { described_class.decorate(object) }
 
+  describe ".glpi_sync_type_options_for_select" do
+    it do
+      expect(described_class.glpi_sync_type_options_for_select).to eq(
+        [["Aucun", "none"], ["Serveur", "server"], ["Équipement réseau", "network_equipment"]],
+      )
+    end
+  end
+
+  describe ".glpi_sync_type_human_for" do
+    it do
+      expect(described_class.glpi_sync_type_human_for(:no)).to eq(Category.human_attribute_name("glpi_sync_type.no"))
+    end
+  end
+
   describe "#glpi_sync_type_human" do
     it do
-      expect(decorated_category.glpi_sync_type_human).to eq(Category.human_attribute_name(:glpi_sync_type_server))
+      expect(decorated_category.glpi_sync_type_human).to eq(Category.human_attribute_name("glpi_sync_type.server"))
     end
 
     context "with glpi_sync_type is equal to no" do
@@ -18,22 +32,6 @@ RSpec.describe CategoryDecorator, type: :decorator do
         expect(decorated_category.glpi_sync_type_human)
           .to have_tag("span.fst-italic.fw-light.text-body-secondary", text: "n/c")
       end
-    end
-  end
-
-  describe ".glpi_sync_type_human_for" do
-    it do
-      expect(described_class.glpi_sync_type_human_for(:no)).to eq(Category.human_attribute_name(:glpi_sync_type_no))
-    end
-  end
-
-  describe ".glpi_sync_type_options_for_select" do
-    it do
-      expect(described_class.glpi_sync_type_options_for_select).to eq(
-        %i[none server network_equipment].map do |s|
-          [Category.human_attribute_name("glpi_sync_type_#{s}"), s.to_s]
-        end,
-      )
     end
   end
 end
