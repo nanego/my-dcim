6dea3ac9abf095b19110b66ccdbc47e123d8e7a6
moxyom <infdev.fil@protonmail.com>
2026-04-15 16:36:05 +0200

    make migration rollbackabke

--- db/migrate/20260415135219_update_search_results_to_version_2.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class UpdateSearchResultsToVersion2 < ActiveRecord::Migration[8.0]
-  def change
-    update_view :search_results, version: 2, revert_to_version: 1
-  end
-end
