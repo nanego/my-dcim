d0f4c44dcf40b25c5ea5b16b71ae3df0f4a76a4c
moxyom <infdev.fil@protonmail.com>
2026-04-17 15:42:12 +0200

    correct migration

--- db/migrate/20260415155120_rename_gestions_to_managers.rb
+++ db/migrate/20260415155120_rename_gestions_to_managers.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
+class MigrationChangelogEntry < ActiveRecord::Base
+  self.table_name = :changelog_entries
+end
+
 class RenameGestionsToManagers < ActiveRecord::Migration[8.0]
-  def change
+  def up
     rename_table :gestions, :managers
     rename_column :servers, :gestion_id, :manager_id
+
+    say_with_time "Changing Gestion changelog entries type to Manager" do
+      MigrationChangelogEntry.where(object_type: "Gestion")
+        .update_all(object_type: "Manager")
+    end
+  end
+
+  def down
+    rename_table :managers, :gestions
+    rename_column :servers, :manager_id, :gestion_id
+
+    say_with_time "Changing Manager changelog entries type to Gestion" do
+      MigrationChangelogEntry.where(object_type: "Manager")
+        .update_all(object_type: "Gestion")
+    end
   end
 end
