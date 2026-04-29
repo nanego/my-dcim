e3137ab3f9e557697f9f8da66db52a3cb44536a5
Félix Filmont <f.filmont@pantographe.studio>
2026-04-29 14:58:03 +0200

    make a nice useDebounce for clarity

--- app/javascript/controllers/global_search_controller.js
+++ app/javascript/controllers/global_search_controller.js
@@ -1,15 +1,9 @@
 import { Controller } from "@hotwired/stimulus"
 import { Turbo } from "@hotwired/turbo-rails"
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-
 export default class extends Controller {
   static targets = ["searchInput"]
   static values = { onSearchPage: Boolean }
-
-  #fetchKey
 
   goToSearch() {
     if (!this.onSearchPageValue) {
@@ -20,19 +14,18 @@ export default class extends Controller {
   submit() {
     if (!this.onSearchPageValue) return
 
-    // debouce 300ms
-    const currentKey = getRandomInt(10000)
-    this.#fetchKey = currentKey
-
-    setTimeout(async () => {
-      // another input has been modified since
-      if (this.#fetchKey != currentKey) return
-
+    this.useDebounce(300, () => {
       // don't accept short queries
       let queryValue = this.searchInputTarget.value
       if (queryValue.length < 2) { queryValue = "" }
 
       this.element.requestSubmit()
-    }, 300)
+    })
+  }
+
+  #lastDebounceKey
+  useDebounce(duration, callback) {
+    if (this.#lastDebounceKey) clearTimeout(this.#lastDebounceKey)
+    this.#lastDebounceKey = setTimeout(callback, duration)
   }
 }
