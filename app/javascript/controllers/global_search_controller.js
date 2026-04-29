1b13ea2d1043bbf7e9f377e6788fc05a89f00bf2
Félix Filmont <f.filmont@pantographe.studio>
2026-04-29 14:50:47 +0200

    cleanup search js controller

--- app/javascript/controllers/global_search_controller.js
+++ app/javascript/controllers/global_search_controller.js
@@ -6,85 +6,33 @@ function getRandomInt(max) {
 }
 
 export default class extends Controller {
-  static targets = ["input", "button"]
-  static values = {
-    url: String,
-    onSearchPage: Boolean
-  }
+  static targets = ["searchInput"]
+  static values = { onSearchPage: Boolean }
 
   #fetchKey
 
-  onFocus() {
+  goToSearch() {
     if (!this.onSearchPageValue) {
-      window.location.href = this.urlValue
+      this.element.requestSubmit()
     }
   }
 
-  onBtnClick() {
-    const currentQuery = this.inputTarget.value
-
-    if (!this.onSearchPageValue) {
-      window.location.href = `${this.urlValue}?search_query=${currentQuery}`
-      return
-    }
-
-    this.search(this.inputTarget.value)
-  }
-
-  onInput() {
+  submit() {
     if (!this.onSearchPageValue) return
 
     // debouce 300ms
     const currentKey = getRandomInt(10000)
     this.#fetchKey = currentKey
 
-    this.startLoading()
     setTimeout(async () => {
       // another input has been modified since
       if (this.#fetchKey != currentKey) return
 
       // don't accept short queries
-      let queryValue = this.inputTarget.value
+      let queryValue = this.searchInputTarget.value
       if (queryValue.length < 2) { queryValue = "" }
 
-      try {
-        await this.search(queryValue)
-      } catch (e) {
-        console.error(e)
-      }
-
-      this.stopLoading()
+      this.element.requestSubmit()
     }, 300)
-  }
-
-  async search(query) {
-    const ress = await fetch(`${this.urlValue}?search_query=${query}`, {
-      headers: { Accept: "text/vnd.turbo-stream.html" }
-    })
-
-    const html = await ress.text()
-    Turbo.renderStreamMessage(html)
-
-    // update url
-    const url = new URL(window.location)
-    url.searchParams.set("search_query", query)
-    window.history.replaceState({}, "", url)
-  }
-
-  startLoading() {
-    // skip if already loading
-    if (this.buttonTarget.disabled) return
-
-    this.buttonTarget.disabled = true
-    this.buttonTarget.innerHTML = `
-      <span class="spinner-border spinner-border-sm"></span>
-    `
-  }
-
-  stopLoading() {
-    this.buttonTarget.disabled = false
-    this.buttonTarget.innerHTML = `
-      <span class="bi bi-search"></span>
-    `
   }
 }
