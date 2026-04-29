397411c61e2133a3f57438523d8c7721526c5535
infdev-fil <infdevFil@protonmail.com>
2026-04-29 16:23:42 +0200

    435840 implementing real time search (#652)

---
+++ app/javascript/controllers/global_search_controller.js
@@ -0,0 +1,30 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["searchInput"]
+  static values = { onSearchPage: Boolean }
+
+  goToSearch() {
+    if (!this.onSearchPageValue) {
+      this.element.requestSubmit()
+    }
+  }
+
+  submit() {
+    if (!this.onSearchPageValue) return
+
+    this.useDebounce(300, () => {
+      // don't accept short queries
+      let queryValue = this.searchInputTarget.value
+      if (queryValue.length < 2) { queryValue = "" }
+
+      this.element.requestSubmit()
+    })
+  }
+
+  #lastDebounceKey
+  useDebounce(duration, callback) {
+    if (this.#lastDebounceKey) clearTimeout(this.#lastDebounceKey)
+    this.#lastDebounceKey = setTimeout(callback, duration)
+  }
+}
