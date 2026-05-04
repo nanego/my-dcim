638b32e296db090cf8cb6f1884e53372d48aee6c
Félix Filmont <f.filmont@pantographe.studio>
2026-05-04 17:05:35 +0200

    wip

--- app/controllers/frames_controller.rb
+++ app/controllers/frames_controller.rb
@@ -13,6 +13,9 @@ class FramesController < ApplicationController
     authorize! @frames = scoped_frames.includes(bay: { islet: :room }).references(bay: { islet: :room })
     @filter = ProcessorFilter.new(@frames, params)
     @frames = @filter.results
+    @authorized_serveur_per_frame = authorized_scope(
+      Server.where(frame_id: @frames.map(&:id)),
+    ).group_by(&:frame_id)
   end
 
   def show
