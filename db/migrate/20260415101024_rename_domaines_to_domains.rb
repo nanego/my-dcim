117a0283a5e60e81d17418b0825bd50db716aefc
Félix Filmont <f.filmont@pantographe.studio>
2026-05-05 15:34:47 +0200

    migration fix

--- db/migrate/20260415101024_rename_domaines_to_domains.rb
+++ db/migrate/20260415101024_rename_domaines_to_domains.rb
@@ -5,33 +5,20 @@ class MigrationChangelogEntry < ActiveRecord::Base
 end
 
 class RenameDomainesToDomains < ActiveRecord::Migration[8.0]
-  def up
+  def change
     drop_view :search_results, revert_to_version: 1
 
     rename_table :domaines, :domains
     rename_column :servers, :domaine_id, :domain_id
     rename_column :permission_scope_domains, :domaine_id, :domain_id
 
-    say_with_time "Changing Domaine changelog entries type to Domain" do
-      MigrationChangelogEntry.where(object_type: "Domaine")
-        .update_all(object_type: "Domain")
-    end
-
     create_view :search_results, version: 2
-  end
 
-  def down
-    drop_view :search_results, revert_to_version: 2
-
-    say_with_time "Changing Domain changelog entries type to Domaine" do
-      MigrationChangelogEntry.where(object_type: "Domain")
-        .update_all(object_type: "Domaine")
+    reversible do |dir|
+      say_with_time "Updating changelog entries type" do
+        dir.up { MigrationChangelogEntry.where(object_type: "Domaine").update_all(object_type: "Domain") }
+        dir.down { MigrationChangelogEntry.where(object_type: "Domain").update_all(object_type: "Domaine") }
+      end
     end
-
-    rename_column :permission_scope_domains, :domain_id, :domaine_id
-    rename_column :servers, :domain_id, :domaine_id
-    rename_table :domains, :domaines
-
-    create_view :search_results, version: 1
   end
 end
