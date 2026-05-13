ceb44346e14c67586861aca3fcc95d9a745ca5e7
Félix Filmont <f.filmont@pantographe.studio>
2026-05-13 14:49:45 +0200

    real time search on all page implemented, cleanup needed

--- app/javascript/controllers/global_search_controller.js
+++ app/javascript/controllers/global_search_controller.js
@@ -2,17 +2,8 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   static targets = ["searchInput"]
-  static values = { onSearchPage: Boolean }
-
-  goToSearch() {
-    if (!this.onSearchPageValue) {
-      this.element.requestSubmit()
-    }
-  }
 
   submit() {
-    if (!this.onSearchPageValue) return
-
     this.useDebounce(300, () => {
       // don't accept short queries
       if (this.searchInputTarget.value.length >= 2) {
--- app/javascript/controllers/search_results_controller.js
+++ app/javascript/controllers/search_results_controller.js
@@ -1,9 +1,19 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class SearchResults extends Controller {
-  static targets = ["result"]
+  static targets = ["result", "popup"]
 
   connect() {
+    if (this.hasPopupTarget) {
+      document.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") this.popupTarget.innerHTML = ""
+      })
+
+      document.addEventListener("click", (e) => {
+        if (!this.popupTarget.contains(e.target)) this.popupTarget.innerHTML = ""
+      })
+    }
+
     if (this.hasResultTarget) {
       this.resultTarget.focus()
     } else {
