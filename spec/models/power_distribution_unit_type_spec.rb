fe312b8a9eb87791ed247314dceac6bac0dc9c90
Félix Filmont <f.filmont@pantographe.studio>
2026-05-20 16:08:48 +0200

    add model spec

--- spec/models/power_distribution_unit_type_spec.rb
+++ spec/models/power_distribution_unit_type_spec.rb
@@ -3,5 +3,7 @@
 require "rails_helper"
 
 RSpec.describe PowerDistributionUnitType do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do
+    it { is_expected.to belong_to(:manufacturer) }
+  end
 end
