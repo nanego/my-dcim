a49d1f8ef14ad0f631ac4c39dd9d23a684e54bb1
Félix Filmont <f.filmont@pantographe.studio>
2026-05-20 16:03:56 +0200

    adding processor and policy test

---
+++ spec/processors/power_distribution_unit_types_processor_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe PowerDistributionUnitTypesProcessor do
+  subject(:result) { described_class.call(input, params) }
+
+  let(:input) { PowerDistributionUnitType.all }
+  let(:params) { {} }
+
+  describe "when searching" do
+    let(:params) { { q: "wood" } }
+
+    before do
+      PowerDistributionUnitType.create!(name: "brick", manufacturer: manufacturers(:fortinet))
+      PowerDistributionUnitType.create!(name: "wood", manufacturer: manufacturers(:fortinet))
+      PowerDistributionUnitType.create!(name: "wooden", manufacturer: manufacturers(:fortinet))
+    end
+
+    it { expect(result.size).to eq(2) }
+  end
+
+  describe "when filtering by manufacturer_ids" do
+    let(:manufacturer) { Manufacturer.create!(name: "M1") }
+    let(:power_distribution_unit_type) { PowerDistributionUnitType.create!(name: "PDUType1", manufacturer:) }
+
+    before do
+      power_distribution_unit_type
+      PowerDistributionUnitType.create!(name: "PDUType2", manufacturer: manufacturers(:juniper))
+    end
+
+    context "with one manufacturer_ids" do
+      let(:params) { { manufacturer_ids: manufacturer.id } }
+
+      it { expect(result.size).to eq(1) }
+      it { is_expected.to contain_exactly(power_distribution_unit_type) }
+    end
+
+    context "with many site_ids" do
+      let(:manufacturer_second) { Manufacturer.create!(name: "M2") }
+      let(:power_distribution_unit_type_second) do
+        PowerDistributionUnitType.create!(name: "PDUType2", manufacturer: manufacturer_second)
+      end
+
+      let(:params) { { manufacturer_ids: [manufacturer.id, manufacturer_second.id] } }
+
+      before do
+        power_distribution_unit_type_second
+      end
+
+      it { expect(result.size).to eq(2) }
+      it { is_expected.to contain_exactly(power_distribution_unit_type, power_distribution_unit_type_second) }
+    end
+  end
+
+  describe "when sorting" do
+    pending "TODO"
+  end
+end
