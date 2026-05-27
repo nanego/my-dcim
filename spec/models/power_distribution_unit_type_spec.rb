323504389d291044c95e6cbcae70189a4001c200
Félix Filmont <f.filmont@pantographe.studio>
2026-05-27 16:51:15 +0200

    add changelog test and validations to PDU type

--- spec/models/power_distribution_unit_type_spec.rb
+++ spec/models/power_distribution_unit_type_spec.rb
@@ -3,6 +3,36 @@
 require "rails_helper"
 
 RSpec.describe PowerDistributionUnitType do
+  it_behaves_like "changelogable", object: -> { described_class.new(name: "name", manufacturer: manufacturers(:fortinet), current_type: :three_phase) },
+                                   new_attributes: { name: "new name" }
+
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to define_enum_for(:current_type).with_values(%i[three_phase single_phase]) }
+
+    describe "documentation_url format" do
+      before { composant.valid? }
+
+      describe "with empty documentation_url" do
+        subject(:composant) { described_class.new(documentation_url: "") }
+
+        it { expect(composant.errors.where(:name, :invalid).count).to eq(0) }
+      end
+
+      describe "with valid documentation_url" do
+        subject(:composant) { described_class.new(documentation_url: "http://exemple.com/doc/1") }
+
+        it { expect(composant.errors.where(:name, :invalid).count).to eq(0) }
+      end
+
+      describe "with invalid documentation_url" do
+        subject(:composant) { described_class.new(documentation_url: "some invalid url") }
+
+        it { expect(composant.errors.where(:documentation_url, :invalid).count).to eq(1) }
+      end
+    end
+  end
+
   describe "associations" do
     it { is_expected.to belong_to(:manufacturer) }
   end
