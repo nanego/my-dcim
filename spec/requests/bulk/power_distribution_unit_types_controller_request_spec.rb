0c3c646df185c1b75491f3053b6370d76a1f8033
Félix Filmont <f.filmont@pantographe.studio>
2026-05-27 16:24:31 +0200

    adding tests on bulk

--- spec/requests/bulk/power_distribution_unit_types_controller_request_spec.rb
+++ spec/requests/bulk/power_distribution_unit_types_controller_request_spec.rb
@@ -12,9 +12,9 @@ RSpec.describe Bulk::PowerDistributionUnitTypesController do
       @response # rubocop:disable RSpec/InstanceVariable
     end
 
-    let(:ids) { [power_distribution_unit_types(:one).id] }
+    let(:ids) { [power_distribution_unit_types(:one).id, power_distribution_unit_types(:two).id] }
 
-    it { expect { response }.to change(PowerDistributionUnitType, :count).by(-1) }
+    it { expect { response }.to change(PowerDistributionUnitType, :count).by(-2) }
     it { expect(response).to redirect_to(power_distribution_unit_types_path) }
   end
 end
