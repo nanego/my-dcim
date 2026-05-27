fa2c835035d222d14756822cd05f403152faee32
Nicolas Brousse <n.brousse@pantographe.studio>
2026-05-27 17:42:07 +0200

    refactor: group CardType by PortType on Server form

---
+++ app/decorators/card_type_decorator.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+class CardTypeDecorator < ApplicationDecorator
+  class << self
+    include ActionView::Helpers::FormOptionsHelper
+
+    def grouped_by_port_type_options_for_select
+      grouped_card_types = CardType.includes(:port_type)
+        .sorted
+        .group_by(&:port_type)
+        .map do |port_type, card_types|
+          [port_type.to_s, card_types.map { |card_type| [card_type.to_s, card_type.id] }]
+      end
+
+      grouped_options_for_select(grouped_card_types)
+    end
+  end
+end
