5c0366e7e81a7d2bbc4d013e4decc2a81f8041d0
Félix Filmont <f.filmont@pantographe.studio>
2026-06-04 18:18:07 +0200

    scaffold

--- app/controllers/power_distribution_units_controller.rb
+++ app/controllers/power_distribution_units_controller.rb
@@ -1,116 +1,71 @@
 # frozen_string_literal: true
 
 class PowerDistributionUnitsController < ApplicationController
-  before_action :set_pdu, only: %i[show edit update destroy]
-  before_action except: %i[index] do
-    # TODO: prefer use ActiveRecord translation of Modele name
-    breadcrumb.add_step(t("power_distribution_units.index.title"), power_distribution_units_path)
-  end
+  before_action :set_power_distribution_unit, only: %i[show edit update destroy]
 
+  # GET /power_distribution_units or /power_distribution_units.json
   def index
-    @pdus = scoped_power_distribution_units.includes(:frame, :room, :islet, bay: :frames, modele: :category)
-      .references(:room, :islet, :bay, modele: :category)
-      .order(:name)
-    authorize! @pdus, with: PowerDistributionUnitPolicy
-
-    @filter = ProcessorFilter.new(@pdus, params, with: PowerDistributionUnitsProcessor)
-    @pdus = @filter.results
-
-    respond_to do |format|
-      format.json
-      format.html { @pagy, @pdus = pagy(@pdus) }
-    end
+    @power_distribution_units = PowerDistributionUnit.all
   end
 
+  # GET /power_distribution_units/1 or /power_distribution_units/1.json
   def show; end
 
+  # GET /power_distribution_units/new
   def new
-    @pdu = Server.new
-    authorize! @pdu, with: PowerDistributionUnitPolicy
+    @power_distribution_unit = PowerDistributionUnit.new
   end
 
+  # GET /power_distribution_units/1/edit
   def edit; end
 
+  # POST /power_distribution_units or /power_distribution_units.json
   def create
-    @pdu = Server.new(pdu_params)
-    authorize! @pdu, with: PowerDistributionUnitPolicy
+    @power_distribution_unit = PowerDistributionUnit.new(power_distribution_unit_params)
 
     respond_to do |format|
-      if @pdu.save
-        format.html { redirect_to_new_or_to(power_distribution_unit_path(@pdu), notice: t(".flashes.created")) }
-        format.json { render :show, status: :created, location: @pdu }
+      if @power_distribution_unit.save
+        format.html { redirect_to @power_distribution_unit, notice: "Power distribution unit was successfully created." }
+        format.json { render :show, status: :created, location: @power_distribution_unit }
       else
-        format.html { render :new }
-        format.json { render json: @pdu.errors, status: :unprocessable_content }
+        format.html { render :new, status: :unprocessable_content }
+        format.json { render json: @power_distribution_unit.errors, status: :unprocessable_content }
       end
     end
   end
 
+  # PATCH/PUT /power_distribution_units/1 or /power_distribution_units/1.json
   def update
-    @pdu.assign_attributes(pdu_params)
-
-    if params[:preview]
-      respond_to do |format|
-        format.turbo_stream { render :preview, status: :unprocessable_content }
-      end
-    else
-      respond_to do |format|
-        if @pdu.save
-          format.html { redirect_to power_distribution_unit_path(@pdu), notice: t(".flashes.updated") }
-          format.json { render :show, status: :ok, location: @pdu }
-        else
-          format.html { render :edit }
-          format.json { render json: @pdu.errors, status: :unprocessable_content }
-        end
-      end
-    end
-  end
-
-  destroy_confirmation
-  def destroy
     respond_to do |format|
-      if @pdu.destroy
-        format.html { redirect_back_to_param_or power_distribution_units_path(search_params), notice: t(".flashes.destroyed") }
-        format.json { head :no_content }
+      if @power_distribution_unit.update(power_distribution_unit_params)
+        format.html { redirect_to @power_distribution_unit, notice: "Power distribution unit was successfully updated.", status: :see_other }
+        format.json { render :show, status: :ok, location: @power_distribution_unit }
       else
-        format.html { redirect_back_to_param_or power_distribution_units_path(search_params), alert: t(".flashes.not_destroyed") }
-        format.json { head :bad_request }
+        format.html { render :edit, status: :unprocessable_content }
+        format.json { render json: @power_distribution_unit.errors, status: :unprocessable_content }
       end
     end
   end
 
-  def duplicate
-    @original_pdu = scoped_power_distribution_units.friendly.find(params[:id].to_s.downcase)
-    authorize! @original_pdu, with: PowerDistributionUnitPolicy
+  # DELETE /power_distribution_units/1 or /power_distribution_units/1.json
+  def destroy
+    @power_distribution_unit.destroy!
 
-    @pdu = @original_pdu.deep_dup
+    respond_to do |format|
+      format.html { redirect_to power_distribution_units_path, notice: "Power distribution unit was successfully destroyed.", status: :see_other }
+      format.json { head :no_content }
+    end
   end
 
   private
 
-  def scoped_power_distribution_units
-    Server.only_pdus
+  # Use callbacks to share common setup or constraints between actions.
+  def set_power_distribution_unit
+    @power_distribution_unit = PowerDistributionUnit.find(params.expect(:id))
   end
 
-  def set_pdu
-    @pdu = scoped_power_distribution_units.friendly_find_by_numero_or_name(params[:id])
-    authorize! @pdu, with: PowerDistributionUnitPolicy
-  end
-
-  def pdu_params
-    params.expect(
-      power_distribution_unit: [
-        :photo, :comment, :position, :frame_id, :manager_id, :name, :modele_id, :numero, :critique, :domain_id,
-        :frame, # TODO: Check if it should be removed or if it's used somewhere
-        { cards_attributes: [%i[composant_id card_type_id twin_card_id orientation name first_position _destroy id]] },
-        { documents_attributes: [%i[document id _destroy]] },
-      ],
-    )
-  end
-
-  def search_params
-    params.permit(:sort, :sort_by, :page, :per_page, :q,
-                  network_types: [], bay_ids: [], islet_ids: [], room_ids: [], frame_ids: [], cluster_ids: [],
-                  manager_ids: [], domain_ids: [], modele_ids: [], stack_ids: [])
+  # Only allow a list of trusted parameters through.
+  def power_distribution_unit_params
+    params.expect(power_distribution_unit: %i[type_id_id bay_id_id side orientation name slug ipmi_url serial_number comment])
   end
 end
