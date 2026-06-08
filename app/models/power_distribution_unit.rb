754a24d22ec7a9ee229e9a56bf610eb4f1880593
Félix Filmont <f.filmont@pantographe.studio>
2026-06-08 15:57:38 +0200

    correct migration and model

--- app/models/power_distribution_unit.rb
+++ app/models/power_distribution_unit.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class PowerDistributionUnit < ApplicationRecord
-  belongs_to :type_id
-  belongs_to :bay_id
+  belongs_to :type, class_name: "PowerDistributionUnitType"
+  belongs_to :bay
 end
