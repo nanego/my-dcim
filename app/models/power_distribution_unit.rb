60b38ea9a9b9b91c917870ed5ed699166f2b0ddd
Félix Filmont <f.filmont@pantographe.studio>
2026-06-08 17:56:50 +0200

    wip

--- app/models/power_distribution_unit.rb
+++ app/models/power_distribution_unit.rb
@@ -3,4 +3,11 @@
 class PowerDistributionUnit < ApplicationRecord
   belongs_to :type, class_name: "PowerDistributionUnitType"
   belongs_to :bay
+
+  enum :orientation, { asc: 0, desc: 1 }, validate: true
+  enum :side, { left: 0, right: 1 }, validate: true
+
+  validates :name, presence: true
+
+  delegate :to_s, to: :name
 end
