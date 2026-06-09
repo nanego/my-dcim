6b15a9ef10f702f2316ac62bc8a1524ba9fdf2fa
Félix Filmont <f.filmont@pantographe.studio>
2026-06-09 17:05:34 +0200

    add through has_one in model

--- app/models/power_distribution_unit.rb
+++ app/models/power_distribution_unit.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 class PowerDistributionUnit < ApplicationRecord
+  has_changelog
+
   belongs_to :type, class_name: "PowerDistributionUnitType"
   belongs_to :bay
+
+  has_one :manufacturer, through: :type
+  has_one :islet, through: :bay
+  has_one :room, through: :bay
 
   enum :orientation, { asc: 0, desc: 1 }, validate: true
   enum :side, { left: 0, right: 1 }, validate: true
