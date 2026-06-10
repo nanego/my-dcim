c1bbd203e8d8cbd8af894cafea6c16d69aef35dc
Félix Filmont <f.filmont@pantographe.studio>
2026-06-10 14:39:06 +0200

    wip

--- app/processors/power_distribution_units_processor.rb
+++ app/processors/power_distribution_units_processor.rb
@@ -9,20 +9,24 @@ class PowerDistributionUnitsProcessor < ApplicationProcessor
     raw.where(PowerDistributionUnit.arel_table[:name].matches("%#{q}%"))
   end
 
-  map :room_ids, filter_with: :non_empty_array do |room_ids:|
-    raw.joins(:room).where(room_id: room_ids)
+  map :types_ids, filter_with: :non_empty_array do |types_ids:|
+    where(type_id: types_ids)
+  end
+
+  map :bay_ids, filter_with: :non_empty_array do |bay_ids:|
+    raw.where(bay_id: bay_ids)
+  end
+
+  map :side do |side|
+    where(side:)
+  end
+
+  map :orientation do |orientation|
+    where(orientation:)
   end
 
   map :islet_ids, filter_with: :non_empty_array do |islet_ids:|
     raw.joins(:islet).where(islet_id: islet_ids)
-  end
-
-  map :bays_ids, filter_with: :non_empty_array do |bays_ids:|
-    raw.joins(:bay).where(bay_id: bays_ids)
-  end
-
-  map :power_distribution_unit_type_ids, filter_with: :non_empty_array do |power_distribution_unit_type_ids:|
-    raw.joins(:power_distribution_unit_type).where(power_distribution_unit_type_id: power_distribution_unit_type_ids)
   end
 
   map :manufacturer_ids, filter_with: :non_empty_array do |manufacturer_ids:|
