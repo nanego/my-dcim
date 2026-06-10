0b3642b061ef7d9822b469d5d6fad0b580b69497
Félix Filmont <f.filmont@pantographe.studio>
2026-06-10 11:14:03 +0200

    view work

--- app/models/power_distribution_unit_type.rb
+++ app/models/power_distribution_unit_type.rb
@@ -7,6 +7,8 @@ class PowerDistributionUnitType < ApplicationRecord
 
   enum :current_type, { three_phase: 0, single_phase: 1 }, validate: true
 
+  scope :sorted, -> { order(Arel.sql("LOWER(name)")) }
+
   validates :name, presence: true
   validates :documentation_url, format: URI::DEFAULT_PARSER.make_regexp(%w[http https]), allow_blank: true
 
