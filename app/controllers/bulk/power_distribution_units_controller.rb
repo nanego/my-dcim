9682c6eb382b4af54d75f826aa203d9d9c79f4d8
Félix Filmont <f.filmont@pantographe.studio>
2026-06-10 14:39:06 +0200

    wip

--- app/controllers/bulk/power_distribution_units_controller.rb
+++ app/controllers/bulk/power_distribution_units_controller.rb
@@ -6,29 +6,27 @@ module Bulk
 
     def destroy
       respond_to do |format|
-        if @power_distribution_units.map(&:destroy).all?
-          # TODO: Use correct translation when available
-          format.html { redirect_to power_distribution_units_path, notice: t("bulk.resource.destroy.flashes.destroyed", resource: "PDU"), status: :see_other }
+        if @power_distribution_unit_types.map(&:destroy).all?
+          format.html do
+            redirect_to power_distribution_unit_path,
+                        notice: t("bulk.resource.destroy.flashes.destroyed", resource: PowerDistributionUnit.model_name.human.pluralize),
+                        status: :see_other
+          end
         else
           # TODO: tell which records has not been removed
-          # TODO: Use correct translation when available
-          format.html { redirect_to power_distribution_units_path, alert: t("bulk.resource.destroy.flashes.not_destroyed", resource: "PDU"), status: :see_other }
+          format.html do
+            redirect_to power_distribution_unit_path,
+                        alert: t("bulk.resource.destroy.flashes.not_destroyed", resource: PowerDistributionUnit.model_name.human),
+                        status: :see_other
+          end
         end
       end
     end
 
     private
 
-    def scoped_power_distribution_units
-      Server.only_pdus
-    end
-
     def set_power_distribution_units
-      authorize! @power_distribution_units = scoped_power_distribution_units.where(id: params[:ids])
-    end
-
-    def default_authorization_policy_class
-      PowerDistributionUnitPolicy
+      authorize! @power_distribution_units = PowerDistributionUnitType.where(id: params[:ids])
     end
   end
 end
