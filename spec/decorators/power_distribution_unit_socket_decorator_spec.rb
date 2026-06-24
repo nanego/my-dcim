d0a02b0578c9c82788f6935c34f7960bc571df33
Félix Filmont <f.filmont@pantographe.studio>
2026-06-24 17:30:33 +0200

    fix spec

--- spec/decorators/power_distribution_unit_socket_decorator_spec.rb
+++ spec/decorators/power_distribution_unit_socket_decorator_spec.rb
@@ -5,7 +5,7 @@ require "rails_helper"
 RSpec.describe PowerDistributionUnitSocketDecorator, type: :decorator do
   describe ".alim_port_type_options_for_select" do
     it do
-      expect(described_class.port_type_options_for_select).to contain_exactly(["ALIM", 4])
+      expect(described_class.alim_port_type_options_for_select).to contain_exactly(["ALIM", 4])
     end
   end
 end
