04cb654ab3bcbca6cc12e28e3f834aa318cbd457
Félix Filmont <f.filmont@pantographe.studio>
2026-06-24 15:29:22 +0200

    fix typo and tests

--- spec/models/power_distribution_unit/circuit_spec.rb
+++ spec/models/power_distribution_unit/circuit_spec.rb
@@ -5,8 +5,6 @@ require "rails_helper"
 RSpec.describe PowerDistributionUnit::Circuit do
   subject(:circuit) { described_class.new(name: "C1", record: power_distribution_units(:one)) }
 
-  it_behaves_like "changelogable", object: -> { circuit }, new_attributes: { name: "New name" }
-
   describe "associations" do
     it { is_expected.to belong_to(:record) }
   end
