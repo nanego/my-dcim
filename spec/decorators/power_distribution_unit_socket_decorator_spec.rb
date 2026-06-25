17ecdc7eeb4abd257b95a2c3ef70721f94b4077f
Nicolas Brousse <n.brousse@pantographe.studio>
2026-06-26 01:52:51 +0200

    Rename PowerDistributionUnitSocketDecorator.port_type_options_for_select

--- spec/decorators/power_distribution_unit_socket_decorator_spec.rb
+++ spec/decorators/power_distribution_unit_socket_decorator_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe PowerDistributionUnitSocketDecorator, type: :decorator do
-  describe ".alim_port_type_options_for_select" do
+  describe ".port_type_options_for_select" do
     it { expect(described_class.alim_port_type_options_for_select).to contain_exactly(["ALIM", 4]) }
   end
 end
