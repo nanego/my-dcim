4f2c15763f05aaedc5567622bcb46c7872befc89
B-Rass <B-Rass@users.noreply.github.com>
2026-06-25 14:57:25 +0200

    Rework PDU Type pages (clean and refacto) (#750)

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -48,4 +48,48 @@ RSpec.describe ApplicationHelper do
       it { expect(helper.value_with_unit(nil, "mm")).to be_nil }
     end
   end
+
+  describe "#boolean_to_badge_component" do
+    subject(:badge) { helper.boolean_to_badge_component(boolean) }
+
+    let(:boolean) { true }
+
+    context "with true" do
+      it { is_expected.to be_a BadgeComponent }
+      it { expect(badge.instance_variable_get(:@color)).to eq :success }
+
+      it do
+        expect(badge.content).to eq(
+          "<span class=\"d-inline-flex align-items-center\"><span class=\"bi bi-check-circle-fill text-success me-2\"></span><span>Oui</span></span>",
+        )
+      end
+    end
+
+    context "with false" do
+      let(:boolean) { false }
+
+      it { is_expected.to be_a BadgeComponent }
+      it { expect(badge.instance_variable_get(:@color)).to eq :danger }
+
+      it do
+        expect(badge.content).to eq(
+          "<span class=\"d-inline-flex align-items-center\"><span class=\"bi bi-x-circle-fill text-danger me-2\"></span><span>Non</span></span>",
+        )
+      end
+    end
+  end
+
+  describe "#value_or_nc" do
+    context "with a value" do
+      it { expect(helper.value_or_nc("value")).to eq("value") }
+    end
+
+    context "with a blank value" do
+      it { expect(helper.value_or_nc(nil)).to eq("<span class=\"fst-italic fw-light text-body-secondary\">n/c</span>") }
+    end
+
+    context "with an empty value" do
+      it { expect(helper.value_or_nc("")).to eq("<span class=\"fst-italic fw-light text-body-secondary\">n/c</span>") }
+    end
+  end
 end
