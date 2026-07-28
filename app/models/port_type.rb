e75f422cafa780fc20e477690406a9953902806c
Félix Filmont <f.filmont@pantographe.studio>
2026-07-28 18:01:58 +0200

    add array inclusion validation and trads

--- app/models/port_type.rb
+++ app/models/port_type.rb
@@ -6,6 +6,8 @@ class PortType < ApplicationRecord
   has_many :card_types, dependent: :restrict_with_error
   has_many :sockets, class_name: "PowerDistributionUnit::Socket", dependent: :restrict_with_error
 
+  validates :attachable_to, array_inclusion: { in: %w[server] }
+
   scope :sorted, -> { order(name: :asc) }
 
   delegate :to_s, to: :name
