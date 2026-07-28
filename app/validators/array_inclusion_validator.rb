3f8d62f31983d38d34fb78f666c48efcbdcc2a7a
Félix Filmont <f.filmont@pantographe.studio>
2026-07-28 18:01:58 +0200

    add array inclusion validation and trads

---
+++ app/validators/array_inclusion_validator.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class ArrayInclusionValidator < ActiveModel::EachValidator
+  def validate_each(record, attribute, values)
+    not_allowed_values = values - options[:in]
+
+    return if not_allowed_values.empty?
+
+    wrong_values = not_allowed_values.join(", ")
+    record.errors.add(attribute, :array_inclusion, wrong_values:)
+  end
+end
--- spec/models/port_type_spec.rb
+++ spec/models/port_type_spec.rb
@@ -12,6 +12,10 @@ RSpec.describe PortType do
     it { is_expected.to have_many(:sockets).dependent(:restrict_with_error) }
   end
 
+  describe "validations" do
+    it { is_expected.to validate_array_inclusion_of(:attachable_to).in(%w[server]) }
+  end
+
   describe "#is_power?" do
     it { expect(port_type.is_power?).to be false }
   end
