f3fbf03abefc619b354d474f7cb40846ae93336b
infdev-fil <f.filmont@pantographe.studio>
2026-07-29 17:57:32 +0200

    Socket unique number per PDU or PDUType (#779)

--- app/models/power_distribution_unit/socket.rb
+++ app/models/power_distribution_unit/socket.rb
@@ -11,5 +11,17 @@ class PowerDistributionUnit
     has_one :power_distribution_unit, through: :circuit, source: :record, source_type: "PowerDistributionUnit"
 
     validates :number, presence: true
+    validate :number_unique_within_circuit_record
+
+    private
+
+    def number_unique_within_circuit_record
+      return unless circuit
+
+      errors.add(:number, :taken) if circuit.record.sockets
+        .where(number:)
+        .where.not(id:)
+        .any?
+    end
   end
 end
