e047364fccebe5b436446dc9682f63933b6df50a
Félix Filmont <f.filmont@pantographe.studio>
2026-07-29 15:51:10 +0200

    fix test

--- spec/models/port_type_spec.rb
+++ spec/models/port_type_spec.rb
@@ -13,7 +13,7 @@ RSpec.describe PortType do
   end
 
   describe "validations" do
-    it { is_expected.to validate_array_inclusion_of(:attachable_to).in(%w[server]) }
+    it { is_expected.to validate_array_inclusion_of(:attachable_to).in(%w[pdu server]) }
   end
 
   describe "#is_power?" do
