962dc37effa292f695f95434037f667d6bc41757
Félix Filmont <f.filmont@pantographe.studio>
2026-07-29 17:29:35 +0200

    apply review changes

--- app/models/power_distribution_unit/socket.rb
+++ app/models/power_distribution_unit/socket.rb
@@ -11,11 +11,11 @@ class PowerDistributionUnit
     has_one :power_distribution_unit, through: :circuit, source: :record, source_type: "PowerDistributionUnit"
 
     validates :number, presence: true
-    validate :number_unique_within_pdu_or_pdu_type
+    validate :number_unique_within_circuit_record
 
     private
 
-    def number_unique_within_pdu_or_pdu_type
+    def number_unique_within_circuit_record
       return unless circuit
 
       errors.add(:number, :taken) if circuit.record.sockets
