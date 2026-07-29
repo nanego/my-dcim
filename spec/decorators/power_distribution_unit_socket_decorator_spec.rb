a1b3eb08b05c609d9c0253124e9893caabe35a1f
Félix Filmont <f.filmont@pantographe.studio>
2026-07-29 15:24:45 +0200

    make decorators use new enum array

--- spec/decorators/power_distribution_unit_socket_decorator_spec.rb
+++ spec/decorators/power_distribution_unit_socket_decorator_spec.rb
@@ -3,7 +3,7 @@
 require "rails_helper"
 
 RSpec.describe PowerDistributionUnitSocketDecorator, type: :decorator do
-  describe ".port_type_options_for_select" do
-    it { expect(described_class.port_type_options_for_select).to contain_exactly(["ALIM", 4]) }
+  describe ".port_types_options_for_select" do
+    it { expect(described_class.port_types_options_for_select).to contain_exactly(["ALIM", 4]) }
   end
 end
