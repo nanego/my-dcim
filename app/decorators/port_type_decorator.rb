d0534fc8ff6315f01cf965d66a5ac9124407ebf8
Félix Filmont <f.filmont@pantographe.studio>
2026-07-31 17:43:11 +0200

    wip

--- app/decorators/port_type_decorator.rb
+++ app/decorators/port_type_decorator.rb
@@ -5,6 +5,12 @@ class PortTypeDecorator < ApplicationDecorator
     collection.select(:id, :name).sorted.map { |p| [p.to_s, p.id] }
   end
 
+  def self.usable_by_options_for_select
+    PortType::USABLE_BY_VALUES.map do |value|
+      [I18n.t(".activerecord.attributes.port_type.usable_by/#{value}"), value]
+    end
+  end
+
   def css_class_name
     case name
     when "RJ", "XRJ"
