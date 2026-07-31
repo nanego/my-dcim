2b7bbbb1656b17a335ec4743a6885061ab6aac1c
Félix Filmont <f.filmont@pantographe.studio>
2026-07-31 17:02:51 +0200

    apply review changes wip

--- app/models/port_type.rb
+++ app/models/port_type.rb
@@ -6,16 +6,11 @@ class PortType < ApplicationRecord
   has_many :card_types, dependent: :restrict_with_error
   has_many :sockets, class_name: "PowerDistributionUnit::Socket", dependent: :restrict_with_error
 
-  validates :attachable_to, array_inclusion: { in: %w[pdu server] }
+  validates :usable_by, array_inclusion: { in: %w[pdu server] }
 
   scope :sorted, -> { order(name: :asc) }
   scope :power_ones, -> { where(is_power: true) }
-  scope :attachable_to_server, -> { where("? = ANY(attachable_to)", :server) }
-  scope :attachable_to_pdu, -> { where("? = ANY(attachable_to)", :pdu) }
+  scope :usable_by, ->(usable_by) { where("? = ANY(usable_by)", usable_by) }
 
   delegate :to_s, to: :name
-
-  def is_power?
-    is_power
-  end
 end
