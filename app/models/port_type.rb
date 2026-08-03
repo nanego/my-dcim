1d85a0e0040b7e2c3f9a84303547adab084700b8
Félix Filmont <f.filmont@pantographe.studio>
2026-08-03 18:17:22 +0200

    fix concern and tests

--- app/models/port_type.rb
+++ app/models/port_type.rb
@@ -9,7 +9,7 @@ class PortType < ApplicationRecord
   has_many :sockets, class_name: "PowerDistributionUnit::Socket", dependent: :restrict_with_error
 
   USABLE_BY_VALUES = %w[pdu server].freeze
-  array_enum :usable_by, USABLE_BY_VALUES.index_with(&:to_s)
+  array_enum :usable_by, USABLE_BY_VALUES.index_with(&:to_s), validate: true
 
   scope :sorted, -> { order(name: :asc) }
   scope :power_ones, -> { where(is_power: true) }
