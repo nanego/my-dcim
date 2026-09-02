b9e45969cb0daa9d67e2dac8fcdbeef35d3717dd
Félix Filmont <f.filmont@pantographe.studio>
2026-09-02 17:35:38 +0200

    add tests

--- spec/requests/connections_controller_request_spec.rb
+++ spec/requests/connections_controller_request_spec.rb
@@ -107,17 +107,13 @@ RSpec.describe ConnectionsController do
       end
     end
 
-    # context "without attributes" do
-    #   let(:params) { { connection: {} } }
+    context "without to_port_id" do
+      let(:params) { { connection: { from_port_id: "1" } } }
+      let(:records) { [connections(:one), cables(:one), connections(:two)] }
 
-    #   it { expect { response }.to raise_error(ActionController::ParameterMissing) }
-    # end
-
-    # context "without parameters" do
-    #   let(:params) { {} }
-
-    #   it { expect { response }.to raise_error(ActionController::ParameterMissing) }
-    # end
+      it { expect(response).to have_http_status(:redirect) }
+      it { expect { response }.to change { records.count { |r| r.class.exists?(r.id) } }.from(3).to(0) }
+    end
 
     context "with invalid parameters" do
       let(:params) { { connection: invalid_attributes } }
