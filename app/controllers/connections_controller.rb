269c7c353a79c62db0ee52e4c7d8c0244012e894
Félix Filmont <f.filmont@pantographe.studio>
2026-09-09 12:29:01 +0200

    fix rubocop

--- app/controllers/connections_controller.rb
+++ app/controllers/connections_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class ConnectionsController < ApplicationController # rubocop:disable Metrics/ClassLength
-  def edit # rubocop:disable Metrics/AbcSize, Metrics/PerceivedComplexity
+  def edit # rubocop:disable Metrics/AbcSize, Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity
     authorize!
 
     @from_port = if params[:from_port_id].present? && params[:from_port_id].to_i.positive?
