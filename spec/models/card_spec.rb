d8b2c0ae2dbb3d0ec53c9e63a4bd33e907632c5b
Félix Filmont <f.filmont@pantographe.studio>
2026-09-14 17:55:36 +0200

    create card shared exemple wip

--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -4,57 +4,18 @@ require "rails_helper"
 
 RSpec.describe Card do
   let(:card) { cards(:one) }
+
+  it_behaves_like "a card"
+
   # it_behaves_like "changelogable", object: -> { described_class.new }, new_attributes: { name: "New name" }
 
   describe "associations" do
-    it { is_expected.to belong_to(:card_type) }
-    it { is_expected.to belong_to(:twin_card).optional }
     it { is_expected.to belong_to(:server) }
     it { is_expected.to belong_to(:composant) }
-
-    it { is_expected.to have_many(:ports).dependent(:destroy) }
-    it { is_expected.to have_many(:cables).through(:ports) }
-  end
-
-  describe "validations" do
-    it { is_expected.to validate_numericality_of(:first_position).only_integer.is_in(0..100).allow_nil }
-  end
-
-  describe "#ensure_card_type_have_enough_ports" do
-    before do
-      card.card_type = card_type
-      card.validate
-    end
-
-    context "when enough ports" do
-      let(:card_type) { card_types(:four) }
-
-      it { expect(card).to be_valid }
-    end
-
-    context "when not enough ports" do
-      let(:card) { cards(:one) }
-      let(:card_type) { card_types(:two) }
-
-      it { expect(card).not_to be_valid }
-      it { expect(card.errors.where(:card_type_id, :not_enough_ports).count).to eq(1) }
-    end
   end
 
   describe "#to_s" do
     it { expect(card.to_s).to eq("Carte ServerName1 / Card1 / compo1") }
-  end
-
-  describe "#first_port_position" do
-    pending
-  end
-
-  describe "#positions_with_ports" do
-    pending
-  end
-
-  describe "#create_missing_ports" do
-    pending
   end
 
   describe "#set_twin_card" do
