5a886cd256f7ba0dbc64ea0a7db6781bea64d1bc
Félix Filmont <f.filmont@pantographe.studio>
2026-09-16 16:50:33 +0200

    fix cards chosen when duping

--- app/models/power_distribution_unit.rb
+++ app/models/power_distribution_unit.rb
@@ -56,7 +56,7 @@ class PowerDistributionUnit < ApplicationRecord
 
     copy.tap do |pdu|
       pdu.circuits = circuits.map(&:deep_dup)
-      pdu.cards = type.cards.map(&:dup)
+      pdu.cards = cards.map(&:dup)
     end
   end
 
