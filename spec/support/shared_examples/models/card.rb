d8948b5e6f0d578d7e852aec46e7506fe20e93fb
Félix Filmont <f.filmont@pantographe.studio>
2026-09-17 11:45:49 +0200

    move card types port number check on validation

--- spec/support/shared_examples/models/card.rb
+++ spec/support/shared_examples/models/card.rb
@@ -10,26 +10,26 @@ shared_examples_for "a card" do
 
   describe "validations" do
     it { is_expected.to validate_numericality_of(:first_position).only_integer.is_in(0..100).allow_nil }
-  end
 
-  describe "#ensure_card_type_have_enough_ports" do
-    before do
-      card.card_type = card_type
-      card.validate
-    end
+    describe "#ensure_card_type_have_enough_ports" do
+      before do
+        card.card_type = card_type
+        card.validate
+      end
 
-    context "when enough ports" do
-      let(:card_type) { card_types(:four) }
+      context "when enough ports" do
+        let(:card_type) { card_types(:four) }
 
-      it { expect(card).to be_valid }
-    end
+        it { expect(card).to be_valid }
+      end
 
-    context "when not enough ports" do
-      let(:card) { cards(:one) }
-      let(:card_type) { card_types(:two) }
+      context "when not enough ports" do
+        let(:card) { cards(:one) }
+        let(:card_type) { card_types(:two) }
 
-      it { expect(card).not_to be_valid }
-      it { expect(card.errors.where(:card_type_id, :not_enough_ports).count).to eq(1) }
+        it { expect(card).not_to be_valid }
+        it { expect(card.errors.where(:card_type_id, :not_enough_ports).count).to eq(1) }
+      end
     end
   end
 
